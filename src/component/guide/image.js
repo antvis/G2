73914e1240b119bd01a891c21e890029419b6739
simaQ <sima.zhang1990@gmail.com>
2017-06-20 14:36:20 +0800

    添加 Guide.Image

--- src/component/guide/image.js
+++ src/component/guide/image.js
@@ -1,62 +1,82 @@
-/**
- * @fileOverview guide image
- * @author dxq613@gmail.com
- */
+const Util = require('../../util');
+const Base = require('./base');
 
-'use strict';
-
-var Util = require('@ali/g-util');
-var Guide = require('./guide');
-
-/**
- * @class Guide.Image
- * 辅助图片
- */
-var Img = function(cfg) {
-  Img.superclass.constructor.call(this, cfg);
-};
-
-Util.extend(Img, Guide);
-
-Util.augment(Img, {
-  /**
-   * 起始位置
-   * @type {Array}
-   */
-  start: [],
-  /**
-   * 结束位置
-   * @type {Array}
-   */
-  end: null,
-  /**
-   * 图片地址
-   * @type {String}
-   */
-  src: '',
-  // override
-  paint: function(coord, group) {
-    var self = this;
-    var start = self.parsePoint(coord, self.start);
-    var cfg = self.cfg;
-
-    cfg.img = cfg.src;
-    cfg = Util.mix({
-      src: self.src
-    }, cfg, start);
-    if (self.end) {
-      var end = self.parsePoint(coord, self.end);
-      cfg.x = start.x;
-      cfg.y = end.y;
-      cfg.width = end.x - start.x;
-      cfg.height = start.y - end.y;
-    } else {
-      cfg.y = cfg.y - cfg.height;
-    }
-    group.addShape('Image', {
-      attrs: cfg
+class Image extends Base {
+  getDefaultCfg() {
+    const cfg = super.getDefaultCfg();
+    return Util.mix({}, cfg, {
+      /**
+       * 辅助元素类型
+       * @type {String}
+       */
+      type: 'image',
+      zIndex: 1,
+      /**
+       * 辅助图片的起点位置
+       * @type {Object | Function}
+       */
+      start: null,
+      /**
+       * 辅助图片的终点位置
+       * @type {Object | Function}
+       */
+      end: null,
+      /**
+       * 辅助图片的地址
+       * @type {Strinf}
+       */
+      src: null,
+      /**
+       * x 方向的偏移量
+       * @type {Number}
+       */
+      offsetX: null,
+      /**
+       * y 方向的偏移量
+       * @type {Number}
+       */
+      offsetY: null
     });
   }
-});
 
-module.exports = Img;
+  paint(coord, group) {
+    const self = this;
+    const start = self.parsePoint(coord, self.start);
+
+    const cfg = {
+      x: start.x,
+      y: start.y
+    };
+    cfg.img = self.src;
+
+    if (!self.end) { // 如果咩有指定结束点，则 start 为图片的左上角坐标
+      if (self.width) {
+        cfg.width = self.width;
+      }
+
+      if (self.height) {
+        cfg.height = self.height;
+      }
+    } else {
+      const end = self.parsePoint(coord, self.end);
+      cfg.width = Math.abs(end.x - start.x);
+      cfg.height = Math.abs(start.y - end.y);
+    }
+
+    if (self.offsetX) {
+      cfg.x += self.offsetX;
+    }
+
+    if (self.offsetY) {
+      cfg.y += self.offsetY;
+    }
+
+    const imgGuide = group.addShape('Image', {
+      zIndex: 1,
+      attrs: cfg
+    });
+    imgGuide.name = 'guide-image';
+  }
+}
+
+module.exports = Image;
