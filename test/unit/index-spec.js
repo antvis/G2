46fa063860ef607a6619cfab2411d87976c1ed24
leungwensen <leungwensen@gmail.com>
2017-07-17 16:43:29 +0800

    electron-mocha to @lite-js/torch

--- test/unit/index-spec.js
+++ test/unit/index-spec.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect;
-const isRenderer = require('is-electron-renderer');
 const G2 = require('../../index');
 
 describe('sample', () => {
@@ -7,13 +6,4 @@ describe('sample', () => {
     expect('G2 3.0').to.be.a('string');
     expect(G2).to.be.an('object');
   });
-});
-
-after(() => {
-  if (isRenderer && window.__coverage__) {
-    const { remote } = require('electron');
-    const fs = remote.require('fs');
-    const path = remote.require('path');
-    fs.writeFileSync(path.resolve(process.cwd(), './test/coverage/coverage.json'), JSON.stringify(window.__coverage__));
-  }
 });
