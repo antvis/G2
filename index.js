1de55cec18193fdea6dafbf156523af5faf304d9
simaQ <sima.zhang1990@gmail.com>
2017-08-09 17:15:42 +0800

    支持注册动画

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const Scale = require('./src/scale/index');
 const Chart = require('./src/chart/chart');
 const Shape = require('./src/geom/shape/');
 const Global = require('./src/global');
+const Animate = require('./src/animate/animate');
+
 module.exports = {
   Util,
   Scale,
   Chart,
   Shape,
-  Global
+  Global,
+  Animate
 };
