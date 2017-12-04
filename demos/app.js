1b161a94e9b0a44aaadda9b76875642f14824e13
leungwensen <leungwensen@gmail.com>
2017-12-04 11:51:37 +0800

    chore: better scripts for dev
    1. `npm run dev`: npm run watch && npm run demos-web
    2. ./bin/screenshot.js --port 12345 --name area
    3. demos page: support filtering, live-editting, code copying, etc.
    
    Closes: #218

--- demos/app.js
+++ demos/app.js
@@ -1,13 +1,43 @@
+const debug = require('debug')('app:demos');
 const commander = require('commander');
 const connect = require('connect');
 const getPort = require('get-port');
 const http = require('http');
 const open = require('open');
 const serveStatic = require('serve-static');
+const parseurl = require('parseurl');
 const assign = require('lodash').assign;
-const resolve = require('path').resolve;
-
+const path = require('path');
+const resolve = path.resolve;
+const extname = path.extname;
+const basename = path.basename;
+const join = path.join;
+const fs = require('fs');
+const statSync = fs.statSync;
+const lstatSync = fs.lstatSync;
+const readdirSync = fs.readdirSync;
+const readFileSync = fs.readFileSync;
+const mkdirSync = fs.mkdirSync;
+const nunjucks = require('nunjucks');
+const renderString = nunjucks.renderString;
 const pkg = require('../package.json');
+
+function isFile(source) {
+  return lstatSync(source).isFile();
+}
+
+function getFiles(source) {
+  return readdirSync(source).map(function(name) {
+    return join(source, name);
+  }).filter(isFile);
+}
+
+const screenshotsPath = join(process.cwd(), './demos/assets/screenshots');
+try {
+  statSync(screenshotsPath);
+} catch (e) {
+  mkdirSync(screenshotsPath);
+}
 
 commander
   .version(pkg.version)
@@ -17,24 +47,54 @@ commander
 
 function startService(port) {
   const server = connect();
+  server.use((req, res, next) => {
+    if (req.method === 'GET') {
+      const pathname = parseurl(req).pathname;
+      if (pathname === '/demos/index.html') {
+        const demoFiles = getFiles(__dirname)
+          .filter(filename => {
+            return extname(filename) === '.html';
+          })
+          .map(filename => {
+            const bn = basename(filename, '.html');
+            const file = {
+              screenshot: `/demos/assets/screenshots/${bn}.png`,
+              basename: bn,
+              content: readFileSync(filename),
+              filename
+            };
+            return file;
+          });
+        const template = readFileSync(join(__dirname, './index.njk'), 'utf8');
+        res.end(renderString(template, {
+          demoFiles
+        }));
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  });
   server.use(serveStatic(process.cwd()));
   http.createServer(server).listen(port);
 
   const url = `http://127.0.0.1:${port}/demos/index.html`;
-  console.log(`server started, demos available! ${url}`);
+  debug(`server started, demos available! ${url}`);
 
   if (commander.web) {
+    debug('running on web!');
     open(url);
   } else {
+    debug('running on electron!');
     const app = require('electron').app;
     const BrowserWindow = require('electron').BrowserWindow;
     const watch = require('@lite-js/torch/lib/watch');
     const windowBoundsConfig = require('@lite-js/torch/lib/windowBoundsConfig')(
-      resolve(app.getPath('userData'), './g2-config.json')
+      resolve(app.getPath('userData'), './data-set-config.json')
     );
 
     let win;
-
     app.once('ready', () => {
       win = new BrowserWindow(assign({
         // transparent: true
@@ -42,26 +102,22 @@ function startService(port) {
           nodeIntegration: false
         }
       }, windowBoundsConfig.get('demos')));
-
       win.loadURL(url);
-
       win.openDevTools();
 
       win.on('close', () => {
         windowBoundsConfig.set('demos', win.getBounds());
       });
-
       win.on('closed', () => {
         win = null;
       });
-
       watch([
-        'demos/**/*.*'
+        'demos/**/*.*',
+        'src/**/*.*'
       ], () => {
         win.webContents.reloadIgnoringCache();
       });
     });
-
     app.on('window-all-closed', () => {
       app.quit();
     });
--- demos/assets/index.js
+++ demos/assets/index.js
@@ -1,14 +1,25 @@
-(() => {
-  const {
-    plotById,
-    plotsByGeometry
-  } = GLOBAL_DATA
-  const {
-    assignIn
-  } = _
+(function () {
+  // filtering
+  const $query = $('#query');
+  function filter() {
+    const str = $query.val();
+    if (!str) {
+      $('.demo-thumbnail').show();
+    } else {
+      $('.demo-thumbnail').each(function () {
+        const $thumbnail = $(this);
+        const basename = $thumbnail.data('basename');
+        if (basename.indexOf(str) === -1) {
+          $thumbnail.hide();
+        }
+      });
+    }
+  }
+  $query.on('input', _.debounce(filter));
 
-  const $code = $('#code')
-
+  // router
+  let currentId;
+  const $code = $('#code');
   const htmlEditor = CodeMirror.fromTextArea($code[0], {
     mode: "text/html",
     extraKeys: {
@@ -21,27 +32,72 @@
     ],
     lineNumbers: true,
     lineWrapping: false
-  })
+  });
 
-  const $docContainer = $('.doc-container')
-  const $chartPanel = $('.chart-panel')
+  const $docContainer = $('#doc-container');
+  const $chartPanel = $('#chart-panel');
+  const $codePanel = $('#code-panel');
 
-  function loadPlotPage(id) {
-    $docContainer.show()
-    const plot = plotById[id]
-    $.get(plot.path, data => {
-      $chartPanel.html('<iframe class="chart-frame" frameborder="0"></iframe>')
-      $chartPanel.find('iframe')[0].contentWindow.document.write(data)
-      htmlEditor.getDoc().setValue(data)
-    })
+  function syncCode(code) {
+    $chartPanel.html('<iframe class="chart-frame" frameborder="0"></iframe>');
+    $chartPanel.find('iframe')[0].contentWindow.document.write(code);
+    htmlEditor.getDoc().setValue(code);
   }
 
   routie({
     '/:id': id => {
-      loadPlotPage(id)
+      $docContainer.show();
+      const $htmlCode = $(`#code-${id}`);
+      const code = $htmlCode.text();
+      syncCode(code)
     },
-    'geometry-:geometry': geometry => {
-      $docContainer.hide()
+    '': () => {
+      $docContainer.hide();
+    }
+  });
+
+  // resizable
+  $codePanel.resizable({
+    handleSelector: '#resize-handler',
+    resizeWidthFrom: 'right',
+    resizeHeight: false,
+    onDragStart() {
+      $docContainer.css('pointer-events', 'none');
+      $docContainer.css('cursor', 'col-resize');
+      $codePanel.find('.CodeMirror-gutter-elt').css('cursor', 'col-resize');
     },
-  })
+    onDragEnd() {
+      $docContainer.css('pointer-events', 'auto');
+      $docContainer.css('cursor', 'default');
+      $codePanel.find('.CodeMirror-gutter-elt').css('cursor', 'default');
+    },
+  });
+
+  // copy code
+  const BTN_COPY_SELECTOR = '#copy-code';
+  const clipboard = new Clipboard(BTN_COPY_SELECTOR, {
+    text: () => htmlEditor.getValue(),
+  });
+  let timer;
+  clipboard.on('success', e => {
+    e.clearSelection();
+    $(BTN_COPY_SELECTOR).text('Succeed!');
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      $(BTN_COPY_SELECTOR).text('Copy');
+    }, 2000);
+  });
+  clipboard.on('error', e => {
+    e.clearSelection();
+    $(BTN_COPY_SELECTOR).text('Failed!');
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      $(BTN_COPY_SELECTOR).text('Copy');
+    }, 2000);
+  });
+
+  // run code
+  $('#execute').on('click', () => {
+    syncCode(htmlEditor.getValue());
+  });
 })();
