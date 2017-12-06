64d4ad2b1b55c11e3af1df83875ccd6c765c79ec
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2017-12-06 16:03:19 +0800

    fix(scale): very little number ticks

--- src/global.js
+++ src/global.js
@@ -7,7 +7,7 @@ const Theme = require('./theme/index');
 
 const Global = {};
 const Default = {
-  version: '3.0.1',
+  version: '3.0.2-beta.1',
   trackable: true,
   animate: true,
   snapArray: [ 0, 1, 2, 4, 5, 10 ],
