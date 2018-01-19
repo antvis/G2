8f5ea9122f3e203d830870a32707507f8a136716
dxq613 <dxq613@gmail.com>
2018-01-19 11:10:22 +0800

    feat(highlight): active shape support highlight

--- test/bugs/issue-325-spec.js
+++ test/bugs/issue-325-spec.js
@@ -76,7 +76,47 @@ describe('#360', () => {
     expect(shapes[0].get('selected')).equal(true);
   });
 
-  it('destroy', function() {
+  it('interval active highlight', function(done) {
+    const newData = [
+      { genre: 'Sports', sold: 275, type: '1' },
+      { genre: 'Strategy', sold: 115, type: '1' },
+      { genre: 'Action', sold: 120, type: '1' },
+      { genre: 'Shooter', sold: 350, type: '1' },
+      { genre: 'Other', sold: 150, type: '1' },
+
+      { genre: 'Sports', sold: 175, type: '2' },
+      { genre: 'Strategy', sold: 215, type: '2' },
+      { genre: 'Action', sold: 220, type: '2' },
+      { genre: 'Shooter', sold: 250, type: '2' },
+      { genre: 'Other', sold: 50, type: '2' }
+    ];
+    chart.clear();
+    chart.coord();
+    chart.source(newData);
+    chart.tooltip({
+      crosshairs: false,
+      shared: false
+    });
+    const geom = chart.interval()
+      .position('genre*sold')
+      .color('type')
+      .active(true, {
+        animate: true,
+        highlight: true,
+        style: { fillOpacity: 0.9 }
+      })
+      .adjust('dodge');
+    chart.render();
+
+    const shapes = geom.getShapes();
+    geom.setShapesActived([ shapes[0] ]);
+    setTimeout(function() {
+      expect(shapes[0].attr('fillOpacity')).equal(0.9);
+      done();
+    }, 600);
+  });
+
+  xit('destroy', function() {
     chart.destroy();
   });
 
