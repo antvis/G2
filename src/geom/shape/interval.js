6c7e32aff2c0d6a7542119abc7a61fa88a2fcbe4
leungwensen <leungwensen@gmail.com>
2018-01-24 17:36:29 +0800

    feat(liquid-fill): add functions to draw water waves, etc.

--- src/geom/shape/interval.js
+++ src/geom/shape/interval.js
@@ -426,6 +426,135 @@ Shape.registerShape('interval', 'pyramid', {
 });
 
 // 水波图
+/**
+ * 用贝塞尔曲线模拟正弦波
+ * Using Bezier curves to fit sine wave.
+ * There is 4 control points for each curve of wave,
+ * which is at 1/4 wave length of the sine wave.
+ *
+ * The control points for a wave from (a) to (d) are a-b-c-d:
+ *          c *----* d
+ *     b *
+ *       |
+ * ... a * ..................
+ *
+ * whose positions are a: (0, 0), b: (0.5, 0.5), c: (1, 1), d: (PI / 2, 1)
+ *
+ * @param {number} x          x position of the left-most point (a)
+ * @param {number} stage      0-3, stating which part of the wave it is
+ * @param {number} waveLength wave length of the sine wave
+ * @param {number} amplitude  wave amplitude
+ * @return {Array} 正弦片段曲线
+ */
+function getWaterWavePositions(x, stage, waveLength, amplitude) {
+  if (stage === 0) {
+    return [
+      [ x + 1 / 2 * waveLength / Math.PI / 2, amplitude / 2 ],
+      [ x + 1 / 2 * waveLength / Math.PI, amplitude ],
+      [ x + waveLength / 4, amplitude ]
+    ];
+  } else if (stage === 1) {
+    return [
+      [ x + 1 / 2 * waveLength / Math.PI / 2 * (Math.PI - 2),
+        amplitude
+      ],
+      [ x + 1 / 2 * waveLength / Math.PI / 2 * (Math.PI - 1),
+        amplitude / 2
+      ],
+      [ x + waveLength / 4, 0 ]
+    ];
+  } else if (stage === 2) {
+    return [
+      [ x + 1 / 2 * waveLength / Math.PI / 2, -amplitude / 2 ],
+      [ x + 1 / 2 * waveLength / Math.PI, -amplitude ],
+      [ x + waveLength / 4, -amplitude ]
+    ];
+  }
+  return [
+    [ x + 1 / 2 * waveLength / Math.PI / 2 * (Math.PI - 2), -amplitude ],
+    [ x + 1 / 2 * waveLength / Math.PI / 2 * (Math.PI - 1), -amplitude / 2 ],
+    [ x + waveLength / 4, 0 ]
+  ];
+}
+/**
+ * 获取水波路径
+ * @param  {number} radius          半径
+ * @param  {number} waterLevel      水位
+ * @param  {number} waveLength      波长
+ * @param  {number} phase           相位
+ * @param  {number} amplitude       震幅
+ * @param  {number} cx              圆心x
+ * @param  {number} cy              圆心y
+ * @return {Array}  path            路径
+ * @reference http://gitlab.alipay-inc.com/datavis/g6/blob/1.2.0/src/graph/utils/path.js#L135
+ */
+function getWaterWavePath(radius, waterLevel, waveLength, phase, amplitude, cx, cy) {
+  const curves = Math.ceil(2 * radius / waveLength * 4) * 2;
+  const path = [];
+
+  // map phase to [-Math.PI * 2, 0]
+  while (phase < -Math.PI * 2) {
+    phase += Math.PI * 2;
+  }
+  while (phase > 0) {
+    phase -= Math.PI * 2;
+  }
+  phase = phase / Math.PI / 2 * waveLength;
+
+  const left = cx - radius + phase - radius * 2;
+  /**
+   * top-left corner as start point
+   *
+   * draws this point
+   *  |
+   * \|/
+   *  ~~~~~~~~
+   *  |      |
+   *  +------+
+   */
+  path.push([ 'M', left, waterLevel ]);
+
+  /**
+   * top wave
+   *
+   * ~~~~~~~~ <- draws this sine wave
+   * |      |
+   * +------+
+   */
+  let waveRight = 0;
+  for (let c = 0; c < curves; ++c) {
+    const stage = c % 4;
+    const pos = getWaterWavePositions(c * waveLength / 4, stage, waveLength, amplitude);
+    path.push([
+      'C',
+      pos[0][0] + left, -pos[0][1] + waterLevel,
+      pos[1][0] + left, -pos[1][1] + waterLevel,
+      pos[2][0] + left, -pos[2][1] + waterLevel
+    ]);
+
+    if (c === curves - 1) {
+      waveRight = pos[2][0];
+    }
+  }
+
+  /**
+   * top-right corner
+   *
+   *                       ~~~~~~~~
+   * 3. draws this line -> |      | <- 1. draws this line
+   *                       +------+
+   *                          ^
+   *                          |
+   *                  2. draws this line
+   */
+  path.push([ 'L', waveRight + left, cy + radius ]);
+  path.push([ 'L', left, cy + radius ]);
+  path.push([ 'L', left, waterLevel ]);
+  return path;
+}
+
+console.log(getWaterWavePath);
+
 Shape.registerShape('interval', 'liquid-fill-gauge', {
   getPoints(pointInfo) {
     pointInfo.size = pointInfo.size * 2; // 漏斗图的 size 是柱状图的两倍
