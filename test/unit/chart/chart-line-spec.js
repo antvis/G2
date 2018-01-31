13e6d6cc2b681ce7075e1c60042e0b88d5f95926
dxq613 <dxq613@gmail.com>
2018-01-31 16:44:43 +0800

    feat(geom): support connect null values, #415

--- test/unit/chart/chart-line-spec.js
+++ test/unit/chart/chart-line-spec.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const Chart = require('../../../src/chart/chart');
-
+const Global = require('../../../src/global');
 const div = document.createElement('div');
 div.id = 'cchartl';
 document.body.appendChild(div);
@@ -59,6 +59,21 @@ describe('test line chart', function() {
     chart.changeData(newData);
     const group = chart.get('viewContainer').getFirst();
     expect(group.getCount()).equal(2);
+  });
+
+  it('line with null values and connectNulls', function() {
+    const newData = [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: null },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 }
+    ];
+    Global.connectNulls = true;
+    chart.changeData(newData);
+    const group = chart.get('viewContainer').getFirst();
+    expect(group.getCount()).equal(1);
+    Global.connectNulls = false;
   });
 
   it('change coord', function() {
