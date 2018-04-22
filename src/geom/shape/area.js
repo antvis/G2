7fce734bd923923e22815cf9104ddc350a882156
leungwensen <leungwensen@gmail.com>
2018-04-22 19:54:38 +0800

    fix(area): urgent fix of basic area plot

--- src/geom/shape/area.js
+++ src/geom/shape/area.js
@@ -36,14 +36,14 @@ function getPath(cfg, smooth, shape) {
   const topLinePoints = []; // area 区域上部分
   let bottomLinePoints = []; // area 区域下部分
   const isInCircle = cfg.isInCircle;
-  const hasStack = cfg.hasStack;
+  // const hasStack = cfg.hasStack;
   Util.each(cfg.points, function(point) {
     topLinePoints.push(point[1]);
     bottomLinePoints.push(point[0]);
   });
-  if (isInCircle || hasStack) {
-    bottomLinePoints = bottomLinePoints.reverse();
-  }
+  // if (!isInCircle) {
+  bottomLinePoints = bottomLinePoints.reverse();
+  // }
   pointsArr.push(topLinePoints, bottomLinePoints);
   Util.each(pointsArr, function(points, index) {
     let subPath = [];
@@ -57,7 +57,7 @@ function getPath(cfg, smooth, shape) {
     } else {
       subPath = PathUtil.getLinePath(points, false);
     }
-    if ((isInCircle || hasStack) && index > 0) {
+    if (index > 0) {
       subPath[0][0] = 'L';
     }
     path = path.concat(subPath);
