2d34d2f7280b3e827fa68e5cd1a5cc7edc075b4d
leungwensen <leungwensen@gmail.com>
2018-05-03 17:39:13 +0800

    chore(interact): start point

--- src/interaction/base.js
+++ src/interaction/base.js
@@ -1,6 +1,30 @@
+// const Global = require('../global');
+const Util = require('../util');
+
+const assign = Util.assign;
 
 class Interaction {
+  getDefaultCfg() {
+    return {
 
+    };
+  }
+
+  constructor(cfg) {
+    const me = this;
+    const defaultCfg = me.getDefaultCfg();
+    assign(me, defaultCfg, cfg);
+    me.init();
+  }
+
+  init() {
+  }
+
+  execute() {
+  }
+
+  clear() {
+  }
 }
 
 module.exports = Interaction;
--- src/interaction/index.js
+++ src/interaction/index.js
@@ -2,18 +2,54 @@
  * @fileOverview Interaction
  * @author leungwensen@gmail.com
  */
-// const Util = require('../util');
 const View = require('../chart/view');
 const G2 = require('../core.js');
+const Util = require('../util');
 
-const Interactions = {};
-
-View.prototype.interact = function(/* type, cfg */) {
-};
-
-G2.__interactions = {};
+G2.__Interactions = {};
 G2.registerInteraction = function(type, constructor) {
-  G2.__interactions[type] = constructor;
+  G2.__Interactions[type] = constructor;
+};
+G2.getInteraction = function(type) {
+  return G2.__Interactions[type];
 };
 
-module.exports = Interactions;
+View.prototype.getInteractions = function() {
+  const me = this;
+  if (!me.__interactions) {
+    me.__interactions = {};
+  }
+  return me.__interactions;
+};
+
+View.prototype.setInteraction = function(type, interact) {
+  const me = this;
+  const interactions = me.getInteractions();
+  interactions[type] = interactions[type] || [];
+  interactions[type].push(interact);
+};
+
+View.prototype.clearInteraction = function(type) {
+  const me = this;
+  const interactions = me.getInteractions();
+  if (type) {
+    (interactions[type] || []).forEach(interact => {
+      interact.clear();
+    });
+  } else {
+    Util.each(interactions, collection => {
+      (collection || []).forEach(interact => {
+        interact.clear();
+      });
+    });
+  }
+};
+View.prototype.interact = function(type, cfg) {
+  const me = this;
+  const Interaction = G2.getInteraction[type];
+  const interact = new Interaction(cfg);
+  interact.execute();
+  me.setInteraction(type, interact);
+};
+
+module.exports = G2.__interactions;
