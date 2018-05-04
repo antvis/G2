ee2d75a3a33b14ea4f8d33d32455650fc97a4047
zhfanrui <15398280@uni.massey.ac.nz>
2018-05-04 15:21:13 +0800

    fix(chart): fix downloadImage method in chart to fit IE 10+ and Firefox. close #617

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -545,9 +545,9 @@ class Chart extends View {
       const mime = arr[0].match(/:(.*?);/)[1];
       const bstr = atob(arr[1]);
       let n = bstr.length;
-      let u8arr = new Uint8Array(n);
+      const u8arr = new Uint8Array(n);
       while (n--) {
-        const u8arr[n] = bstr.charCodeAt(n);
+        u8arr[n] = bstr.charCodeAt(n);
       }
       const blobObj = new Blob([ u8arr ], { type: mime });
       if (window.navigator.msSaveBlob) {
