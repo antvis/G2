bdbb014544cfe6682ae10bcd15610db4a5cf6354
Elaine1234 <yilin.qyl@antfin.com>
2018-05-29 18:18:59 +0800

    fix: pass path array instead of manipulating context when defining Marker

--- src/geom/shape/schema.js
+++ src/geom/shape/schema.js
@@ -229,25 +229,27 @@ Shape.registerShape('schema', 'box', {
   },
   getMarkerCfg(cfg) {
     return {
-      symbol(x, y, r, ctx) {
+      symbol(x, y, r) {
         const yValues = [ y - 6, y - 3, y, y + 3, y + 6 ];
         const points = getBoxPoints(x, yValues, r);
-        ctx.moveTo(points[0].x + 1, points[0].y);
-        ctx.lineTo(points[1].x - 1, points[1].y);
-        ctx.moveTo(points[2].x, points[2].y);
-        ctx.lineTo(points[3].x, points[3].y);
-        ctx.moveTo(points[4].x, points[4].y);
-        ctx.lineTo(points[5].x, points[5].y);
-        ctx.lineTo(points[6].x, points[6].y);
-        ctx.lineTo(points[7].x, points[7].y);
-        ctx.lineTo(points[4].x, points[4].y);
-        ctx.closePath();
-        ctx.moveTo(points[8].x, points[8].y);
-        ctx.lineTo(points[9].x, points[9].y);
-        ctx.moveTo(points[10].x + 1, points[10].y);
-        ctx.lineTo(points[11].x - 1, points[11].y);
-        ctx.moveTo(points[12].x, points[12].y);
-        ctx.lineTo(points[13].x, points[13].y);
+        return [
+          [ 'M', points[0].x + 1, points[0].y ],
+          [ 'L', points[1].x - 1, points[1].y ],
+          [ 'M', points[2].x, points[2].y ],
+          [ 'L', points[3].x, points[3].y ],
+          [ 'M', points[4].x, points[4].y ],
+          [ 'L', points[5].x, points[5].y ],
+          [ 'L', points[6].x, points[6].y ],
+          [ 'L', points[7].x, points[7].y ],
+          [ 'L', points[4].x, points[4].y ],
+          [ 'Z' ],
+          [ 'M', points[8].x, points[8].y ],
+          [ 'L', points[9].x, points[9].y ],
+          [ 'M', points[10].x + 1, points[10].y ],
+          [ 'L', points[11].x - 1, points[11].y ],
+          [ 'M', points[12].x, points[12].y ],
+          [ 'L', points[13].x, points[13].y ]
+        ];
       },
       radius: 6,
       lineWidth: 1,
@@ -273,18 +275,20 @@ Shape.registerShape('schema', 'candle', {
   },
   getMarkerCfg(cfg) {
     return {
-      symbol(x, y, r, ctx) {
+      symbol(x, y, r) {
         y = [ y + 7.5, y + 3, y - 3, y - 7.5 ];
         const points = getCandlePoints(x, y, r);
-        ctx.moveTo(points[0].x, points[0].y);
-        ctx.lineTo(points[1].x, points[1].y);
-        ctx.moveTo(points[2].x, points[2].y);
-        ctx.lineTo(points[3].x, points[3].y);
-        ctx.lineTo(points[4].x, points[4].y);
-        ctx.lineTo(points[5].x, points[5].y);
-        ctx.closePath();
-        ctx.moveTo(points[6].x, points[6].y);
-        ctx.lineTo(points[7].x, points[7].y);
+        return [
+          [ 'M', points[0].x, points[0].y ],
+          [ 'L', points[1].x, points[1].y ],
+          [ 'M', points[2].x, points[2].y ],
+          [ 'L', points[3].x, points[3].y ],
+          [ 'L', points[4].x, points[4].y ],
+          [ 'L', points[5].x, points[5].y ],
+          [ 'Z' ],
+          [ 'M', points[6].x, points[6].y ],
+          [ 'L', points[7].x, points[7].y ]
+        ];
       },
       lineWidth: 1,
       stroke: cfg.color,
