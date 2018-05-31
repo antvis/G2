cf37a281cf63932fdf5b50df66d2a290952a4898
刘叶 <liuye@liuyedeMacBook-Pro.local>
2018-05-31 14:48:12 +0800

    modify spec code

--- test/unit/chart/controller/legend-spec.js
+++ test/unit/chart/controller/legend-spec.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const Chart = require('../../../../src/chart/chart');
-// const Line = require('../../../../src/geom/line');
+require('../../../../src/geom/line');
 const LegendController = require('../../../../src/chart/controller/legend');
 
 const div = document.createElement('div');
@@ -8,34 +8,34 @@ div.id = 'legendTest';
 document.body.appendChild(div);
 
 const data = [
-      { country: 'Asia', year: '1750', value: 502 },
-      { country: 'Asia', year: '1800', value: 635 },
-      { country: 'Asia', year: '1850', value: 809 },
-      { country: 'Asia', year: '1900', value: 947 },
-      { country: 'Asia', year: '1950', value: 1402 },
-      { country: 'Asia', year: '1999', value: 3634 },
-      { country: 'Asia', year: '2050', value: 5268 },
-      { country: 'Africa', year: '1750', value: 106 },
-      { country: 'Africa', year: '1800', value: 107 },
-      { country: 'Africa', year: '1850', value: 111 },
-      { country: 'Africa', year: '1900', value: 133 },
-      { country: 'Africa', year: '1950', value: 221 },
-      { country: 'Africa', year: '1999', value: 767 },
-      { country: 'Africa', year: '2050', value: 1766 },
-      { country: 'Europe', year: '1750', value: 163 },
-      { country: 'Europe', year: '1800', value: 203 },
-      { country: 'Europe', year: '1850', value: 276 },
-      { country: 'Europe', year: '1900', value: 408 },
-      { country: 'Europe', year: '1950', value: 547 },
-      { country: 'Europe', year: '1999', value: 729 },
-      { country: 'Europe', year: '2050', value: 628 },
-      { country: 'Oceania', year: '1750', value: 200 },
-      { country: 'Oceania', year: '1800', value: 200 },
-      { country: 'Oceania', year: '1850', value: 200 },
-      { country: 'Oceania', year: '1900', value: 300 },
-      { country: 'Oceania', year: '1950', value: 230 },
-      { country: 'Oceania', year: '1999', value: 300 },
-      { country: 'Oceania', year: '2050', value: 460 }
+  { country: 'Asia', year: '1750', value: 502 },
+  { country: 'Asia', year: '1800', value: 635 },
+  { country: 'Asia', year: '1850', value: 809 },
+  { country: 'Asia', year: '1900', value: 947 },
+  { country: 'Asia', year: '1950', value: 1402 },
+  { country: 'Asia', year: '1999', value: 3634 },
+  { country: 'Asia', year: '2050', value: 5268 },
+  { country: 'Africa', year: '1750', value: 106 },
+  { country: 'Africa', year: '1800', value: 107 },
+  { country: 'Africa', year: '1850', value: 111 },
+  { country: 'Africa', year: '1900', value: 133 },
+  { country: 'Africa', year: '1950', value: 221 },
+  { country: 'Africa', year: '1999', value: 767 },
+  { country: 'Africa', year: '2050', value: 1766 },
+  { country: 'Europe', year: '1750', value: 163 },
+  { country: 'Europe', year: '1800', value: 203 },
+  { country: 'Europe', year: '1850', value: 276 },
+  { country: 'Europe', year: '1900', value: 408 },
+  { country: 'Europe', year: '1950', value: 547 },
+  { country: 'Europe', year: '1999', value: 729 },
+  { country: 'Europe', year: '2050', value: 628 },
+  { country: 'Oceania', year: '1750', value: 200 },
+  { country: 'Oceania', year: '1800', value: 200 },
+  { country: 'Oceania', year: '1850', value: 200 },
+  { country: 'Oceania', year: '1900', value: 300 },
+  { country: 'Oceania', year: '1950', value: 230 },
+  { country: 'Oceania', year: '1999', value: 300 },
+  { country: 'Oceania', year: '2050', value: 460 }
 ];
 
 const chartWidth = 400;
