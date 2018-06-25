706a4fb3fe3a82e2f7e869490d154fed6ed63641
leungwensen <leungwensen@gmail.com>
2018-06-25 19:56:55 +0800

    3.2.0-beta.5

--- src/global.js
+++ src/global.js
@@ -7,7 +7,7 @@ const Theme = require('./theme/index');
 
 // const Global = {};
 const Global = {
-  version: '3.2.0-beta.4',
+  version: '3.2.0-beta.5',
   renderer2d: 'canvas',
   // renderer2d: 'svg',
   trackable: true,
