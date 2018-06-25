a06878615373f0da82321dc8fa11b3edb87dd63f
leungwensen <leungwensen@gmail.com>
2018-06-25 19:55:55 +0800

    bugfix(util): DomUtil compatible with older version

--- src/renderer2d.js
+++ src/renderer2d.js
@@ -1,6 +1,7 @@
 
 const G = require('@antv/g');
 const Global = require('./global');
+const Util = require('./util');
 
 let renderer = G.canvas;
 
@@ -8,5 +9,7 @@ let renderer = G.canvas;
 if (Global.renderer2d === 'svg') {
   renderer = G.svg;
 }
+
+Util.mix(renderer, G);
 
 module.exports = renderer;
