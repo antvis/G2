31387db2873e36645d9031fccc88c8ec567f63c8
刘叶 <liuye@liuyedeMacBook-Pro.local>
2018-07-03 20:11:20 +0800

    add components features

--- src/component/legend/tail.js
+++ src/component/legend/tail.js
@@ -4,7 +4,8 @@
  */
 
 const Util = require('../../util');
-const Category = require('./category');
+const Category = require('./Category');
+const Global = require('../../global');
 
 class Tail extends Category {
   getDefaultCfg() {
@@ -25,58 +26,70 @@ class Tail extends Category {
     });
   }
 
-  _addItemMarker(item, itemGroup) {
+  _addItem(item) {
+    const itemsGroup = this.get('itemsGroup');
+    const x = this._getNextX();
+    const y = 0;
     const unCheckColor = this.get('unCheckColor');
-    const markerAttrs = Util.mix({}, item.marker, {
-      x: item.marker.radius,
-      y: 0
+    const itemGroup = itemsGroup.addGroup({
+      x: 0,
+      y: 0,
+      value: item.value,
+      scaleValue: item.scaleValue,
+      checked: item.checked
     });
+    itemGroup.translate(x, y);
+    itemGroup.set('viewId', itemsGroup.get('viewId'));
 
-    if (!item.checked) {
-      if (markerAttrs.fill) {
-        markerAttrs.fill = unCheckColor;
+    const textStyle = this.get('textStyle');
+    const wordSpace = this.get('_wordSpaceing');
+    let startX = 0;
+
+    if (item.marker) { // 如果有marker添加marker
+      const markerAttrs = Util.mix({}, item.marker, {
+        x: item.marker.radius,
+        y: 0
+      });
+
+      if (!item.checked) {
+        if (markerAttrs.fill) {
+          markerAttrs.fill = unCheckColor;
+        }
+        if (markerAttrs.stroke) {
+          markerAttrs.stroke = unCheckColor;
+        }
       }
-      if (markerAttrs.stroke) {
-        markerAttrs.stroke = unCheckColor;
-      }
+
+      const markerShape = itemGroup.addShape('marker', {
+        type: 'marker',
+        attrs: markerAttrs
+      });
+      markerShape.attr('cursor', 'pointer');
+      markerShape.name = 'legend-marker';
+      startX += markerShape.getBBox().width + wordSpace;
     }
 
-    const markerShape = itemGroup.addShape('marker', {
-      type: 'marker',
-      attrs: markerAttrs
-    });
-    markerShape.attr('cursor', 'pointer');
-    markerShape.name = 'legend-marker';
-
-    return markerShape;
-  }
-
-  _addItemText(item, itemGroup, startX) {
-    const self = this;
-    const unCheckColor = self.get('unCheckColor');
-    const textStyle = self.get('textStyle');
     const textAttrs = Util.mix({}, textStyle, {
       x: startX,
       y: 0,
-      text: self._formatItemValue(item.value)
+      text: this._formatItemValue(item.value)
     });
     if (!item.checked) {
       Util.mix(textAttrs, {
         fill: unCheckColor
       });
     }
+
     const textShape = itemGroup.addShape('text', {
       attrs: textAttrs
     });
     textShape.attr('cursor', 'pointer');
     textShape.name = 'legend-text';
-    self.get('appendInfo') && textShape.setSilent('appendInfo', this.get('appendInfo'));
-  }
+    this.get('appendInfo') && textShape.setSilent('appendInfo', this.get('appendInfo'));
 
-  _addItemWrapper(item, itemGroup, x, y) {
-    const self = this;
+    // 添加一个包围矩形，用于事件支持
     const bbox = itemGroup.getBBox();
-    const itemWidth = self.get('itemWidth');
+    const itemWidth = this.get('itemWidth');
     const wrapperShape = itemGroup.addShape('rect', {
       attrs: {
         x,
@@ -91,33 +104,6 @@ class Tail extends Category {
     wrapperShape.setSilent('origin', item); // 保存图例项相关的数据，便于事件操作
     wrapperShape.name = 'legend-item';
     this.get('appendInfo') && wrapperShape.setSilent('appendInfo', this.get('appendInfo'));
-  }
-
-  _addItem(item) {
-    const self = this;
-    const itemsGroup = self.get('itemsGroup');
-    const x = 0;
-    const y = 0;
-    const itemGroup = itemsGroup.addGroup({
-      x,
-      y,
-      value: item.value,
-      scaleValue: item.scaleValue,
-      checked: item.checked
-    });
-    itemGroup.translate(x, y);
-    itemGroup.setSilent('viewId', itemsGroup.get('viewId'));
-    const wordSpace = this.get('_wordSpaceing');
-    let startX = 0;
-    // 如果有marker添加marker
-    if (item.marker) {
-      const markerShape = self._addItemMarker(item, itemGroup);
-      startX += markerShape.getBBox().width + wordSpace;
-    }
-    // text
-    self._addItemText(item, itemGroup, startX);
-    // 添加一个包围矩形，用于事件支持
-    self._addItemWrapper(item, itemGroup, x, y);
     itemGroup.name = 'legendGroup';
     return itemGroup;
   }
@@ -138,9 +124,14 @@ class Tail extends Category {
         }
         const groupHeight = groupItem.getBBox().height;
         const x = groupItem.get('x');
-        groupItem.translate(x, lastY - groupHeight);
+        const y = lastY - (groupHeight / 2);
+
+        // groupItem.set('y', lastY - groupHeight);
+        // adjust y position by previous
+        groupItem.translate(x, y);
         index++;
       });
+      self._collisionDetection(groups);
     }
   }
 
@@ -157,6 +148,48 @@ class Tail extends Category {
     const chart = self.get('chart');
     chart.once('afterpaint', function() {
       self._adjust();
+    });
+  }
+
+  _getPreviousY(item) {
+    const y = item.attr('matrix')[7];
+    const height = item.getBBox().height;
+    return y + height;
+  }
+
+  _collisionDetection(items) {
+    const group = this.get('itemsGroup');
+    items.sort(function(a, b) {
+      const ay = a.attr('matrix')[7];
+      const by = b.attr('matrix')[7];
+      return by - ay;
+    });
+    for (let i = 1; i < items.length; i++) {
+      const previous = items[i - 1];
+      const previous_y = previous.attr('matrix')[7] - previous.getBBox().height;
+      const current = items[i];
+      const current_y = current.attr('matrix')[7];
+      if (current_y >= previous_y && current.get('value')) { // collision
+        const target = previous_y - 20;
+        const gap = current_y - target;
+        current.translate(0, gap);
+        this._adjustDenote(group, current.attr('matrix')[7], current_y);
+      }
+    }
+  }
+
+  _adjustDenote(group, start, end) {
+    const margin = Global.legend.legendMargin - 2;
+    const x0 = -2;
+    const x1 = -margin;
+    const x2 = -margin * 2;
+    group.addShape('path', {
+      attrs: {
+        path: 'M' + x0 + ',' + start + 'L' + x1 + ',' + (end + 3) + 'L' + x2 + ',' + (end + 3),
+        lineWidth: 1,
+        lineDash: [ 2, 2 ],
+        stroke: '#999999'
+      }
     });
   }
 
