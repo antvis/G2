56d0a082f6d7bdfff640de5c1321480391da3d54
Elaine1234 <yilin.qyl@antfin.com>
2018-09-17 14:31:24 +0800

    feat: add label type: interval

--- src/geom/label/index.js
+++ src/geom/label/index.js
@@ -4,13 +4,13 @@ const PieLabels = require('./pie-labels');
 const IntervalLabels = require('./interval-labels');
 
 const Labels = {
-  getLabelsClass(coordType) {
+  getLabelsClass(coordType, type) {
     let rst = GeomLabels;
     if (coordType === 'polar') {
       rst = PolarLabels;
     } else if (coordType === 'theta') { // pie chart
       rst = PieLabels;
-    } else if (coordType === 'interval') { // bar
+    } else if (type === 'interval' || type === 'polygon') { // bar
       rst = IntervalLabels;
     }
     return rst;
