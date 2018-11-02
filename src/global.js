5e092d683b6e034de4fdec542fbb1c9514dcd62c
Elaine1234 <yilin.qyl@antfin.com>
2018-11-02 18:09:59 +0800

    chore(package): update package version

--- src/global.js
+++ src/global.js
@@ -6,7 +6,7 @@ const Util = require('./util');
 const Theme = require('./theme/index');
 
 const Global = {
-  version: '3.3.3',
+  version: '3.3.4',
   renderer: 'canvas',
   trackable: true,
   trackingInfo: {},
