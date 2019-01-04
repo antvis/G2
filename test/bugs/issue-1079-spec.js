005aa221427255ae5dc538d2d8b2ecb77852cfaf
leungwensen <leungwensen@gmail.com>
2019-01-04 16:43:29 +0800

    fix: applyMatrix when calculating bbox of axis title

--- test/bugs/issue-1079-spec.js
+++ test/bugs/issue-1079-spec.js
@@ -2,10 +2,10 @@ const G2 = require('../../src/index');
 const plotRange2BBox = require('../../src/chart/util/plot-range2bbox');
 const expect = require('chai').expect;
 
-describe('#1079', () => {
+describe.only('#1079', () => {
   const div = document.createElement('div');
   document.body.appendChild(div);
-  it('error on legend for shape', () => {
+  it('result of auto padding is passable when title is too long', () => {
     const data = [
       { genre: 'Sports', sold: 275 },
       { genre: 'Strategy', sold: 115 },
