1186bf18f9b7ba824107a0ddafb6499d55188876
leungwensen <leungwensen@gmail.com>
2019-02-22 17:07:52 +0800

    3.5.0-beta.3

--- src/global.js
+++ src/global.js
@@ -6,7 +6,7 @@ const Util = require('./util');
 const Theme = require('./theme/index');
 
 const Global = {
-  version: '3.5.0-beta.2',
+  version: '3.5.0-beta.3',
   renderer: 'canvas',
   // trackable: false,
   trackingInfo: {},
