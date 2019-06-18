3fc10032d21863aba11a7c9f2412fc2ecbdf24de
dxq613 <dxq613@gmail.com>
2019-06-18 16:56:47 +0800

    feat(bug): fixed bug of #1186

--- src/chart/util/bbox-of-back-plot.js
+++ src/chart/util/bbox-of-back-plot.js
@@ -36,24 +36,30 @@ function getTitleBBox(title) {
 
 module.exports = function BBoxOfBackPlot(backPlot, defaultBBox) {
   let bbox = defaultBBox;
-  Util.each(backPlot.get('children'), element => {
-    if ((element instanceof G.Group && element.get('children').length) || element instanceof G.Path) {
-      bbox = mergeBBox(bbox, element.getBBox());
-    } else if (element instanceof G.Text) { // title
-      const elementBBox = getTitleBBox(element);
-      const dx = Math.abs(elementBBox.maxX - elementBBox.minX);
-      const dy = Math.abs(elementBBox.maxY - elementBBox.minY);
-      if (dx < dy) {
-        bbox = mergeBBox(bbox, Util.mix(bbox, {
-          minX: Math.min(bbox.minX, elementBBox.minX),
-          maxX: Math.max(bbox.maxX, elementBBox.maxX)
-        }));
-      } else {
-        bbox = mergeBBox(bbox, Util.mix(bbox, {
-          minY: Math.min(bbox.minY, elementBBox.minY),
-          maxY: Math.max(bbox.maxY, elementBBox.maxY)
-        }));
-      }
+  Util.each(backPlot.get('children'), group => {
+    // 这段代码假设了子元素是 axis，同时 title 超出长度，
+    // 这种临时代码需要在 4.0 中修复掉
+    if (group instanceof G.Group) {
+      Util.each(group.get('children'), element => {
+        if ((element instanceof G.Group && element.get('children').length) || element instanceof G.Path) {
+          bbox = mergeBBox(bbox, element.getBBox());
+        } else if (element instanceof G.Text) { // title
+          const elementBBox = getTitleBBox(element);
+          const dx = Math.abs(elementBBox.maxX - elementBBox.minX);
+          const dy = Math.abs(elementBBox.maxY - elementBBox.minY);
+          if (dx < dy) {
+            bbox = mergeBBox(bbox, Util.mix(bbox, {
+              minX: Math.min(bbox.minX, elementBBox.minX),
+              maxX: Math.max(bbox.maxX, elementBBox.maxX)
+            }));
+          } else {
+            bbox = mergeBBox(bbox, Util.mix(bbox, {
+              minY: Math.min(bbox.minY, elementBBox.minY),
+              maxY: Math.max(bbox.maxY, elementBBox.maxY)
+            }));
+          }
+        }
+      });
     }
   });
   return bbox;
