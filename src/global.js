17dc445cfa50a6fb2214fa5788e70ebc8db7588a
paleface001 <mipha.ly@antfin.com>
2019-09-05 10:48:37 +0800

    chore(): 版本号修改

--- src/global.js
+++ src/global.js
@@ -6,7 +6,7 @@ const Util = require('./util');
 const Theme = require('./theme/index');
 
 const Global = {
-  version: '3.5.8-beta.1',
+  version: '3.5.8-beta.3',
   renderer: 'canvas',
   // trackable: false,
   trackingInfo: {},
