452af0550cd1acf7bfbbe5d5189d3fae04d1f188
hustcc <i@hust.cc>
2019-09-05 14:25:37 +0800

    test(torch): use torch as unit test framework

--- __tests__/unit/template-spec.ts
+++ __tests__/unit/template-spec.ts
@@ -1,10 +1,12 @@
+import { expect } from 'chai';
+
 describe('template', () => {
   const el = document.createElement('div');
   el.id = 'test-div-id';
   el.innerHTML = 'hello g2';
   document.querySelector('body').appendChild(el);
 
-  test('exports', () => {
-    expect(document.querySelector('body div').innerHTML).toBe('hello g2');
+  it('div content', () => {
+    expect(document.querySelector('#test-div-id').innerHTML).to.equals('hello g2');
   });
 });
