5887c54566ee59ed5d820d6f0db81cfc52cb13cc
simaQ <sima.zhang1990@gmail.com>
2019-09-12 16:39:45 +0800

    feat: add Polygon geometry

---
+++ src/geometry/polygon/index.ts
@@ -0,0 +1,45 @@
+import * as Util from '@antv/util';
+
+import Geometry from '../geometry';
+import './shape';
+
+export default class Polygon extends Geometry {
+  public type = 'polygon';
+  public shapeType = 'polygon';
+  public generatePoints = true;
+
+  protected createShapePointsCfg(obj: object) {
+    const cfg: any = super.createShapePointsCfg(obj);
+    let x = cfg.x;
+    let y = cfg.y;
+    let temp;
+    // x y 都是数组时，不做处理
+    if (!(Util.isArray(x) && Util.isArray(y))) {
+      const xScale = this.getXScale();
+      const yScale = this.getYScale();
+      const xCount = xScale.values.length;
+      const yCount = yScale.values.length;
+      const xOffset = (0.5 * 1) / xCount;
+      const yOffset = (0.5 * 1) / yCount;
+
+      if (xScale.isCategory && yScale.isCategory) {
+        // 如果x,y都是分类
+        x = [x - xOffset, x - xOffset, x + xOffset, x + xOffset];
+        y = [y - yOffset, y + yOffset, y + yOffset, y - yOffset];
+      } else if (Util.isArray(x)) {
+        // x 是数组
+        temp = x;
+        x = [temp[0], temp[0], temp[1], temp[1]];
+        y = [y - yOffset / 2, y + yOffset / 2, y + yOffset / 2, y - yOffset / 2];
+      } else if (Util.isArray(y)) {
+        // y 是数组
+        temp = y;
+        y = [temp[0], temp[1], temp[1], temp[0]];
+        x = [x - xOffset / 2, x - xOffset / 2, x + xOffset / 2, x + xOffset / 2];
+      }
+      cfg.x = x;
+      cfg.y = y;
+    }
+    return cfg;
+  }
+}
