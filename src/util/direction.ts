e8b122ff79e29fe0459976c027600f4e4427f0fd
hustcc <i@hust.cc>
2019-09-18 11:39:41 +0800

    test(util): add unit tests for util/direction

--- src/util/direction.ts
+++ src/util/direction.ts
@@ -10,43 +10,29 @@ import { Position } from '../interface';
  */
 export function directionToPosition(parentBBox: BBox, bbox: BBox, direction: DIRECTION): Position {
   if (direction === DIRECTION.TOP) {
-    return [parentBBox.width / 2 - bbox.width / 2 + parentBBox.minX, parentBBox.minY];
+    return [parentBBox.minX + parentBBox.width / 2 - bbox.width / 2, parentBBox.minY];
   }
-  if (direction === DIRECTION.TOP_LEFT) {
-    return [0, 0];
-  }
-  if (direction === DIRECTION.TOP_RIGHT) {
-    return [0, 0];
-  }
-
   if (direction === DIRECTION.BOTTOM) {
-    return [parentBBox.width / 2 - bbox.width / 2, parentBBox.maxY - bbox.height];
+    return [parentBBox.minX + parentBBox.width / 2 - bbox.width / 2, parentBBox.maxY - bbox.height];
   }
-  if (direction === DIRECTION.BOTTOM_LEFT) {
-    return [0, 0];
-  }
-  if (direction === DIRECTION.BOTTOM_RIGHT) {
-    return [0, 0];
-  }
-
   if (direction === DIRECTION.LEFT) {
-    return [parentBBox.minX, parentBBox.height / 2 - bbox.height / 2];
+    return [parentBBox.minX, parentBBox.minY + parentBBox.height / 2 - bbox.height / 2];
   }
-  if (direction === DIRECTION.LEFT_TOP) {
-    return [0, 0];
-  }
-  if (direction === DIRECTION.LEFT_BOTTOM) {
-    return [0, 0];
+  if (direction === DIRECTION.RIGHT) {
+    return [parentBBox.maxX - bbox.width, parentBBox.minY + parentBBox.height / 2 - bbox.height / 2];
   }
 
-  if (direction === DIRECTION.RIGHT) {
-    return [parentBBox.maxX - bbox.width, parentBBox.height / 2 - bbox.height / 2 + parentBBox.minX];
+  if (direction === DIRECTION.TOP_LEFT || direction === DIRECTION.LEFT_TOP) {
+    return [parentBBox.tl.x, parentBBox.tl.y];
   }
-  if (direction === DIRECTION.RIGHT_TOP) {
-    return [0, 0];
+  if (direction === DIRECTION.TOP_RIGHT || direction === DIRECTION.RIGHT_TOP) {
+    return [parentBBox.tr.x - bbox.width, parentBBox.tr.y];
   }
-  if (direction === DIRECTION.RIGHT_BOTTOM) {
-    return [0, 0];
+  if (direction === DIRECTION.BOTTOM_LEFT || direction === DIRECTION.LEFT_BOTTOM) {
+    return [parentBBox.bl.x, parentBBox.bl.y - bbox.height];
+  }
+  if (direction === DIRECTION.BOTTOM_RIGHT || direction === DIRECTION.RIGHT_BOTTOM) {
+    return [parentBBox.br.x - bbox.width, parentBBox.br.y - bbox.height];
   }
 
   return [0, 0];
