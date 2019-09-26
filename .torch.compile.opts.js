69021cff91c50152f8ba61315365972024f5bae5
simaQ <sima.zhang1990@gmail.com>
2019-09-26 15:56:44 +0800

    chore: rename __tests__ folder to tests.

--- .torch.compile.opts.js
+++ .torch.compile.opts.js
@@ -4,7 +4,7 @@ module.exports = {
     sourceMaps: 'inline',
   },
   extensions: ['.es6', '.es', '.jsx', '.js', '.ts', '.tsx', '.css', '.svg'],
-  include: ['src/**', 'lib/**', '__tests__/**'],
+  include: ['src/**', 'lib/**', 'tests/**'],
   exclude: ['bower_components/**', 'node_modules/**'],
   tsconfig: require('./tsconfig.json'),
 };
