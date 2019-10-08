7b573d87e4e23d476f60adf305c44c60dc147db4
simaQ <sima.zhang1990@gmail.com>
2019-10-08 18:05:37 +0800

    chore: import ScaleConfig interface from @antv/scale.

--- src/dependents.ts
+++ src/dependents.ts
@@ -1,8 +1,8 @@
 /* 依赖的模块，在这里统一引入，方便打包优化 */
 
 // 只引入框架，不引入内容
-export { registerScale, getScale, Scale } from '@antv/scale/lib/factory';
 export { registerAdjust, getAdjust, Adjust } from '@antv/adjust/lib/factory';
 // coordinate 全部引入即可
 export { getCoordinate, registerCoordinate, Coordinate, CoordinateCfg } from '@antv/coord';
 export { getAttribute, Attribute, colorUtil } from '@antv/attr/lib/factory';
+export { getScale, registerScale, Scale, ScaleConfig } from '@antv/scale';
