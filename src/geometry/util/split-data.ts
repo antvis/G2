7a1302cfa20fa573772ac23162adbdf8b36e5520
simaQ <sima.zhang1990@gmail.com>
2019-10-12 15:22:28 +0800

    refactor: refactor splitData().

--- src/geometry/util/split-data.ts
+++ src/geometry/util/split-data.ts
@@ -2,45 +2,53 @@ import * as _ from '@antv/util';
 import { FIELD_ORIGIN } from '../../constant';
 import { Data, Datum } from '../../interface';
 
+function isValueNil(value) {
+  if ((_.isArray(value) && _.isNil(value[0])) || _.isNil(value)) {
+    return true;
+  }
+
+  return false;
+}
+
 /**
  * 分割数据，用于处理在一组数据中，field 对应的数值存在 null/undefined 的情况
  * 应用于折线图、区域图以及路径图
  * @example
- * // return [{x: 1, y: 2}, {x: 3, y: 3}]
- * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], true, 'y');
+ * // return [[{x: 1, y: 2}, {x: 3, y: 3}]]
+ * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], 'y', true);
  * @example
  * // return [[{x: 1, y: 2}], [{x: 3, y: 3}]]
- * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], false, 'y');
+ * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], 'y', false);
  *
- * @param data 要进行分割的数据
- * @param connectNulls 是否连接空值数据
+ * @param data 要进行处理的数据
  * @param field 判断空值的字段名
- * @returns data 返回处理的结果
+ * @param connectNulls 是否连接空值数据
  */
-export function splitData(data: Data, connectNulls?: boolean, field?: string) {
+export function splitData(data: Data, field: string, connectNulls?: boolean) {
   if (!data.length) {
     return [];
   }
 
+  if (connectNulls) {
+    // 即忽略 field 对应值为 null 的场景
+    const filtered = _.filter(data, (obj: Datum) => {
+      const yValue = _.get(obj, [FIELD_ORIGIN, field], obj[field]);
+      return !isValueNil(yValue);
+    });
+    return [filtered];
+  }
+
   const result = [];
   let tmp = [];
-  let yValue;
   data.forEach((obj: Datum) => {
-    yValue = _.get(obj, [FIELD_ORIGIN, field], obj[field]);
-    if (connectNulls) {
-      // 如果忽视 null 直接连接节点，则将 value = null 的数据过滤掉
-      if (!_.isNil(yValue)) {
-        tmp.push(obj);
+    const yValue = _.get(obj, [FIELD_ORIGIN, field], obj[field]);
+    if (isValueNil(yValue)) {
+      if (tmp.length) {
+        result.push(tmp);
+        tmp = [];
       }
     } else {
-      if ((_.isArray(yValue) && _.isNil(yValue[0])) || _.isNil(yValue)) {
-        if (tmp.length) {
-          result.push(tmp);
-          tmp = [];
-        }
-      } else {
-        tmp.push(obj);
-      }
+      tmp.push(obj);
     }
   });
 
--- tests/unit/geometry/util/split-data-spec.ts
+++ tests/unit/geometry/util/split-data-spec.ts
@@ -2,12 +2,12 @@ import { splitData } from '../../../../src/geometry/util/split-data';
 
 describe('splitData', () => {
   test('data is empty', () => {
-    expect(splitData([]).length).toBe(0);
+    expect(splitData([], 'y').length).toBe(0);
   });
 
   test('connectNulls is true', () => {
     const data = [{ x: 1, y: 10 }, { x: 2, y: null }, { x: 4, y: 90 }, { x: 8, y: 12 }, { x: 9, y: 34 }];
-    const result = splitData(data, true, 'y');
+    const result = splitData(data, 'y', true);
     expect(result).toEqual([[{ x: 1, y: 10 }, { x: 4, y: 90 }, { x: 8, y: 12 }, { x: 9, y: 34 }]]);
   });
 
@@ -19,7 +19,7 @@ describe('splitData', () => {
       { x: 8, y: 12, _origin: { x: 8, y: 12 } },
       { x: 9, y: 34, _origin: { x: 9, y: 34 } },
     ];
-    const result = splitData(data, false, 'y');
+    const result = splitData(data, 'y', false);
     expect(result).toEqual([
       [{ x: 1, y: 10, _origin: { x: 1, y: 10 } }],
       [{ x: 8, y: 12, _origin: { x: 8, y: 12 } }, { x: 9, y: 34, _origin: { x: 9, y: 34 } }],
