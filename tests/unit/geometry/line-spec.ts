91365b4d2f4e8d68904a9a650f832f4ca9166940
simaQ <sima.zhang1990@gmail.com>
2019-10-14 16:55:49 +0800

    feat: add Line Geometry.

---
+++ tests/unit/geometry/line-spec.ts
@@ -0,0 +1,46 @@
+import { getCoordinate } from '@antv/coord';
+import Line from '../../../src/geometry/line';
+import { createCanvas, createDiv, removeDom } from '../../util/dom';
+import Theme from '../../util/theme';
+
+import 'jest-extended';
+
+const CartesianCoordinate = getCoordinate('rect');
+
+describe('Line', () => {
+  const div = createDiv();
+  const canvas = createCanvas({
+    container: div,
+    width: 300,
+    height: 300,
+  });
+  const rectCoord = new CartesianCoordinate({
+    start: { x: 0, y: 300 },
+    end: { x: 300, y: 0 },
+  });
+
+  it('Data should be sorted.', () => {
+    const line = new Line({
+      data: [{ x: 1996, y: 30 }, { x: 1990, y: 210 }, { x: 1993, y: 29 }],
+      container: canvas.addGroup(),
+      theme: Theme,
+      coordinate: rectCoord,
+    });
+
+    line.position('x*y');
+    line.initial();
+    line.paint();
+    canvas.draw();
+
+    expect(line.shapeType).toBe('line');
+    expect(line.sortable).toBe(true);
+
+    const element = line.elements[0];
+    expect(element.data).toEqual([{ x: 1990, y: 210 }, { x: 1993, y: 29 }, { x: 1996, y: 30 }]);
+  });
+
+  afterAll(() => {
+    canvas.destroy();
+    removeDom(div);
+  });
+});
