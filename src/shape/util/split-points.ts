3bd1cfc324f520872f3a55efb8fc1778ccd8de62
simaQ <sima.zhang1990@gmail.com>
2019-10-15 16:40:14 +0800

    chore: better splitPoints()

--- src/shape/util/split-points.ts
+++ src/shape/util/split-points.ts
@@ -2,8 +2,10 @@ import * as _ from '@antv/util';
 import { Point, ShapePoint } from '../../interface';
 
 export function splitPoints(obj: ShapePoint): Point[] {
-  const x = _.isArray(obj.x) ? obj.x : [obj.x];
-  const y = _.isArray(obj.y) ? obj.y : [obj.y];
+  // y 有可能是数组，对应原始数据中 y 为一个区间数据，如 [19, 30]，为了统一也将 x 转换为数组
+  let { x, y } = obj;
+  x = _.isArray(x) ? x : [x];
+  y = _.isArray(y) ? x : [y];
 
   return y.map((eachY, index) => {
     return {
