4e2d7f3d88318b5d66c7efc7dc861bc0f83a73b6
simaQ <sima.zhang1990@gmail.com>
2019-10-17 14:58:40 +0800

    feat: add animate module.

---
+++ src/geometry/animate/action.ts
@@ -0,0 +1,77 @@
+import { transform } from '@antv/matrix-util';
+import { ShapeDrawCFG } from 'interface';
+import { IGroup, IShape } from '../../dependents';
+/**
+ * 垂直方向的缩放动画
+ * @param shape
+ * @param animateCfg
+ * @param shapeModel
+ */
+export function scaleInY(shape: IShape | IGroup, animateCfg, shapeModel: ShapeDrawCFG) {
+  const box = shape.getBBox();
+  const x = (box.minX + box.maxX) / 2;
+  const { points } = shapeModel;
+  // 数值如果为负值，那么应该从上往下生长
+  const y = points[0].y - points[1].y <= 0 ? box.maxY : box.minY;
+
+  const v = [x, y, 1];
+  shape.applyToMatrix(v);
+
+  const matrix = transform(shape.getMatrix(), [['t', -x, -y], ['s', 1, 0.01], ['t', x, y]]);
+  shape.setMatrix(matrix);
+
+  const endState = {
+    matrix: transform(shape.getMatrix(), [['t', -x, -y], ['s', 1, 100], ['t', x, y]]),
+  };
+
+  shape.animate(endState, animateCfg);
+}
+
+/**
+ * x 方向的缩放动画
+ * @param shape
+ * @param animateCfg
+ * @param shapeModel
+ */
+export function scaleInX(shape: IShape | IGroup, animateCfg, shapeModel: ShapeDrawCFG) {
+  const box = shape.getBBox();
+  const { points } = shapeModel;
+  // x 数值如果为负值，那么应该从右往左生长
+  const x = points[0].y - points[1].y > 0 ? box.maxX : box.minX;
+  const y = (box.minY + box.maxY) / 2;
+
+  const v = [x, y, 1];
+  shape.applyToMatrix(v);
+
+  const matrix = transform(shape.getMatrix(), [['t', -x, -y], ['s', 0.01, 1], ['t', x, y]]);
+  shape.setMatrix(matrix);
+
+  const endState = {
+    matrix: transform(shape.getMatrix(), [['t', -x, -y], ['s', 100, 1], ['t', x, y]]),
+  };
+
+  shape.animate(endState, animateCfg);
+}
+
+/**
+ * 渐隐动画
+ * @param shape
+ * @param animateCfg
+ * @param shapeModel
+ */
+export function fadeOut(shape: IShape | IGroup, animateCfg, shapeModel: ShapeDrawCFG) {
+  const endState = {
+    fillOpacity: 0,
+    strokeOpacity: 0,
+  };
+  const { easing, duration, delay, callback } = animateCfg;
+  shape.animate(
+    endState,
+    duration,
+    easing,
+    () => {
+      shape.remove(true);
+    },
+    delay
+  );
+}
