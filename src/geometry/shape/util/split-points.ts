70a909e9e5e2289a88ae6e73f2b2b0b360949c85
simaQ <sima.zhang1990@gmail.com>
2019-10-17 16:33:07 +0800

    fix: fix error variables.

--- src/geometry/shape/util/split-points.ts
+++ src/geometry/shape/util/split-points.ts
@@ -5,7 +5,7 @@ export function splitPoints(obj: ShapePoint): Point[] {
   // y 有可能是数组，对应原始数据中 y 为一个区间数据，如 [19, 30]，为了统一也将 x 转换为数组
   let { x, y } = obj;
   x = _.isArray(x) ? x : [x];
-  y = _.isArray(y) ? x : [y];
+  y = _.isArray(y) ? y : [y];
 
   return y.map((eachY, index) => {
     return {
