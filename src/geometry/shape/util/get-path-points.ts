c258e3a37a4c1fe17918b58a64b51f52d8b6cccd
simaQ <sima.zhang1990@gmail.com>
2019-10-22 14:33:12 +0800

    refactor: move splitData() to getPathPoints()

---
+++ src/geometry/shape/util/get-path-points.ts
@@ -0,0 +1,53 @@
+import * as _ from '@antv/util';
+import { FIELD_ORIGIN } from '../../../constant';
+import { Data, Datum, Point, RangePoint } from '../../../interface';
+
+function isValueNil(value) {
+  return (_.isArray(value) && _.isNil(value[0])) || _.isNil(value);
+}
+
+/**
+ * 分割数据，用于处理在一组数据中，field 对应的数值存在 null/undefined 的情况
+ * 应用于折线图、区域图以及路径图
+ * @example
+ * // return [[{x: 1, y: 2}, {x: 3, y: 3}]]
+ * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], 'y', true);
+ * @example
+ * // return [[{x: 1, y: 2}], [{x: 3, y: 3}]]
+ * splitData([{x: 1, y: 2}, {x: 2, y: null}, {x: 3, y: 3}], 'y', false);
+ *
+ * @param data 要进行处理的数据
+ * @param field 判断空值的字段名
+ * @param connectNulls 是否连接空值数据
+ */
+export function getPathPoints(points: Point[] | RangePoint[], connectNulls?: boolean) {
+  if (!points.length) {
+    return [];
+  }
+
+  if (connectNulls) {
+    // 即 y 值为空的场景
+    const filtered = _.filter(points, (point: Point) => {
+      return !isValueNil(point.y);
+    });
+    return [filtered];
+  }
+
+  const result = [];
+  let tmp = [];
+  points.forEach((point: Point) => {
+    if (isValueNil(point.y)) {
+      if (tmp.length) {
+        result.push(tmp);
+        tmp = [];
+      }
+    } else {
+      tmp.push(point);
+    }
+  });
+
+  if (tmp.length) {
+    result.push(tmp);
+  }
+  return result;
+}
