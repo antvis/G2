ba56bc15ca0d5ca8580dc5cdddad640760b29bdd
以昕 <huanian.chn@alibaba-inc.com>
2019-10-29 16:17:40 +0800

    fix: registerTheme ts 声明

--- src/index.d.ts
+++ src/index.d.ts
@@ -17,6 +17,7 @@ declare namespace G2 {
     setTheme(option: 'default' | 'dark'): void;
     setTheme(option: string): void;
     setTheme(option: object): void;
+    registerTheme(name: string, option: object): void;
     version: string;
     trackable: boolean;
     animate: boolean;
