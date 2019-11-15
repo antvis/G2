9ec67899c4af98577bbae367958c63fcd750d66f
simaQ <sima.zhang1990@gmail.com>
2019-11-15 17:33:08 +0800

    fix: should be PathCfg type

--- src/geometry/line.ts
+++ src/geometry/line.ts
@@ -1,12 +1,11 @@
-import { GeometryCfg } from './base';
-import Path from './path';
+import Path, { PathCfg } from './path';
 /** 引入 Path 对应的 ShapeFactory */
 import './shape/line';
 
 export default class Line extends Path {
   public type: string = 'line';
 
-  constructor(cfg: GeometryCfg) {
+  constructor(cfg: PathCfg) {
     super(cfg);
 
     const { sortable = true } = cfg; // Line 默认会对数据按照 x 轴字段进行排序
