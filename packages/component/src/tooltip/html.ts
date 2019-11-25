d00537525af85481aa534f93b28593ede7635bab
paleface001 <mipha.ly@antfin.com>
2019-11-25 23:06:26 +0800

    fix: rect crosshair render group

--- packages/component/src/tooltip/html.ts
+++ packages/component/src/tooltip/html.ts
@@ -71,7 +71,7 @@ export default class HtmlTooltip extends Tooltip<TooltipCfg> {
     // crosshair
     const crosshair = this.get('crosshairs');
     if (crosshair) {
-      const plot = this.get('frontgroundGroup');
+      const plot = crosshair.type !== 'rect' ? this.get('frontgroundGroup') : this.get('backgroundGroup');
       const crosshairGroup = new Crosshair(
         Util.mix(
           {
