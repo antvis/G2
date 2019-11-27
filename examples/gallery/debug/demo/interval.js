58a0b56edf195da38dfa1f68c429a1047246c82f
simaQ <sima.zhang1990@gmail.com>
2019-11-27 15:22:32 +0800

    chore: add bug demo for g debug

--- examples/gallery/debug/demo/interval.js
+++ examples/gallery/debug/demo/interval.js
@@ -1,34 +1,22 @@
 import { Chart } from '@ant/g2';
 
 const data = [
-  { name: 'London', 月份: 'Jan.', 月均降雨量: -18.9 },
-  { name: 'London', 月份: 'Feb.', 月均降雨量: -8.8 },
-  { name: 'London', 月份: 'Mar.', 月均降雨量: 39.3 },
-  { name: 'London', 月份: 'May', 月均降雨量: 47 },
-  { name: 'London', 月份: 'Jun.', 月均降雨量: 20.3 },
-  { name: 'London', 月份: 'Jul.', 月均降雨量: -24 },
-  { name: 'London', 月份: 'Aug.', 月均降雨量: -35.6 },
-  { name: 'Berlin', 月份: 'Jan.', 月均降雨量: 12.4 },
-  { name: 'Berlin', 月份: 'Feb.', 月均降雨量: 23.2 },
-  { name: 'Berlin', 月份: 'Mar.', 月均降雨量: 34.5 },
-  { name: 'Berlin', 月份: 'Apr.', 月均降雨量: 99.7 },
-  { name: 'Berlin', 月份: 'May', 月均降雨量: 52.6 },
-  { name: 'Berlin', 月份: 'Jun.', 月均降雨量: 35.5 },
-  { name: 'Berlin', 月份: 'Jul.', 月均降雨量: 37.4 },
-  { name: 'Berlin', 月份: 'Aug.', 月均降雨量: 42.4 },
+  { name: 'London', 月份: 'Jan.', 月均降雨量: 18.9 },
+  { name: 'London', 月份: 'Feb.', 月均降雨量: 8.8 },
 ];
+
 const chart = new Chart({
   container: 'container',
   autoFit: false,
-  height: 600,
-  width: 500,
+  height: 300,
+  width: 400,
 });
 chart.data(data);
-chart.interaction('active');
-// @ts-ignore
-chart
-  .interval()
-  .position('月份*月均降雨量')
-  .color('name')
-  .adjust('stack');
+chart.interval().position('月份*月均降雨量');
+
 chart.render();
+
+setTimeout(() => {
+  const newData = [{ name: 'London', 月份: 'Jan.', 月均降雨量: 50 }];
+  chart.changeData(newData);
+}, 1000);
