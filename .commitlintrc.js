25832182937c4fad4bf2d727b7a78fe8f4d68277
hustcc <i@hust.cc>
2019-11-27 20:53:33 +0800

    feat(changelog): add commit-msg and changelog generator

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'wip'],
+    ],
+  },
+};
