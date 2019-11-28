79047d48413efdaf1ec15354607f83aa5b4c2c0e
simaQ <sima.zhang1990@gmail.com>
2019-11-28 14:05:43 +0800

    fix: fix review

--- src/geometry/util/is-model-change.ts
+++ src/geometry/util/is-model-change.ts
@@ -8,15 +8,7 @@ import { ShapeInfo } from '../../interface';
  * @returns
  */
 export function isModelChange(currentModel: ShapeInfo, preModel: ShapeInfo) {
-  let result = false;
-  // 判断映射数据以及原始数据是否发生改变
-  const keys = ['color', 'shape', 'size', 'x', 'y', 'isInCircle', 'data', 'style'];
-  _.each(keys, (key) => {
-    if (!_.isEqual(currentModel[key], preModel[key])) {
-      result = true;
-      return false;
-    }
+  return _.some(['color', 'shape', 'size', 'x', 'y', 'isInCircle', 'data', 'style'], (key: string) => {
+    return !_.isEqual(currentModel[key], preModel[key]);
   });
-
-  return result;
 }
