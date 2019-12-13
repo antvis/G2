705cec8ac6e7e6fe2b52b1b50dc91dae1e0662ca
dxq613 <dxq613@gmail.com>
2019-12-13 19:49:40 +0800

    docs(interaction): add interaction

---
+++ examples/interaction/interaction/demo/range-highlight.js
@@ -0,0 +1,22 @@
+import { Chart } from '@antv/g2';
+
+const data = [
+  { year: '1991', value: 3 },
+  { year: '1992', value: 4 },
+  { year: '1993', value: 3.5 },
+  { year: '1994', value: 5 },
+  { year: '1995', value: 4.9 },
+];
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+  height: 500,
+});
+
+chart.data(data);
+chart.interaction('element-range-highlight');
+chart
+  .interval()
+  .position('year*value')
+  .color('year');
+chart.render();
