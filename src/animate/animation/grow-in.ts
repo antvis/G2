3fd3c8cb51561e3d8dddd95c721cd916d77d1349
simaQ <sima.zhang1990@gmail.com>
2019-12-19 17:40:34 +0800

    fix: grow-in 动画执行函数是直接对图形进行矩阵变换的动画，而非 clip

--- src/animate/animation/grow-in.ts
+++ src/animate/animation/grow-in.ts
@@ -1,39 +1,39 @@
 import { IGroup, IShape } from '../../dependents';
 import { AnimateCfg, AnimateExtraCfg } from '../interface';
-import { doClipScaleAnimate } from './util';
+import { doScaleAnimate } from './util';
 
 /**
- * 整体动画
- * 在整个坐标系范围内执行 x 轴方向上的入场动画
+ * 入场动画
+ * x 方向的生长
  * @param element 执行动画的元素
  * @param animateCfg 动画配置
  * @param cfg 额外信息
  */
 export function growInX(element: IShape | IGroup, animateCfg: AnimateCfg, cfg: AnimateExtraCfg) {
   const { coordinate, minYPoint } = cfg;
-  doClipScaleAnimate(element, animateCfg, coordinate, minYPoint, 'x');
+  doScaleAnimate(element, animateCfg, coordinate, minYPoint, 'x');
 }
 
 /**
- * 整体动画
- * 在整个坐标系范围内执行 y 轴方向上的入场动画
+ * 入场动画
+ * y 轴方向上的生长
  * @param element 执行动画的元素
  * @param animateCfg 动画配置
  * @param cfg 额外信息
  */
 export function growInY(element: IShape | IGroup, animateCfg: AnimateCfg, cfg: AnimateExtraCfg) {
   const { coordinate, minYPoint } = cfg;
-  doClipScaleAnimate(element, animateCfg, coordinate, minYPoint, 'y');
+  doScaleAnimate(element, animateCfg, coordinate, minYPoint, 'y');
 }
 
 /**
- * 整体动画
- * 在整个坐标系范围内执行从坐标系中心点向外放大的动画
+ * 入场
+ * 中心点的向四周的生长动画
  * @param element 执行动画的元素
  * @param animateCfg 动画配置
  * @param cfg 额外信息
  */
 export function growInXY(element: IShape | IGroup, animateCfg: AnimateCfg, cfg: AnimateExtraCfg) {
   const { coordinate, minYPoint } = cfg;
-  doClipScaleAnimate(element, animateCfg, coordinate, minYPoint, 'xy');
+  doScaleAnimate(element, animateCfg, coordinate, minYPoint, 'xy');
 }
