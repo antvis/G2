b8622f07c97207e5a94e4a8243ccc5bc2de40a30
hustcc <i@hust.cc>
2019-12-19 15:55:53 +0800

    feat(util): add omit util

--- tests/unit/util/helper-spec.ts
+++ tests/unit/util/helper-spec.ts
@@ -1,4 +1,4 @@
-import { isBetween, padEnd } from '../../../src/util/helper';
+import { isBetween, omit, padEnd } from '../../../src/util/helper';
 
 describe('Helper', () => {
   it('isBetween()', () => {
@@ -10,5 +10,17 @@ describe('Helper', () => {
     expect(padEnd('skr', 5, '~')).toBe('skr~~');
     expect(padEnd([1, 2, 3], 5, 4)).toEqual([1, 2, 3, 4, 4]);
     expect(padEnd([1, 2, 3], 3, 3)).toEqual([1, 2, 3]);
+  });
+
+  it('omit', () => {
+    expect(omit({}, [])).toEqual({});
+    expect(omit({ a: 1, b: 2 }, ['a'])).toEqual({ b: 2 });
+    expect(omit({ a: 1, b: 2 }, ['a', 'c'])).toEqual({ b: 2 });
+    expect(omit({ a: 1, b: 2 }, ['a', 'b', 'c'])).toEqual({});
+
+    // 保持引用
+    const obj = { a: 1, b: 2 };
+    expect(omit(obj, ['a', 'b'])).toEqual({});
+    expect(obj).toEqual({});
   });
 });
