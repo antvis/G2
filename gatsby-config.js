af1c78ed27557d365d12d30777dc1e9b830fb6a5
afc163 <afc163@gmail.com>
2019-12-24 10:22:14 +0800

    fix: Reducers may not dispatch actions
    https://github.com/gatsbyjs/gatsby/issues/20082#issuecomment-568292986

--- gatsby-config.js
+++ gatsby-config.js
@@ -223,21 +223,21 @@ module.exports = {
     docsearchOptions: {
       apiKey: '200ec461f4aa0bb4f0e761566f1a1336',
       indexName: 'antv_g2'
-    }
-  },
-  htmlCodeTemplate: `<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <title>{{title}}</title>
-      </head>
-      <body>
-        <div id="container" />
-        <script src="https://gw.alipayobjects.com/os/antv/pkg/_antv.g2-3.5.1/dist/g2.min.js"></script>
-        <script src="https://gw.alipayobjects.com/os/antv/pkg/_antv.data-set-0.10.1/dist/data-set.min.js"></script>
-        <script>
-    {{code}}
-        </script>
-      </body>
-    </html>`
+    },
+    htmlCodeTemplate: `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <title>{{title}}</title>
+        </head>
+        <body>
+          <div id="container" />
+          <script src="https://gw.alipayobjects.com/os/antv/pkg/_antv.g2-3.5.1/dist/g2.min.js"></script>
+          <script src="https://gw.alipayobjects.com/os/antv/pkg/_antv.data-set-0.10.1/dist/data-set.min.js"></script>
+          <script>
+      {{code}}
+          </script>
+        </body>
+      </html>`
+  }
 };
