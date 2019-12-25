c6ecf8029c8d56b29fa4f361887583b73af2f090
simaQ <sima.zhang1990@gmail.com>
2019-12-25 12:18:40 +0800

    fix: 修复饼图、环图的更新动画

--- src/animate/animation/sector-path-update.ts
+++ src/animate/animation/sector-path-update.ts
@@ -1,39 +1,99 @@
 import getArcParams from '@antv/g-canvas/lib/util/arc-params';
-import { each } from '@antv/util';
-import { IShape } from '../../dependents';
-import { getSectorPath } from '../../util/graphics';
+import { isNumberEqual } from '@antv/util';
+import { IShape, PathCommand } from '../../dependents';
+import { getArcPath, getSectorPath } from '../../util/graphics';
 import { AnimateCfg, AnimateExtraCfg } from '../interface';
 
-function getAngle(path) {
-  let startPoint;
-  let arcPathCommand;
-  let index;
-  each(path, (command, idx) => {
-    if (command[0] === 'A' || command[0] === 'a') {
-      arcPathCommand = command;
-      index = idx;
-      return false;
-    }
-  });
+function getAngle(startPoint: number[], arcPath: PathCommand) {
+  let { startAngle, endAngle } = getArcParams(startPoint, arcPath);
 
-  startPoint = [path[index - 1][1], path[index - 1][2]];
-  let { startAngle, endAngle } = getArcParams(startPoint, arcPathCommand);
-
-  if (startAngle < -Math.PI * 0.5) {
+  if (!isNumberEqual(startAngle, -Math.PI * 0.5) && startAngle < -Math.PI * 0.5) {
     startAngle += Math.PI * 2;
   }
-  if (endAngle < -Math.PI * 0.5) {
+  if (!isNumberEqual(endAngle, -Math.PI * 0.5) && endAngle < -Math.PI * 0.5) {
     endAngle += Math.PI * 2;
   }
 
-  if (endAngle > startAngle) {
-    endAngle -= Math.PI * 2;
+  if (arcPath[5] === 0) {
+    // 逆时针，需要将 startAngle 和 endAngle 转置，因为 G2 极坐标系为顺时针方向
+    [startAngle, endAngle] = [endAngle, startAngle];
+  }
+
+  if (isNumberEqual(startAngle, Math.PI * 1.5)) {
+    startAngle = Math.PI * -0.5;
+  }
+
+  if (isNumberEqual(endAngle, Math.PI * -0.5)) {
+    endAngle = Math.PI * 1.5;
   }
 
   return {
-    startAngle: endAngle,
-    endAngle: startAngle,
-    radius: arcPathCommand[1],
+    startAngle,
+    endAngle,
+  };
+}
+
+function getArcStartPoint(path: PathCommand) {
+  let startPoint;
+  if (path[0] === 'M' || path[0] === 'L') {
+    startPoint = [path[1], path[2]];
+  } else if (path[0] === 'a' || path[0] === 'A') {
+    startPoint = [path[path.length - 2], path[path.length - 1]];
+  }
+
+  return startPoint;
+}
+
+/**
+ * path 存在以下情况
+ * 1. 饼图不为整圆的 path，命令为 M, L, A, L, Z
+ * 2. 饼图为整圆的 path，命令为 M, M, A, A, M, Z
+ * 3. 环图不为整圆的 path，命令为 M, A, L, A, L, Z
+ * 4. 环图为整圆的 path，命令为 M, A, A, M, A, A, M, Z
+ * 5. radial-line, 不为整圆时的 path, 命令为 M, A, A, Z
+ * 6. radial-line, 为整圆时的 path，命令为 M, A, A, A, A, Z
+ * @param path theta 坐标系下圆弧的 path 命令
+ */
+function getArcInfo(path: PathCommand[]) {
+  let startAngle;
+  let endAngle;
+
+  const arcPaths = path.filter((command) => {
+    return command[0] === 'A' || command[0] === 'a';
+  });
+
+  const firstArcPathCommand = arcPaths[0];
+  const lastArcPathCommand = arcPaths.length > 1 ? arcPaths[1] : arcPaths[0];
+  const firstIndex = path.indexOf(firstArcPathCommand);
+  const lastIndex = path.indexOf(lastArcPathCommand);
+  const firstStartPoint = getArcStartPoint(path[firstIndex - 1]);
+  const lastStartPoint = getArcStartPoint(path[lastIndex - 1]);
+
+  const { startAngle: firstStartAngle, endAngle: firstEndAngle } = getAngle(firstStartPoint, firstArcPathCommand);
+  const { startAngle: lastStartAngle, endAngle: lastEndAngle } = getAngle(lastStartPoint, lastArcPathCommand);
+
+  if (isNumberEqual(firstStartAngle, lastStartAngle) && isNumberEqual(firstEndAngle, lastEndAngle)) {
+    startAngle = firstStartAngle;
+    endAngle = firstEndAngle;
+  } else {
+    startAngle = Math.min(firstStartAngle, lastStartAngle);
+    endAngle = Math.max(firstEndAngle, lastEndAngle);
+  }
+
+  let radius = firstArcPathCommand[1];
+  let innerRadius = arcPaths[arcPaths.length - 1][1];
+
+  if (radius < innerRadius) {
+    [radius, innerRadius] = [innerRadius, radius];
+  } else if (radius === innerRadius) {
+    innerRadius = 0;
+  }
+
+  return {
+    startAngle,
+    endAngle,
+    radius,
+    innerRadius,
   };
 }
 
@@ -47,8 +107,9 @@ export function sectorPathUpdate(shape: IShape, animateCfg: AnimateCfg, cfg: Ani
   const { toAttrs, coordinate } = cfg;
   // @ts-ignore
   const path = toAttrs.path;
-  const { startAngle: curStartAngle, endAngle: curEndAngle, radius } = getAngle(path);
-  const { startAngle: preStartAngle, endAngle: preEndAngle } = getAngle(shape.attr('path'));
+
+  const { startAngle: curStartAngle, endAngle: curEndAngle, radius, innerRadius } = getArcInfo(path);
+  const { startAngle: preStartAngle, endAngle: preEndAngle } = getArcInfo(shape.attr('path'));
 
   const center = coordinate.getCenter();
   const diffStartAngle = curStartAngle - preStartAngle;
@@ -59,7 +120,11 @@ export function sectorPathUpdate(shape: IShape, animateCfg: AnimateCfg, cfg: Ani
     const onFrameEndAngle = preEndAngle + ratio * diffEndAngle;
     return {
       ...toAttrs,
-      path: getSectorPath(center.x, center.y, radius, onFrameStartAngle, onFrameEndAngle),
+      path:
+        // /examples/bar/basic/demo/radial-line.ts
+        path.length === 4 || shape.attr('path').length === 4
+          ? getArcPath(center.x, center.y, radius, onFrameStartAngle, onFrameEndAngle)
+          : getSectorPath(center.x, center.y, radius, onFrameStartAngle, onFrameEndAngle, innerRadius),
     };
   }, animateCfg);
 }
