18d8f72e82c49ef4b0052b0d4e1c1c40d39f335a
hustcc <i@hust.cc>
2019-12-26 11:24:59 +0800

    fix(polar-grid): fix error theme object for grid component

--- tests/bugs/1823-spec.ts
+++ tests/bugs/1823-spec.ts
@@ -1,7 +1,7 @@
 import { Chart } from '../../src';
-import { createDiv } from '../util/dom';
 import { ComponentOption } from '../../src/chart/interface';
 import { COMPONENT_TYPE } from '../../src/constant';
+import { createDiv } from '../util/dom';
 
 describe('#1823', () => {
   const data = [
@@ -12,28 +12,41 @@ describe('#1823', () => {
     { year: '1958 年', sales: 48 },
     { year: '1959 年', sales: 38 },
     { year: '1960 年', sales: 38 },
-    { year: '1962 年', sales: 38 }
+    { year: '1962 年', sales: 38 },
   ];
   const chart = new Chart({
     container: createDiv(),
     width: 400,
-    height: 300
+    height: 300,
   });
 
   chart.data(data);
   chart.coordinate('theta');
 
-  chart.interval({
-    // visible: false
-  }).position('year*sales');
+  chart
+    .interval({
+      // visible: false
+    })
+    .position('year*sales');
   chart.render();
 
-  it('when theta coordinate, do not create axis and grid', () => {
-    function getComponents(type: COMPONENT_TYPE) {
-      return chart.getComponents().filter((co: ComponentOption) => co.type === type);
-    }
+  function getComponents(type: COMPONENT_TYPE) {
+    return chart.getComponents().filter((co: ComponentOption) => co.type === type);
+  }
 
+  it('when theta coordinate, do not create axis and grid', () => {
     expect(getComponents(COMPONENT_TYPE.GRID)).toEqual([]);
     expect(getComponents(COMPONENT_TYPE.AXIS)).toEqual([]);
+  });
+
+  it('when polar coordinate, create axis and grid when correct theme', () => {
+    chart.coordinate('polar');
+    chart.render();
+
+    const [grid] = getComponents(COMPONENT_TYPE.GRID);
+
+    // grid 读到正确的主题颜色
+    expect(grid.component.get('line').style.stroke).toBe('#E9E9E9');
+    expect(grid.component.get('line').style.lineWidth).toBe(1);
   });
 });
