0fb011da61a03de1a79d0e135dd818a63e012765
hustcc <i@hust.cc>
2019-12-30 20:36:26 +0800

    feat(layout): 布局考虑到 coordinate.scale

--- tests/unit/util/direction-spec.ts
+++ tests/unit/util/direction-spec.ts
@@ -1,6 +1,7 @@
 import { DIRECTION } from '../../../src';
+import { getCoordinate } from '../../../src/dependents';
 import { BBox } from '../../../src/util/bbox';
-import { directionToPosition, getTransposedDirection } from '../../../src/util/direction';
+import { directionToPosition, getTranslateDirection } from '../../../src/util/direction';
 
 const parentBBox = new BBox(10, 10, 100, 100);
 const bbox = new BBox(0, 0, 20, 20);
@@ -28,14 +29,51 @@ describe('util direction', () => {
   });
 
   it('getTransposedDirection', () => {
-    expect(getTransposedDirection(DIRECTION.BOTTOM, false)).toEqual(DIRECTION.BOTTOM);
-    expect(getTransposedDirection(DIRECTION.LEFT, false)).toEqual(DIRECTION.LEFT);
-    expect(getTransposedDirection(DIRECTION.RIGHT, false)).toEqual(DIRECTION.RIGHT);
-    expect(getTransposedDirection(DIRECTION.TOP, false)).toEqual(DIRECTION.TOP);
+    const Ctor = getCoordinate('rect');
+    const coordinate = new Ctor({
+      start: { x: 0, y: 100 },
+      end: { x: 100, y: 100 },
+    });
 
-    expect(getTransposedDirection(DIRECTION.BOTTOM, true)).toEqual(DIRECTION.LEFT);
-    expect(getTransposedDirection(DIRECTION.LEFT, true)).toEqual(DIRECTION.BOTTOM);
-    expect(getTransposedDirection(DIRECTION.RIGHT, true)).toEqual(DIRECTION.RIGHT);
-    expect(getTransposedDirection(DIRECTION.TOP, true)).toEqual(DIRECTION.TOP);
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.BOTTOM);
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.RIGHT);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.TOP);
+
+    coordinate.transpose();
+
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.BOTTOM);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.TOP);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.RIGHT);
+
+    // 关闭 transpose
+    coordinate.transpose();
+
+    expect(coordinate.isTransposed).toBe(false);
+
+    coordinate.scale(-0.5, 0.5); // -0.5, 0.5
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.RIGHT);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.TOP);
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.BOTTOM);
+
+    coordinate.scale(-1, -1); // 0.5, -0.5
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.RIGHT);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.BOTTOM);
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.TOP);
+
+    coordinate.scale(-1, 1); // -0.5 -0.5
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.RIGHT);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.BOTTOM);
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.TOP);
+
+    coordinate.transpose();
+    expect(getTranslateDirection(DIRECTION.LEFT, coordinate)).toEqual(DIRECTION.TOP);
+    expect(getTranslateDirection(DIRECTION.RIGHT, coordinate)).toEqual(DIRECTION.BOTTOM);
+    expect(getTranslateDirection(DIRECTION.TOP, coordinate)).toEqual(DIRECTION.LEFT);
+    expect(getTranslateDirection(DIRECTION.BOTTOM, coordinate)).toEqual(DIRECTION.RIGHT);
   });
 });
