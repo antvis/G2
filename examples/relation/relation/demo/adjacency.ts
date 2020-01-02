ca1ce95626a4cebe55aa5fbb62a79296d5a25e1f
simaQ <sima.zhang1990@gmail.com>
2020-01-02 17:06:04 +0800

    chore: 迁移剩余 demos

---
+++ examples/relation/relation/demo/adjacency.ts
@@ -0,0 +1,42 @@
+import DataSet from '@antv/data-set';
+import { Chart } from '@antv/g2';
+
+fetch('../data/flare.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const ds = new DataSet();
+    const dv = ds.createView().source(data, {
+      type: 'hierarchy',
+    });
+    dv.transform({
+      type: 'hierarchy.partition',
+    });
+
+    const chart = new Chart({
+      container: 'container',
+      autoFit: true,
+      height: 500,
+      padding: 0,
+    });
+    chart.data(
+      dv.getAllNodes().map((node) => ({
+        name: node.data.name,
+        value: node.value,
+        depth: node.depth,
+        x: node.x,
+        y: node.y,
+      }))
+    );
+    chart.tooltip({
+      showTitle: false,
+      showTooltipMarkers: false,
+    });
+    chart.axis(false);
+    chart.legend(false);
+    chart
+      .polygon()
+      .position('x*y')
+      .color('name');
+
+    chart.render();
+  });
