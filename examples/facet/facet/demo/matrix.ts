e84c4b3fbc7838c8dc42132499ced4a8bb42301d
hustcc <i@hust.cc>
2020-02-07 13:36:42 +0800

    feat(matrix): add example for facet matrix

---
+++ examples/facet/facet/demo/matrix.ts
@@ -0,0 +1,56 @@
+import { DataView } from '@antv/data-set';
+import { Chart } from '@antv/g2';
+
+const COLOR = ['#1890FF', '#2FC25B', '#FACC14'];
+
+fetch('../data/iris.json')
+  .then(res => res.json())
+  .then(data => {
+    const chart = new Chart({
+      container: 'container',
+      autoFit: true,
+      height: 500,
+      padding: [16, 16, 48, 48],
+    });
+
+    chart.data(data);
+
+    chart.scale({
+      Species: {
+        sync: true,
+      }
+    });
+
+    chart.facet('matrix', {
+      fields: [ 'SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth' ],
+      eachView(view, facet) {
+        if (facet.rowIndex === facet.columnIndex) {
+          // 对角线的图形，做数据封箱之后绘制图形
+          const dv = new DataView();
+          dv.source(facet.data)
+            .transform({
+              type: 'bin.histogram',
+              field: facet.columnField,  // 对应数轴上的一个点
+              bins: 30,               // 分箱个数
+              as: [ facet.columnField, 'count' ],
+              groupBy: [ 'Species' ]
+            });
+          view.data(dv.rows);
+
+          view.interval()
+            .position(facet.columnField + '*count')
+            .color('Species', COLOR)
+            .adjust('stack')
+            .style({ opacity: 0.85 });
+        } else {
+          view.point()
+            .position([ facet.columnField, facet.rowField ])
+            .color('Species', COLOR)
+            .shape('circle')
+            .style({ opacity: 0.3 })
+            .size(3);
+        }
+      }
+    });
+    chart.render();
+  });
