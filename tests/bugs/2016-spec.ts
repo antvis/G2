9eec951b71ddd7b43a10c67876b1f60b8ed7f93c
simaQ <sima.zhang1990@gmail.com>
2020-02-12 23:32:18 +0800

    test: 添加对应的测试用例

---
+++ tests/bugs/2016-spec.ts
@@ -0,0 +1,41 @@
+import { Chart } from '../../src';
+import { createDiv, removeDom } from '../util/dom';
+
+describe('#2016', () => {
+  let chart;
+  const container = createDiv();
+
+  it('Duplicate data', () => {
+    chart = new Chart({
+      container,
+      width: 400,
+      height: 300,
+      autoFit: false,
+    });
+    chart.data([
+      { gender: 'female', height: 161.2, weight: 51.6 },
+      { gender: 'female', height: 161.2, weight: 51.6 },
+      { gender: 'female', height: 161.2, weight: 51.6 },
+      { gender: 'female', height: 157, weight: 63 },
+    ]);
+    chart.scale({
+      width: { nice: true },
+      height: { nice: true },
+    });
+    const point = chart
+      .point()
+      .position('height*weight')
+      .shape('circle');
+    chart.render();
+
+    expect(point.elements.length).toBe(2);
+
+    chart.render(true);
+    expect(point.elements.length).toBe(2);
+  });
+
+  afterAll(() => {
+    chart.destroy();
+    removeDom(container);
+  });
+});
