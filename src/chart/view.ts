25fa19112cbaef108a9941dc4e0dc8a711fc033d
hustcc <i@hust.cc>
2020-02-12 11:27:42 +0800

    test(slider): add test case for slider controller

--- src/chart/view.ts
+++ src/chart/view.ts
@@ -1091,7 +1091,7 @@ export class View extends Base {
       return data;
     }
 
-    return filter(data, (datum: Datum) => condition(datum[field], datum));
+    return filter(data, (datum: Datum, idx: number) => condition(datum[field], datum, idx));
   }
 
   /**
