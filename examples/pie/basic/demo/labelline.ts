73cfada6d2fec1bbacf7dde6c4f57a8eede213c2
SimaQ <sima.zhang1990@gmail.com>
2020-03-09 14:46:13 +0800

    Revert "fix: 修复测试"
    This reverts commit 33617ed61decd4a20cf116ae02124405d37dc651.

--- examples/pie/basic/demo/labelline.ts
+++ examples/pie/basic/demo/labelline.ts
@@ -37,7 +37,6 @@ chart
   .position('percent')
   .color('item')
   .label('percent', {
-    layout: { type: 'distribute' },
     content: (data) => {
       return `${data.item}: ${data.percent * 100}%`;
     },
