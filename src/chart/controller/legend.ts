97fe7ae72f4611107b428073d9908175f40373cb
simaQ <sima.zhang1990@gmail.com>
2020-03-25 17:01:56 +0800

    perf: 如果容器为空则不需要调用 clear() 方法

--- src/chart/controller/legend.ts
+++ src/chart/controller/legend.ts
@@ -266,7 +266,9 @@ export default class Legend extends Controller<Option> {
   public clear() {
     super.clear();
 
-    this.container.clear();
+    if (this.container.getCount()) {
+      this.container.clear();
+    }
   }
 
   public destroy() {
