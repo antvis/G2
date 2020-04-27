1be889ac874bf1713f525dbfa7fdac0c9f7fdf70
zqlu <zqlu.cn@gmail.com>
2020-04-27 14:20:39 +0800

    fix: off event when destroy

--- tests/unit/chart/controller/gesture-spec.ts
+++ tests/unit/chart/controller/gesture-spec.ts
@@ -29,40 +29,32 @@ describe('Gesture', () => {
 
   xit('swipe', (done) => {
     let swipe;
-    view.on('swipe', (e) => swipe = e);
+    view.on('swipe', (e) => (swipe = e));
     // @ts-ignore
     canvas.emit('touchstart', {
       name: 'touchstart',
       originalEvent: {
-        touches: [
-          { clientX: 10, clientY: 10 }
-        ]
-      }
+        touches: [{ clientX: 10, clientY: 10 }],
+      },
     });
     canvas.emit('touchmove', {
       name: 'touchmove',
       originalEvent: {
-        touches: [
-          { clientX: 20, clientY: 10 },
-        ]
-      }
+        touches: [{ clientX: 20, clientY: 10 }],
+      },
     });
     canvas.emit('touchmove', {
       name: 'touchmove',
       originalEvent: {
-        touches: [
-          { clientX: 22, clientY: 13 },
-        ]
-      }
+        touches: [{ clientX: 22, clientY: 13 }],
+      },
     });
 
     canvas.emit('touchend', {
       name: 'touchend',
       originalEvent: {
-        touches: [
-          { clientX: 150, clientY: 10 },
-        ]
-      }
+        touches: [{ clientX: 150, clientY: 10 }],
+      },
     });
     setTimeout(() => {
       expect(swipe).toBeDefined();
@@ -82,10 +74,8 @@ describe('Gesture', () => {
     canvas.emit('touchstart', {
       name: 'touchstart',
       originalEvent: {
-        touches: [
-          { clientX: 10, clientY: 10 }
-        ]
-      }
+        touches: [{ clientX: 10, clientY: 10 }],
+      },
     });
     canvas.emit('touchmove', {
       name: 'touchmove',
@@ -93,16 +83,14 @@ describe('Gesture', () => {
         touches: [
           { clientX: 20, clientY: 20 },
           { clientX: 25, clientY: 23 },
-        ]
-      }
+        ],
+      },
     });
     canvas.emit('touchend', {
       name: 'touchend',
       originalEvent: {
-        touches: [
-          { clientX: 30, clientY: 30 },
-        ]
-      }
+        touches: [{ clientX: 30, clientY: 30 }],
+      },
     });
     expect(pinchCallback).toBeCalled();
     expect(pinchstartCallback).toBeCalled();
@@ -114,7 +102,7 @@ describe('Gesture', () => {
     const pressstartCallback = jest.fn();
     const pressendCallback = jest.fn();
     view.on('pressstart', pressstartCallback);
-    view.on('press', e => {
+    view.on('press', (e) => {
       pressEvent = e;
     });
     view.on('pressend', pressendCallback);
@@ -122,10 +110,8 @@ describe('Gesture', () => {
     canvas.emit('touchstart', {
       name: 'touchstart',
       originalEvent: {
-        touches: [
-          { clientX: 10, clientY: 10 }
-        ]
-      }
+        touches: [{ clientX: 10, clientY: 10 }],
+      },
     });
 
     await delay(260);
@@ -138,10 +124,8 @@ describe('Gesture', () => {
     canvas.emit('touchmove', {
       name: 'touchmove',
       originalEvent: {
-        touches: [
-          { clientX: 50, clientY: 10 }
-        ]
-      }
+        touches: [{ clientX: 50, clientY: 10 }],
+      },
     });
     expect(pressCallback).toBeCalled();
   });
@@ -151,32 +135,26 @@ describe('Gesture', () => {
     let pan;
     const panendCallback = jest.fn();
     view.on('panstart', panstartCallback);
-    view.on('pan', (e) => pan = e);
+    view.on('pan', (e) => (pan = e));
     view.on('panend', panendCallback);
 
     canvas.emit('touchstart', {
       name: 'touchstart',
       originalEvent: {
-        touches: [
-          { clientX: 10, clientY: 10 }
-        ]
-      }
+        touches: [{ clientX: 10, clientY: 10 }],
+      },
     });
     canvas.emit('touchmove', {
       name: 'touchmove',
       originalEvent: {
-        touches: [
-          { clientX: 20, clientY: 10 },
-        ]
-      }
+        touches: [{ clientX: 20, clientY: 10 }],
+      },
     });
     canvas.emit('touchend', {
       name: 'touchend',
       originalEvent: {
-        touches: [
-          { clientX: 24, clientY: 20 },
-        ]
-      }
+        touches: [{ clientX: 24, clientY: 20 }],
+      },
     });
 
     expect(pan).toBeDefined();
@@ -211,5 +189,51 @@ describe('Register gesture', () => {
     });
 
     expect(view.getController('gesture')).toBeInstanceOf(GestureController);
+  });
+});
+
+describe('Event on/off', () => {
+  const div = createDiv();
+  const canvas = createCanvas({
+    container: div,
+  });
+  registerComponentController('gesture', GestureController);
+
+  it('event on/off', async () => {
+    const view1 = new View({
+      parent: null,
+      canvas,
+      foregroundGroup: canvas.addGroup(),
+      middleGroup: canvas.addGroup(),
+      backgroundGroup: canvas.addGroup(),
+      padding: 5,
+      visible: false,
+    });
+    const view2 = new View({
+      parent: null,
+      canvas,
+      foregroundGroup: canvas.addGroup(),
+      middleGroup: canvas.addGroup(),
+      backgroundGroup: canvas.addGroup(),
+      padding: 5,
+      visible: false,
+    });
+
+    // destroy GestureController in view 1
+    view1.destroy();
+    let error;
+    window.addEventListener('error', (evt) => {
+      error = evt;
+    });
+
+    canvas.emit('touchstart', {
+      name: 'touchstart',
+      originalEvent: {
+        touches: [{ clientX: 10, clientY: 10 }],
+      },
+    });
+
+    await delay(260);
+    expect(error).toBeUndefined();
   });
 });
