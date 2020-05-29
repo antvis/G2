c7d74b3323b24604d0891c436253dfcdb0454ef3
simaQ <sima.zhang1990@gmail.com>
2020-05-29 11:37:59 +0800

    chore: remove unused code

--- tests/bugs/2505-spec.ts
+++ tests/bugs/2505-spec.ts
@@ -27,6 +27,5 @@ describe('2505', () => {
     chart.render();
 
     expect(chart.getCanvas().get('container').style.background).toBe('rgb(20, 20, 20)');
-    // expect(chart.getCanvas().get('ele').style.background).toBe('#141414');
   });
 });
