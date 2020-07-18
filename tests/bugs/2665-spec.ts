fd92f4e3fad9cc980dd0472d198f197e94d73bec
simaQ <sima.zhang1990@gmail.com>
2020-07-18 17:07:19 +0800

    test: 补充测试用例

--- tests/bugs/2665-spec.ts
+++ tests/bugs/2665-spec.ts
@@ -29,10 +29,10 @@ describe('#2665', () => {
     });
 
     chart.data(data);
-    const area = chart.area({
+    let area = chart.area({
       showSinglePoint: true,
     }).position('year*value');
-    const line = chart.line({
+    let line = chart.line({
       showSinglePoint: true,
     }).position('year*value');
     chart.render();
@@ -60,5 +60,28 @@ describe('#2665', () => {
 
     expect(getPathCount(area.elements[0].shape.attr('path'))).toBe(3);
     expect(getPathCount(line.elements[0].shape.attr('path'))).toBe(3);
+
+    // showSinglePoint: false
+    chart.clear();
+    chart.data([
+      { year: '1991', value: 3 },
+      { year: '1992', value: null },
+      { year: '1993', value: 3.5 },
+      { year: '1994', value: 5 },
+      { year: '1995', value: null },
+      { year: '1996', value: 6 },
+      { year: '1997', value: 10 },
+      { year: '1998', value: 9 },
+      { year: '1999', value: 13 },
+    ]);
+    area = chart.area({
+      showSinglePoint: false,
+    }).position('year*value');
+    line = chart.line({
+      showSinglePoint: false,
+    }).position('year*value');
+    chart.render();
+    expect(getPathCount(area.elements[0].shape.attr('path'))).toBe(2);
+    expect(getPathCount(line.elements[0].shape.attr('path'))).toBe(2);
   });
 });
