31965f743ffd2d7450bb2b7cc260b3893c51114e
ArranZeyuWang <zywangx@gmail.com>
2020-07-20 19:11:23 +0800

    fix: path err in dynamic demos

--- examples/gallery/dynamic/demo/dynamic-bar.ts
+++ examples/gallery/dynamic/demo/dynamic-bar.ts
@@ -60,7 +60,7 @@ function handleData(source) {
   return source;
 }
 
-fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/china-gdp.json')
+fetch('https://raw.githubusercontent.com/antvis/G2/master/examples/data/china-gdp.json')
   .then(res => res.json())
   .then(data => {
     let count = 0;
