c2727a82e0311ecd7c06ed71d3d9fe5fe8a93905
SimaQ <sima.zhang1990@gmail.com>
2020-10-12 21:16:04 +0800

    fix: 漏斗图 label position 可生效 (#2909)

--- tests/unit/geometry/label/interval-spec.ts
+++ tests/unit/geometry/label/interval-spec.ts
@@ -1,4 +1,4 @@
-import { flatten } from '@antv/util';
+import { flatten, get } from '@antv/util';
 import { Coordinate, getCoordinate } from '@antv/coord';
 import { Chart } from '../../../../src';
 import Interval from '../../../../src/geometry/interval';
@@ -122,6 +122,7 @@ describe('interval labels', () => {
         position: 'right',
         offset: 0,
       });
+
       const [item1, item2] = gLabels.getLabelItems(mappingArray);
       expect(item1.x).toBe(data1.x + 15);
       expect(item1.y).toBe((data1.y + coord.y.start) / 2);
@@ -136,6 +137,7 @@ describe('interval labels', () => {
         position: 'top',
         offset: 0,
       });
+
       const [item1, item2] = gLabels.getLabelItems(mappingArray);
       expect(item1.x).toBe(data1.x);
       expect(item1.y).toBe(data1.y);
@@ -274,12 +276,15 @@ describe('interval position', () => {
   const getIntervalSize = (coordinate: Coordinate, shapePoints: Point[]) => {
     const transposed = coordinate.isTransposed;
     const point0 = coordinate.convert(shapePoints[0]);
-    const point1 = coordinate.convert(shapePoints[2]);
-    const flag = transposed ? -1 : 1;
-    const width = (point0.x - point1.x) * flag;
-    const height = (point0.y - point1.y) * flag;
+    const point2 = coordinate.convert(shapePoints[2]);
+    const top = Math.min(point2.y, point0.y);
+    const bottom = Math.max(point2.y, point0.y);
+    const right = point2.x;
+    const left = point0.x;
 
-    return { x: point0.x, y: point0.y, width, height };
+    return {
+      top, bottom, right, left,
+    }
   };
 
   it('position bottom', () => {
@@ -304,11 +309,11 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { y } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { bottom } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe('center');
       expect(labelTextShape.attr('textBaseline')).toBe(dir > 0 ? 'bottom' : 'top');
-      expect(near(labelTextShape.attr('y'), dir > 0 ? y - 12 : y + 12)).toBe(true);
+      expect(near(labelTextShape.attr('y'), dir > 0 ? bottom - 12 : bottom + 12)).toBe(true);
     });
   });
 
@@ -334,11 +339,11 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { y, height } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { top, bottom } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe('center');
       expect(labelTextShape.attr('textBaseline')).toBe('middle');
-      expect(near(labelTextShape.attr('y'), y - height / 2)).toBe(true);
+      expect(near(labelTextShape.attr('y'), (top + bottom) / 2)).toBe(true);
     });
   });
 
@@ -364,11 +369,11 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { y, height } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { top } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe('center');
       expect(labelTextShape.attr('textBaseline')).toBe(dir > 0 ? 'bottom' : 'top');
-      expect(near(labelTextShape.attr('y'), y - height + -dir * 12)).toBe(true);
+      expect(near(labelTextShape.attr('y'), top + -dir * 12)).toBe(true);
     });
   });
 
@@ -395,11 +400,11 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { x } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { left } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe(dir > 0 ? 'left' : 'right');
       expect(labelTextShape.attr('textBaseline')).toBe('middle');
-      expect(near(labelTextShape.attr('x'), dir > 0 ? x + 12 : x - 12)).toBe(true);
+      expect(near(labelTextShape.attr('x'), dir > 0 ? left + 12 : left - 12)).toBe(true);
     });
   });
 
@@ -426,11 +431,11 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { x, width } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { left, right } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe('center');
       expect(labelTextShape.attr('textBaseline')).toBe('middle');
-      expect(near(labelTextShape.attr('x'), x + width / 2)).toBe(true);
+      expect(near(labelTextShape.attr('x'), (left + right) / 2)).toBe(true);
     });
   });
 
@@ -457,11 +462,316 @@ describe('interval position', () => {
 
     elements.forEach((element) => {
       const dir = element.getData().value < 0 ? -1 : 1;
-      const { x, width } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
+      const { right } = getIntervalSize(chart.getCoordinate(), element.getModel().points as Point[]);
       const labelTextShape = element.labelShape[0]?.find((el) => el.get('type') === 'text');
       expect(labelTextShape.attr('textAlign')).toBe(dir > 0 ? 'left' : 'right');
       expect(labelTextShape.attr('textBaseline')).toBe('middle');
-      expect(near(labelTextShape.attr('x'), x + width + dir * 12)).toBe(true);
+      expect(near(labelTextShape.attr('x'), right + dir * 12)).toBe(true);
+    });
+  });
+});
+
+describe('funnel position', () => {
+  const funnelData = [
+    { action: '浏览网站', pv: 50000 },
+    { action: '放入购物车', pv: 35000 },
+    { action: '生成订单', pv: 25000 },
+    { action: '支付订单', pv: 15000 },
+    { action: '完成交易', pv: 8000 },
+  ];
+  const getIntervalSize = (coordinate: Coordinate, mappingData) => {
+    const transposed = coordinate.isTransposed;
+    let top;
+    let right;
+    let bottom;
+    let left;
+    const nextPoints = get(mappingData, 'nextPoints');
+    const points = get(mappingData, 'points');
+    const point0 = coordinate.convert(points[0]);
+    const point2 = coordinate.convert(points[2]);
+    if (nextPoints) {
+      // 非漏斗图底部
+      const p0 = coordinate.convert(points[0] as Point);
+      const p1 = coordinate.convert(points[1] as Point);
+      const nextP0 = coordinate.convert(nextPoints[0] as Point);
+      const nextP1 = coordinate.convert(nextPoints[1] as Point);
+
+      // TODO: 使用包围盒的计算方法
+      if (transposed) {
+        top = Math.min(nextP0.y, p0.y);
+        bottom = Math.max(nextP0.y, p0.y);
+        right = (p1.x + nextP1.x) / 2;
+        left = (p0.x + nextP0.x) / 2;
+      } else {
+        top = Math.min((p1.y + nextP1.y) / 2, (p0.y + nextP0.y) / 2);
+        bottom = Math.max((p1.y + nextP1.y) / 2, (p0.y + nextP0.y) / 2);
+        right = nextP1.x;
+        left = p0.x;
+      }
+    } else {
+      top = Math.min(point2.y, point0.y);
+      bottom = Math.max(point2.y, point0.y);
+      right = point2.x;
+      left = point0.x;
+    }
+
+    return {
+      top, bottom, right, left,
+    }
+  };
+
+  it('position bottom', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'bottom',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { bottom } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe('center');
+      expect(labelTextShape.attr('textBaseline')).toBe(dir > 0 ? 'bottom' : 'top');
+      expect(near(labelTextShape.attr('y'), bottom + 12)).toBe(true);
+    });
+  });
+
+  it('position middle', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'middle',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { top, bottom } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe('center');
+      expect(labelTextShape.attr('textBaseline')).toBe('middle');
+      expect(near(labelTextShape.attr('y'), (top + bottom) / 2)).toBe(true);
+    });
+  });
+
+  it('position top', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'top',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { top } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe('center');
+      expect(labelTextShape.attr('textBaseline')).toBe(dir > 0 ? 'bottom' : 'top');
+      expect(near(labelTextShape.attr('y'), top + 12)).toBe(true);
+    });
+  });
+
+  it('transposed position left', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .transpose()
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'left',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { left } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe(dir > 0 ? 'left' : 'right');
+      expect(labelTextShape.attr('textBaseline')).toBe('middle');
+      expect(near(labelTextShape.attr('x'), dir > 0 ? left + 12 : left - 12)).toBe(true);
+    });
+  });
+
+  it('transposed position middle', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .transpose()
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'middle',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { left, right } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe('center');
+      expect(labelTextShape.attr('textBaseline')).toBe('middle');
+      expect(near(labelTextShape.attr('x'), (left + right) / 2)).toBe(true);
+    });
+  });
+
+  it('transposed position right', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      width: 600,
+      height: 500,
+    });
+    chart.data(funnelData);
+    chart.axis(false);
+    chart
+      .coordinate('rect')
+      .transpose()
+      .scale(1, -1);
+    chart.tooltip({
+      showMarkers: false,
+    });
+    const interval = chart
+      .interval()
+      .adjust('symmetric')
+      .position('action*pv')
+      .shape('funnel')
+      .color('action')
+      .label('pv',
+        {
+          position: 'right',
+        }
+      );
+
+    chart.render();
+
+    const { elements } = interval;
+
+    expect(elements).toHaveLength(funnelData.length);
+
+    elements.forEach((element) => {
+      const dir = element.getData().value < 0 ? -1 : 1;
+      const { right } = getIntervalSize(chart.getCoordinate(), element.getModel());
+      const labelTextShape = element.labelShape[0] ?.find((el) => el.get('type') === 'text');
+      expect(labelTextShape.attr('textAlign')).toBe(dir > 0 ? 'left' : 'right');
+      expect(labelTextShape.attr('textBaseline')).toBe('middle');
+      expect(near(labelTextShape.attr('x'), right + dir * 12)).toBe(true);
     });
   });
 });
