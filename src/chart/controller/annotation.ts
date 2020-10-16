9891f40b6c6fd02af2312542c01ec430d004d974
hustcc <i@hust.cc>
2020-10-16 14:44:22 +0800

    fix(annotation): text annotation re-render will be hidden (#2916)
    * fix(annotation): text annotation re-render will be hidden
    
    * refactor(annotation): fixed #2014
    
    * fix: lint
    
    * fix: typo

--- src/chart/controller/annotation.ts
+++ src/chart/controller/annotation.ts
@@ -1,4 +1,4 @@
-import { contains, deepMix, each, get, isArray, isFunction, isNil, isString, keys, upperFirst } from '@antv/util';
+import { contains, deepMix, each, get, isArray, isFunction, isNil, isString, keys, upperFirst, find } from '@antv/util';
 import { Annotation as AnnotationComponent, IElement, IGroup } from '../../dependents';
 import {
   AnnotationBaseOption as BaseOption,
@@ -55,128 +55,51 @@ export default class Annotation extends Controller<BaseOption[]> {
 
   public init() {}
 
+  /**
+   * 因为 annotation 需要依赖坐标系信息，所以 render 阶段为空方法，实际的创建逻辑都在 layout 中
+   */
   public layout() {
-    const components = this.getComponents();
-    const updateComponentFn = (co: ComponentOption) => {
-      const { component, extra } = co;
-      const { type } = extra;
-      const theme = this.getAnnotationTheme(type);
-
-      component.update(this.getAnnotationCfg(type, extra, theme));
-      component.render();
-    };
-    const createComponentFn = (option: BaseOption) => {
-      const co = this.createAnnotation(option);
-      if (co) {
-        co.component.init();
-        // Note：regionFilter 特殊处理，regionFilter需要取到 Geometry 中的 Shape，需要在 view render 之后处理
-        // 其他组件使用外层的统一 render 逻辑
-        if (option.type === 'regionFilter') {
-          co.component.render();
-        }
-        // 缓存起来
-        this.cache.set(option, co);
-      }
-    };
-
-    if (components.length) {
-      each(components, (co: ComponentOption) => {
-        const { component } = co;
-
-        if (component.get('type') === 'regionFilter') {
-          // regionFilter 依赖绘制后的 Geometry Shapes
-          this.whenRegionFilter(() => {
-            updateComponentFn(co);
-          });
-        } else {
-          updateComponentFn(co);
-        }
-      });
-    } else {
-      each(this.option, (option: BaseOption) => {
-        if (option.type === 'regionFilter') {
-          this.whenRegionFilter(() => {
-            createComponentFn(option);
-          });
-        } else {
-          createComponentFn(option);
-        }
-      });
-    }
+    this.update();
   }
 
-  public render() {
-    // 因为 Annotation 不参与布局，但是渲染的位置依赖于坐标系，所以可以将绘制阶段延迟到 layout() 进行
-  }
+   // 因为 Annotation 不参与布局，但是渲染的位置依赖于坐标系，所以可以将绘制阶段延迟到 layout() 进行
+  public render() {}
 
   /**
    * 更新
    */
   public update() {
-    // 已经处理过的 legend
-    const updated = new WeakMap<BaseOption, true>();
-
-    const updateComponentFn = (option: BaseOption) => {
-      const { type } = option;
-      const theme = this.getAnnotationTheme(type);
-      const cfg = this.getAnnotationCfg(type, option, theme);
-
-      const existCo = this.cache.get(option);
-
-      // 存在，则更新
-      if (existCo) {
-        // 忽略掉一些配置
-        omit(cfg, ['container']);
-
-        existCo.component.update(cfg);
-        updated.set(option, true);
-      } else {
-        // 不存在，则创建
-        const co = this.createAnnotation(option);
-        if (co) {
-          co.component.init();
-          // Note：regionFilter 特殊处理，regionFilter需要取到 Geometry 中的 Shape，需要在 view render 之后处理
-          // 其他组件使用外层的统一 render 逻辑
-          if (option.type === 'regionFilter') {
-            co.component.render();
-          }
-          // 缓存起来
-          this.cache.set(option, co);
-          updated.set(option, true);
-        }
-      }
-    };
-
+    // 1. 先处理 regionFilter，regionFilter 需要在 Geometry 完成之后，拿到图形信息
     this.whenRegionFilter(() => {
+      const updated = new Map<BaseOption, ComponentOption>();
       // 先看是否有 regionFilter 要更新
       each(this.option, (option: BaseOption) => {
         if (option.type === 'regionFilter') {
-          updateComponentFn(option);
+          const co = this.updateOrCreate(option);
+          // 存储已经处理过的
+          if (co) {
+            updated.set(this.getCacheKey(option), co);
+          }
         }
       });
 
+      // 处理完成之后，更新 cache
       // 处理完成之后，销毁删除的
-      // 不在处理中的
-      const newCache = new Map<BaseOption, ComponentOption>();
-
-      this.cache.forEach((value: ComponentOption, key: BaseOption) => {
-        if (updated.has(key)) {
-          newCache.set(key, value);
-        } else {
-          // 不存在，则是所有需要被销毁的组件
-          value.component.destroy();
-        }
-      });
-
-      // 更新缓存
-      this.cache = newCache;
+      this.cache = this.syncCache(updated);
     });
 
+    // 2. 处理非 regionFilter
+    const updateCache = new Map<BaseOption, ComponentOption>();
     each(this.option, (option: BaseOption) => {
       if (option.type !== 'regionFilter') {
-        updateComponentFn(option);
+        const co = this.updateOrCreate(option);
+        // 存储已经处理过的
+        if (co) {
+          updateCache.set(this.getCacheKey(option), co);
+        }
       }
     });
+    this.cache = this.syncCache(updateCache);
   }
 
   /**
@@ -643,5 +566,78 @@ export default class Annotation extends Controller<BaseOption[]> {
 
   private getAnnotationTheme(type: string) {
     return get(this.view.getTheme(), ['components', 'annotation', type], {});
+  }
+
+  /**
+   * 创建或者更新 annotation
+   * @param option
+   */
+  private updateOrCreate(option: BaseOption) {
+    // 拿到缓存的内容
+    let co = this.cache.get(this.getCacheKey(option));
+
+    // 存在则更新，不存在在创建
+    if (co) {
+      const { type } = option;
+      const theme = this.getAnnotationTheme(type);
+      const cfg = this.getAnnotationCfg(type, option, theme);
+
+      // 忽略掉一些配置
+      omit(cfg, ['container']);
+      co.component.update(cfg);
+      // 对于 regionFilter，因为生命周期的原因，需要额外 render
+      if (option.type === 'regionFilter') {
+        co.component.render();
+      }
+    } else { // 不存在，创建
+      co = this.createAnnotation(option);
+      if (co) {
+        co.component.init();
+        // Note：regionFilter 特殊处理，regionFilter需要取到 Geometry 中的 Shape，需要在 view render 之后处理
+        // 其他组件使用外层的统一 render 逻辑
+        if (option.type === 'regionFilter') {
+          co.component.render();
+        }
+      }
+    }
+    return co;
+  }
+
+  /**
+   * 更新缓存，以及销毁组件
+   * @param updated 更新或者创建的组件
+   */
+  private syncCache(updated: Map<BaseOption, ComponentOption>) {
+    const newCache = new Map(this.cache); // clone 一份
+
+    // 将 update 更新到 cache
+    updated.forEach((co: ComponentOption, key: BaseOption) => {
+      newCache.set(key, co);
+    });
+
+    // 另外和 options 进行对比，删除
+    newCache.forEach((co: ComponentOption, key: BaseOption) => {
+      // option 中已经找不到，那么就是删除的
+      if (!find(this.option, (option: BaseOption) => {
+        return key === this.getCacheKey(option);
+      })) {
+        co.component.destroy();
+        newCache.delete(key);
+      }
+    });
+
+    return newCache;
+  }
+
+  /**
+   * 获得缓存组件的 key
+   * @param option
+   */
+  private getCacheKey(option: BaseOption) {
+    // 如果存在 id，则使用 id string，否则直接使用 option 引用作为 key
+    return option;
+    // 后续扩展 id 用
+    // const id = get(option, 'id');
+    // return id ? id : option;
   }
 }
