66f26d977de859b214565a84a4e85abbd4ee5c95
xi li <35586469+BBSQQ@users.noreply.github.com>
2020-10-29 23:51:47 +0800

    chore: upgrade gatsby theme preview (#2960)

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,8 @@ module.exports = {
       '3.x': 'https://g2-v3.antv.vision/',
       '2.x': 'https://antv.vision/old-site/g2/doc/index.html',
     },
+    showChartResize: true, // 是否在demo页展示图表视图切换
+    showAPIDoc: true, // 是否在demo页展示API文档
     navs: [
       {
         slug: 'docs/manual',
