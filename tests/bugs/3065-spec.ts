4e71712293217de3d6226ad6239cb78f2f56993d
xinming <xinming.lxj@antfin.com>
2020-12-01 20:03:28 +0800

    feat: 增加 rect 坐标系下label ellipsis-in-plot的单测

--- tests/bugs/3065-spec.ts
+++ tests/bugs/3065-spec.ts
@@ -23,21 +23,81 @@ describe('#3065', () => {
     chart.coordinate('theta');
 
     // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
-    chart.interval().position('1*sold').color('genre').adjust('stack').label('genre', {
-      layout: [{ type: 'pie-outer' }],
-    });
+    chart
+      .interval()
+      .position('1*sold')
+      .color('genre')
+      .adjust('stack')
+      .label('genre', {
+        layout: [{ type: 'pie-outer' }],
+      });
 
     chart.render();
     let labels = chart.geometries[0].labelsContainer.getChildren();
-    expect(labels[1].getBBox().maxX).not.toBeLessThanOrEqual(chart.getCoordinate().end.x);
+    expect(labels[1].getCanvasBBox().maxX).not.toBeLessThanOrEqual(chart.getCoordinate().end.x);
 
     chart.clear();
-    chart.interval().position('1*sold').color('genre').adjust('stack').label('genre', {
-      layout: [{ type: 'pie-outer' }, { type: 'limit-in-plot', cfg: { action: 'ellipsis' } }],
-    });
+    chart
+      .interval()
+      .position('1*sold')
+      .color('genre')
+      .adjust('stack')
+      .label('genre', {
+        layout: [{ type: 'pie-outer' }, { type: 'limit-in-plot', cfg: { action: 'ellipsis' } }],
+      });
     chart.render();
     labels = chart.geometries[0].labelsContainer.getChildren();
-    expect(labels[1].getBBox().maxX).toBeLessThanOrEqual(chart.getCoordinate().end.x);
+    expect(labels[1].getCanvasBBox().maxX).toBeLessThanOrEqual(chart.getCoordinate().end.x);
     expect((labels[1] as IGroup).find((shape) => shape.get('type') === 'text').attr('text')).toMatch('...');
+
+    chart.destroy();
+  });
+
+  it('rect 坐标系', () => {
+    const chart = new Chart({
+      container: createDiv(),
+      autoFit: false,
+      width: 938,
+      height: 601,
+      padding: 0,
+    });
+
+    const data = [
+      { country: '巴西', population: 18203 },
+      { country: '印尼', population: 23489 },
+      { country: '美国', population: 29034 },
+      { country: '印度', population: 104970 },
+      { country: '中国', population: 151744 },
+    ];
+
+    chart.data(data);
+    chart.scale('population', { nice: true });
+    chart.coordinate().transpose();
+    chart
+      .interval()
+      .position('country*population')
+      .label('population', {
+        position: 'right',
+        offsetX: 0,
+      });
+    chart.render();
+
+    let labels = chart.geometries[0].labelsContainer.getChildren();
+    expect(labels[4].getCanvasBBox().maxX).not.toBeLessThanOrEqual(chart.getCoordinate().end.x);
+
+    chart.clear();
+    chart
+      .interval()
+      .position('country*population')
+      .label('population', {
+        position: 'right',
+        layout: [{ type: 'limit-in-plot', cfg: { action: 'ellipsis' } }],
+      });
+    chart.render();
+    labels = chart.geometries[0].labelsContainer.getChildren();
+    expect(labels[4].getCanvasBBox().maxX).toBeLessThanOrEqual(chart.getCoordinate().end.x);
+    expect((labels[4] as IGroup).find((shape) => shape.get('type') === 'text').attr('text')).toMatch('...');
+
+    chart.destroy();
   });
 });
