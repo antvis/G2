218fd939cda4529cc1e1db4eebbae1b59ba9b78a
zqlu <zqlu.cn@gmail.com>
2020-12-03 14:45:30 +0800

    docs: update site banner

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -13,34 +13,28 @@ const IndexPage = () => {
     {
       icon: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*4x_KTKyqwJgAAAAAAAAAAABkARQnAQ',
       title: t('千变万化，自由组合'),
-      description: t(
-        '任何图表，都可以基于图形语法灵活绘制，满足你无限的创意',
-      ),
+      description: t('任何图表，都可以基于图形语法灵活绘制，满足你无限的创意'),
     },
     {
       icon: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ELYbTIVCgPoAAAAAAAAAAABkARQnAQ',
       title: t('专业完备'),
-      description: t(
-        '大量产品实践之上，提供绘图引擎、完备图形语法、专业设计规范',
-      ),
+      description: t('大量产品实践之上，提供绘图引擎、完备图形语法、专业设计规范'),
     },
     {
       icon: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_riSQZrgczgAAAAAAAAAAABkARQnAQ',
       title: t('生动，可交互'),
-      description: t(
-        '强大的交互语法，助力可视分析，让图表栩栩如生',
-      ),
+      description: t('强大的交互语法，助力可视分析，让图表栩栩如生'),
     },
   ];
   const companies = [
     { name: '阿里云', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*V_xMRIvw2iwAAAAAAAAAAABkARQnAQ' },
-    { name: '支付宝', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*lYDrRZvcvD4AAAAAAAAAAABkARQnAQ', },
-    { name: '天猫', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*BQrxRK6oemMAAAAAAAAAAABkARQnAQ', },
-    { name: '淘宝网', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*1l8-TqUr7UcAAAAAAAAAAABkARQnAQ', },
-    { name: '网上银行', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ZAKFQJ5Bz4MAAAAAAAAAAABkARQnAQ', },
-    { name: '京东', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*yh-HRr3hCpgAAAAAAAAAAABkARQnAQ', },
-    { name: 'yunos', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_js7SaNosUwAAAAAAAAAAABkARQnAQ', },
-    { name: '菜鸟', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*TgV-RZDODJIAAAAAAAAAAABkARQnAQ', },
+    { name: '支付宝', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*lYDrRZvcvD4AAAAAAAAAAABkARQnAQ' },
+    { name: '天猫', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*BQrxRK6oemMAAAAAAAAAAABkARQnAQ' },
+    { name: '淘宝网', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*1l8-TqUr7UcAAAAAAAAAAABkARQnAQ' },
+    { name: '网上银行', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*ZAKFQJ5Bz4MAAAAAAAAAAABkARQnAQ' },
+    { name: '京东', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*yh-HRr3hCpgAAAAAAAAAAABkARQnAQ' },
+    { name: 'yunos', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*_js7SaNosUwAAAAAAAAAAABkARQnAQ' },
+    { name: '菜鸟', img: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*TgV-RZDODJIAAAAAAAAAAABkARQnAQ' },
   ];
   const bannerButtons = [
     {
@@ -60,7 +54,7 @@ const IndexPage = () => {
       title: t('精品 Gallery'),
       description: t('真实的数据可视化案例，我们将它们归纳为一个个故事性的设计模板，让用户达到开箱即用的效果。'),
       link: `/${i18n.language}/examples/gallery/line`,
-      image: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*hDrgRb7ma4EAAAAAAAAAAABkARQnAQ'
+      image: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*hDrgRb7ma4EAAAAAAAAAAABkARQnAQ',
     },
   ];
 
@@ -73,31 +67,28 @@ const IndexPage = () => {
     },
   ];
 
-
   return (
     <>
       <SEO title={t('G2 可视化引擎')} titleSuffix="AntV" lang={i18n.language} />
       <Banner
         coverImage={
-          <img width="100%" style={{ marginLeft: '100px', marginTop: '40px' }}
-            src="https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*wo_LToatmbwAAAAAAAAAAABkARQnAQ" />}
+          <img
+            width="100%"
+            style={{ marginLeft: '100px', marginTop: '40px' }}
+            src="https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*wo_LToatmbwAAAAAAAAAAABkARQnAQ"
+          />
+        }
         title={t('G2 可视化引擎')}
         description={t(
-          'G2 一套面向常规统计图表，以数据驱动的高交互可视化图形语法，具有高度的易用性和扩展性。使用 G2，你可以无需关注图表各种繁琐的实现细节，一条语句即可使用 Canvas 或 SVG 构建出各种各样的可交互的统计图表。',
+          'G2 一套面向常规统计图表，以数据驱动的高交互可视化图形语法，具有高度的易用性和扩展性。使用 G2，你可以无需关注图表各种繁琐的实现细节，一条语句即可使用 Canvas 或 SVG 构建出各种各样的可交互的统计图表。'
         )}
         buttons={bannerButtons}
         notifications={notifications}
-        className='banner'
+        className="banner"
       />
-      <Features
-        features={features}
-        style={{ width: '100%' }}
-      />
+      <Features features={features} style={{ width: '100%' }} />
       <Cases cases={cases} />
-      <Companies
-        title={t('感谢信赖')}
-        companies={companies}
-      />
+      <Companies title={t('感谢信赖 感谢使用')} companies={companies} />
     </>
   );
 };
