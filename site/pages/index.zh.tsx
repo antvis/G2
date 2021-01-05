e53f8d406b5ab94fe5c0f7dc78a06ddf45283862
hustcc <i@hust.cc>
2021-01-05 20:53:07 +0800

    docs: add mask documents (#3176)

--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -59,7 +59,7 @@ const IndexPage = () => {
       logo: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*-dLnTIexOxwAAAAAAAAAAABkARQnAQ',
       title: t('精品 Gallery'),
       description: t('真实的数据可视化案例，我们将它们归纳为一个个故事性的设计模板，让用户达到开箱即用的效果。'),
-      link: `/${i18n.language}/examples/gallery/line`,
+      link: `/${i18n.language}/examples/gallery`,
       image: 'https://gw.alipayobjects.com/mdn/rms_2274c3/afts/img/A*hDrgRb7ma4EAAAAAAAAAAABkARQnAQ'
     },
   ];
