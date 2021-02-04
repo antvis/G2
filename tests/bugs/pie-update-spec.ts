218e9354ffda49c37c107a9985a3b6bb4316ce21
simaq <sima.zhang1990@gmail.com>
2021-02-04 19:27:59 +0800

    fix: 修复饼图在动画更新过程中如果宽度变为 0 导致的问题

--- tests/bugs/pie-update-spec.ts
+++ tests/bugs/pie-update-spec.ts
@@ -157,4 +157,33 @@ describe('Pie update animation', () => {
       done();
     }, 500);
   });
+
+  it('pie update animation when width changed to 0.', () => {
+    const data = [
+      { item: '事例一', count: 40, percent: 0.4 },
+      { item: '事例二', count: 21, percent: 0.21 },
+      { item: '事例三', count: 17, percent: 0.17 },
+      { item: '事例四', count: 13, percent: 0.13 },
+      { item: '事例五', count: 9, percent: 0.09 },
+    ];
+
+    const chart = new Chart({
+      container: createDiv(),
+      width: 400,
+      height: 300,
+    });
+
+    chart.coordinate('theta', {
+      radius: 0.75,
+    });
+
+    chart.data(data);
+    const interval = chart.interval().position('percent').color('item').adjust('stack');
+    chart.render();
+    
+
+    expect(() => {
+      chart.changeSize(0, 300);
+    }).not.toThrow();
+  });
 });
