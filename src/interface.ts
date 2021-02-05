39e7b03bdcf2b8285bea7dec8adb4df3a863887d
rxs-michael <ruanxusong@thundersdata.com>
2021-02-05 10:46:17 +0800

    fix(): 修复 TooltipItem data 类型

--- src/interface.ts
+++ src/interface.ts
@@ -1197,9 +1197,9 @@ export type TooltipTitle = string | ((title: string, datum: Datum) => string);
 
 export type TooltipItem = {
   /** 原始数据 */
-  readonly data: Data; // 原始数据
+  readonly data: Datum; // 原始数据
   /** 映射之后的数据 */
-  readonly mappingData: Data; // 映射后的数据
+  readonly mappingData: Datum; // 映射后的数据
   /** tooltip item 中名称 */
   readonly name: string;
   /** tooltip item 中值 */
