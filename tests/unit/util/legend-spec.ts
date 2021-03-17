6aa4000ba095330e279e6202ab167acd21e9114a
阿福 <31396322+lxfu1@users.noreply.github.com>
2021-03-17 16:39:52 +0800

    feat: 单 view 支持默认图例选中 (#3327)
    * feat: 单 view 支持默认图例过滤
    
    * fix: 规范字段
    
    * fix: 补充单测
    
    Co-authored-by: liufu.lf <liufu.lf@antfin.com>

--- tests/unit/util/legend-spec.ts
+++ tests/unit/util/legend-spec.ts
@@ -115,4 +115,42 @@ describe('util legend', () => {
     expect(items[0].marker.style.stroke).toBe('red');
     expect(items[0].marker.style.lineWidth).toBe(4);
   });
+  it('legend selected', () => {
+    const data = [
+      { city: '杭州', value: 1654, type: 'year' },
+      { city: '杭州', value: 654, type: 'month' },
+      { city: '杭州', value: 154, type: 'week' },
+      { city: '上海', value: 4400, type: 'year' },
+      { city: '上海', value: 400, type: 'month' },
+      { city: '上海', value: 100, type: 'week' },
+      { city: '深圳', value: 5300, type: 'year' },
+      { city: '深圳', value: 2300, type: 'month' },
+      { city: '深圳', value: 300, type: 'week' },
+    ];
+    const chart = new Chart({
+      container: createDiv(),
+    });
+    chart.data(data);
+    chart.interval().position('city*value').color('type', ['green', 'yellow', 'red']).adjust('stack');
+    chart.legend('type', {
+      selected: {
+        year: true,
+        month: false,
+      },
+    });
+    chart.render();
+    const geometry = chart.geometries[0];
+    const { filters } = chart.getOptions();
+    const attr = geometry.getGroupAttributes()[0];
+    let items = getLegendItems(chart, geometry, attr, {}, {});
+    expect(filters.type).toBeDefined();
+    expect(items.length).toBe(3);
+    expect(items[0].unchecked).toBeFalsy();
+    expect(items[0].marker.style.fill).toBe('green');
+    expect(items[1].unchecked).toBeTruthy();
+    expect(items[1].marker.style.fill).toBe('yellow');
+    expect(items[2].unchecked).toBeFalsy();
+    expect(items[2].marker.style.fill).toBe('red');
+    expect(chart.getData().length).toBe(6);
+  });
 });
