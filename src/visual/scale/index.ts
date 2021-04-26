cc9107c33949f041e58ed0c935b0bd288df9b48f
hustcc <i@hust.cc>
2021-04-26 15:54:52 +0800

    chore: add scale

--- src/visual/scale/index.ts
+++ src/visual/scale/index.ts
@@ -1,11 +1,17 @@
+import { Linear, Band, Time, Ordinal, Log, Pow, Identity } from '@antv/scale';
+import { Indentity } from '../attribute';
+
+type Scale = any;
+type ScaleCfg = any;
+
 /**
  * G2.Scale 列定义的类型配置
  */
-export type ScaleCfg = {
+export type ScaleDefCfg = {
   /**
    * 字段类型区分
    */
-  type: 'linear' | 'cat' | 'time' | 'timeCat';
+  type: 'identity' | 'linear' | 'log' | 'pow' | 'cat' | 'category' | 'time' | 'timeCat';
   /**
    * 字段名称
    */
@@ -29,58 +35,78 @@ export type ScaleCfg = {
 };
 
 /**
- * // TODO 万木
  * 将 @antv/scale 进行针对于 G2 的二次包装，让使用更加容易方便！
  * 其实名字叫 FieldMeta 更好，但是考虑到 G2 旧版本的概念兼容，所以还是用重名的 Scale 命名，它的内部会包含一个真正的 scale 示例
  */
-export class Scale {
+export class ScaleDef {
+
+  /**
+   * 包含的 antv/scale 实例
+   */
+  private scale: Scale;
+
   /**
    * 传入的配置
    */
-  private options: ScaleCfg;
+  private cfg: ScaleDefCfg;
 
   /**
    * 构造函数
    * @param cfg
    */
-  constructor(cfg: ScaleCfg) {
-    // TODO create scale by type
-    // this.scale = xxx;
+  constructor(cfg: ScaleDefCfg) {
+    // create scale by type
+    this.scale = this.createScale(cfg);
   }
 
   /**
-   * 包含的 antv/scale 实例
+   * scale 的类型
    */
-  private scale;
+  public get type() {
+    return this.cfg.type;
+  }
 
   /**
    * 是否是线性连续 scale
    */
-  public isLinear() {}
+  public isLinear() {
+    return ['linear', 'log', 'pow'].includes(this.type);
+  }
 
   /**
    * 是否是离散的分类 scale
    */
-  public isCategory() {}
+  public isCategory() {
+    return ['cat', 'category', 'time', 'timeCat'].includes(this.type);
+  }
+
+  /**
+   * 是否是常量的 scale
+   */
+  public isIndentity() {
+    return this.type === 'identity';
+  }
 
   /**
    * 将值映射到值域
    * @param v
    */
   public map(v: any): any {
-    this.scale.map(v);
+    return this.scale.map(v);
   }
 
   /**
    * 将数据逆向映射会原始数据
    * @param v
    */
-  public invert(v: any): any {}
+  public invert(v: any): any {
+    return this.scale.invert(v);
+  }
 
   /**
    * 更新 antv/scale 配置和实例
    */
-  public update(cfg: Partial<ScaleCfg>) {
+  public update(cfg: Partial<ScaleDefCfg>) {
     this.scale.update(this.getAntVScaleCfg(cfg));
   }
 
@@ -88,7 +114,38 @@ export class Scale {
    * 从配置中，生成 antv scale 的配置！
    * @param cfg
    */
-  private getAntVScaleCfg(cfg: Partial<ScaleCfg>) {
+  private getAntVScaleCfg(cfg: Partial<ScaleDefCfg>): ScaleCfg {
+    // todo 抽出配置
     return cfg;
+  }
+
+  /**
+   * 创建具体的 scale 类型
+   * @param cfg
+   */
+  private createScale(cfg: ScaleDefCfg) {
+    const { type } = cfg;
+    const scaleCfg = this.getAntVScaleCfg(cfg);
+
+    // 针对不同的类型，创建不同的 scale
+    switch (type) {
+      case 'cat':
+      case 'category':
+        return new Band(scaleCfg);
+      case 'linear':
+        return new Linear(scaleCfg);
+      case 'log':
+        return new Log(scaleCfg);
+      case 'pow':
+        return new Pow(scaleCfg);
+      case 'timeCat':
+        return new Ordinal(scaleCfg);
+      case 'time':
+        return new Time(scaleCfg);
+      case 'identity':
+        return new Indentity(scaleCfg);
+      default:
+        return new Band(scaleCfg);
+    }
   }
 }
