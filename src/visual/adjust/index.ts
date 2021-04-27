925353c6d9c62c9afed9d289fb7011ebd281619a
hustcc <i@hust.cc>
2021-04-27 10:29:55 +0800

    feat(adjust): add module (#3399)

--- src/visual/adjust/index.ts
+++ src/visual/adjust/index.ts
@@ -1,1 +1,15 @@
-export { Stack } from './stack';
+/**
+ * Adjust 相关模块，没有人力梳理，暂时直接使用旧的模块，性能，功能上无考量！
+ */
+import { getAdjust, StackCfg, DodgeCfg, AdjustCfg } from '@antv/adjust';
+
+const Dodge = getAdjust('dodge');
+const Jitter = getAdjust('jitter');
+const Stack = getAdjust('stack');
+const Symmetric = getAdjust('symmetric');
+
+type JitterCfg = AdjustCfg;
+type SymmetricCfg = AdjustCfg;
+
+export { Dodge, Jitter, Stack, Symmetric };
+export { DodgeCfg, JitterCfg, StackCfg, SymmetricCfg };
