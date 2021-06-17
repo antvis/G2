9a8f5091382bc6894b03f81afe00645c85ad76cc
pearmini <subairui@icloud.com>
2021-06-17 11:48:08 +0800

    test(scale): add test case for pow

--- __tests__/unit/visual/scale/pow-spec.ts
+++ __tests__/unit/visual/scale/pow-spec.ts
@@ -1,10 +1,27 @@
-/**
- * 搬运之前的单侧：https://github.com/antvis/scale/tree/v0.3.x/tests/unit
- * 功能上查漏补缺
- */
+import { Pow } from '@antv/scale';
+import { ScaleDef } from '../../../../src/visual/scale/index';
 
-describe('todo', () => {
-  it('补全', () => {
-    expect(1).toBe(1);
+describe('pow scale', () => {
+  const scale = new ScaleDef({
+    min: 0,
+    max: 100,
+    exponent: 2,
+    type: 'pow',
+  });
+
+  it('should has pow instance', () => {
+    expect(scale.getOption('type')).toEqual('pow');
+    // @ts-ignore
+    expect(scale.scale).toBeInstanceOf(Pow);
+  });
+
+  it('should have expected defaults', () => {
+    expect(scale.getOption('min')).toEqual(0);
+    expect(scale.getOption('max')).toEqual(100);
+    expect(scale.getOption('exponent')).toEqual(2);
+  });
+
+  it('should get right ticks by getTickValues()', () => {
+    expect(scale.getTickValues()).toEqual([0, 20, 40, 60, 80, 100]);
   });
 });
