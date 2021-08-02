57ed7b3c7c3c7fc663d76728c6fbdd50c2a98e33
pearmini <subairui@icloud.com>
2021-08-02 16:54:15 +0800

    test(line): add test case for smooth line with extreme value

--- tests/bugs/2425-spec.ts
+++ tests/bugs/2425-spec.ts
@@ -2,31 +2,30 @@ import { Chart } from '../../src';
 import { createDiv } from '../util/dom';
 
 describe('#2425', () => {
-  const data = [
-    {
-      date: 1587631980000,
-      value: 96.017334,
-    },
-    {
-      date: 1587708180000,
-      value: 84.62146,
-    },
-    {
-      date: 1587708900000,
-      value: 9.502827,
-    },
-
-    {
-      date: 1587709800000,
-      value: 32.350388,
-    },
-    {
-      date: 1587819780000,
-      value: 60.33812,
-    },
-  ];
-
   test('smooth line should not overlap for extreme situation', () => {
+    const data = [
+      {
+        date: 1587631980000,
+        value: 96.017334,
+      },
+      {
+        date: 1587708180000,
+        value: 84.62146,
+      },
+      {
+        date: 1587708900000,
+        value: 9.502827,
+      },
+
+      {
+        date: 1587709800000,
+        value: 32.350388,
+      },
+      {
+        date: 1587819780000,
+        value: 60.33812,
+      },
+    ];
     const chart = new Chart({
       container: createDiv(),
       width: 786,
@@ -66,6 +65,79 @@ describe('#2425', () => {
         770.3503198242188,
         193.99731119999996,
       ],
+    ]);
+  });
+
+  test('smooth line should draw the correctly extreme value', () => {
+    const data = [
+      {
+        date: 1587631980000,
+        value: 10,
+      },
+      {
+        date: 1587708180000,
+        value: 20,
+      },
+      {
+        date: 1587708900000,
+        value: 45,
+      },
+
+      {
+        date: 1587789800000,
+        value: 45,
+      },
+      {
+        date: 1587819780000,
+        value: 10,
+      },
+    ];
+    const chart = new Chart({
+      container: createDiv(),
+      width: 786,
+      height: 500,
+    });
+
+    chart.data(data);
+
+    chart.scale({
+      date: {
+        type: 'time',
+        range: [0.02, 0.98],
+        mask: 'YY/MM/DD HH:mm:ss',
+        tickCount: 8,
+      },
+      value: {
+        min: 0,
+        sync: true,
+        nice: true,
+      },
+    });
+
+    const path = chart.path().position('date*value').shape('smooth').size(3);
+    chart.render();
+    expect(path.elements[0].shape.attr('path')).toEqual([
+      ['M', 36.6271142578125, 385.2],
+      ['C', 36.6271142578125, 385.2, 330.57491054252716, 365.863588147504, 334.28174759447387, 290.4],
+      [
+        'C',
+        337.09423231891475,
+        233.14358814750403,
+        334.28174759447387,
+        53.39999999999998,
+        337.09423231891475,
+        53.39999999999998,
+      ],
+      [
+        'C',
+        340.8441129739034,
+        53.39999999999998,
+        571.1329597311636,
+        53.39999999999998,
+        653.1081409401208,
+        53.39999999999998,
+      ],
+      ['C', 744.3820187567227, 53.39999999999998, 770.2168798828125, 385.2, 770.2168798828125, 385.2],
     ]);
   });
 });
