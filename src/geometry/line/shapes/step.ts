b3fac155a6108591953e8dcc1fb2d60d07379a22
pearmini <subairui@icloud.com>
2021-08-05 21:10:04 +0800

    chore(line): add comment for wrong g types

--- src/geometry/line/shapes/step.ts
+++ src/geometry/line/shapes/step.ts
@@ -13,6 +13,7 @@ each(['hv', 'vh', 'hvh', 'vhv'], (shapeType) => {
     draw(cfg: ShapeInfo, container: Group) {
       const attrs = getInterpolateShapeAttrs(cfg, shapeType);
 
+      // TODO: 等 g 的类型定义修复之后再修复这个地方
       const shape = new Path({
         // @ts-ignore
         attrs,
