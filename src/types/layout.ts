4c0697d2d0736f0d3ad16e67ee61e0b63f548c5c
hustcc <i@hust.cc>
2021-08-06 14:46:03 +0800

    feat(view): layout with constraint (#3548)
    * feat(view): layout with constraint
    
    * chore: add comments
    
    * chore: add id for geometry

---
+++ src/types/layout.ts
@@ -0,0 +1,26 @@
+/**
+ * 布局中的元素元信息
+ */
+export type UnitMeta = {
+  /** 元素的分类 */
+  type: string;
+  /**
+   * 额外的信息存储
+   */
+  extra: any;
+  /**
+   * 具体的组件、图形
+   */
+  unit: any;
+}
+
+/**
+ * 元素布局信息
+ * // TODO 需要依赖约束布局的模块定义
+ */
+export type UnitVariable = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
