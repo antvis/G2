b6e88bb3dca2a9aeced9719fbade7360d7fa75e4
MiniPear <pearminipro@gmail.com>
2021-09-15 19:47:43 +0800

    chore/dep (#3623)
    * chore(husky): migrate from 4.x to 7.x
    
    * chore(package.json): update @commitlint/cli and @commitlint/config-angular to 13.x
    
    * chore(package.json): update @types/jest to 27.x
    
    * chore(package.json): update core-js to 3.x
    
    * chore(package.json): update gh-pages to 3.x
    
    * chore(package.json): update ts-loader to 9.x
    
    * chore(package.json): downgrade jest to 25.x
    
    * chore(package.json): remove core-js
    
    * chore(package.json): replace tslint with eslint
    
    * chore(package.json): update lint-staged to 11.x
    
    * chore(package.json): replace __tests__ with tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const webpack = require('webpack');
 const resolve = require('path').resolve;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
