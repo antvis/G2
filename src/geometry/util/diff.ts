9cecfd2e0e0d5241da3974f8d18ff00e13329a56
pearmini <subairui@icloud.com>
2021-09-16 22:26:23 +0800

    test(diff): add test case for diff

--- src/geometry/util/diff.ts
+++ src/geometry/util/diff.ts
@@ -1,3 +1,9 @@
+/**
+ * 对比当前元素和之前的元素，返回 added, updated, removed
+ * @param keyItem 之前的元素的，按照 key-item 的 object 的形式存储
+ * @param keys 现在的元素，按照 array 的形式存储
+ * @returns 由 added, updated, removed array 构成的 object
+ */
 export function diff(keyItem: Record<string, any>, keys: string[]) {
   const added = [];
   const updated = [];
@@ -6,11 +12,8 @@ export function diff(keyItem: Record<string, any>, keys: string[]) {
 
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    if (keyItem[key]) {
-      updated.push(key);
-    } else {
-      added.push(key);
-    }
+    if (keyItem[key]) updated.push(key);
+    else added.push(key);
     keyIncluded.set(key, true);
   }
 
