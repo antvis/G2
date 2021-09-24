ae14a7f2c6a82d9ec664d47036c249b79384499d
pearmini <subairui@icloud.com>
2021-09-24 16:18:02 +0800

    fix(crash): remove -apple-system font and lock normalize-url to 4.x close(#886)

--- examples/column/basic/demo/basic.ts
+++ examples/column/basic/demo/basic.ts
@@ -1,14 +1,14 @@
 import { Chart } from '@antv/g2';
 
 const data = [
-  { year: '1951 年', sales: 38 },
-  { year: '1952 年', sales: 52 },
-  { year: '1956 年', sales: 61 },
-  { year: '1957 年', sales: 145 },
-  { year: '1958 年', sales: 48 },
-  { year: '1959 年', sales: 38 },
-  { year: '1960 年', sales: 38 },
-  { year: '1962 年', sales: 38 },
+  { year: '1951', sales: 38 },
+  { year: '1952', sales: 52 },
+  { year: '1956', sales: 61 },
+  { year: '1957', sales: 145 },
+  { year: '1958', sales: 48 },
+  { year: '1959', sales: 38 },
+  { year: '1960', sales: 38 },
+  { year: '1962', sales: 38 },
 ];
 const chart = new Chart({
   container: 'container',
@@ -17,14 +17,6 @@ const chart = new Chart({
 });
 
 chart.data(data);
-chart.scale('sales', {
-  nice: true,
-});
-
-chart.tooltip({
-  showMarkers: false
-});
-chart.interaction('active-region');
 
 chart.interval().position('year*sales');
 
