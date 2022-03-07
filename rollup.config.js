441041cd7dda15d6ef6dfb581ecf5e9634db3543
pearmini <subairui@icloud.com>
2022-03-07 16:08:56 +0800

    chore: build success with @antv/g

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,17 @@
 import { uglify } from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 
-module.exports = [{
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/template.min.js',
-    name: 'tplv',
-    format: 'umd',
-    sourcemap: false,
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/template.min.js',
+      name: 'tplv',
+      format: 'umd',
+      sourcemap: false,
+    },
+    plugins: [resolve(), commonjs(), typescript(), uglify()],
   },
-  plugins: [
-    resolve(),
-    typescript(),
-    uglify(),
-  ],
-}];
+];
