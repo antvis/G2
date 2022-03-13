53162dd875875673d4533a90d61f8438f929764b
pearmini <subairui@icloud.com>
2022-03-13 13:48:50 +0800

    feat(runtime): add part of light theme component

---
+++ src/theme/light.ts
@@ -0,0 +1,14 @@
+import { ThemeComponent as TC, Theme } from '../runtime';
+
+export type LightOptions = Theme;
+
+export const Light: TC<LightOptions> = (options) => {
+  const defaultOptions: Theme = {
+    defaultColor: '#5B8FF9',
+    defaultCategory10: 'category10',
+    defaultCategory20: 'category20',
+  };
+  return Object.assign({}, defaultOptions, options);
+};
+
+Light.props = {};
