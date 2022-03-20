539b6214337276ebd4e743249b363c530f31113b
pearmini <subairui@icloud.com>
2022-03-20 17:09:00 +0800

    chore: add nodePolyfills to build sucessfully

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,17 @@ import { uglify } from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 module.exports = [
   {
     input: 'src/index.ts',
     output: {
-      file: 'dist/template.min.js',
-      name: 'tplv',
+      file: 'dist/g2.min.js',
+      name: 'G2',
       format: 'umd',
       sourcemap: false,
     },
-    plugins: [resolve(), commonjs(), typescript(), uglify()],
+    plugins: [nodePolyfills(), resolve(), commonjs(), typescript(), uglify()],
   },
 ];
