922cd96a8868460d99c73bd9760c95ad46eff798
pearmini <subairui@icloud.com>
2022-03-27 15:37:44 +0800

    test(scale): add test suite for point and time

--- __tests__/unit/scale/index.spec.ts
+++ __tests__/unit/scale/index.spec.ts
@@ -3,8 +3,17 @@ import {
   Band as BandScale,
   Ordinal as OrdinalScale,
   Identity as IdentityScale,
+  Point as PointScale,
+  Time as TimeScale,
 } from '@antv/scale';
-import { Linear, Band, Ordinal, Identity } from '../../../src/scale';
+import {
+  Linear,
+  Band,
+  Ordinal,
+  Identity,
+  Point,
+  Time,
+} from '../../../src/scale';
 
 describe('scale', () => {
   it('Scale({..}) returns @antv/scale instance', () => {
@@ -12,5 +21,7 @@ describe('scale', () => {
     expect(Band()).toBeInstanceOf(BandScale);
     expect(Ordinal()).toBeInstanceOf(OrdinalScale);
     expect(Identity()).toBeInstanceOf(IdentityScale);
+    expect(Point()).toBeInstanceOf(PointScale);
+    expect(Time()).toBeInstanceOf(TimeScale);
   });
 });
