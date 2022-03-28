4ca44ff991ce58cc3468834457400edee2082caa
jihaina <1748420480@qq.com>
2022-03-28 15:49:48 +0800

    feat: test

--- __tests__/unit/transform/pick.spec.ts
+++ __tests__/unit/transform/pick.spec.ts
@@ -9,7 +9,7 @@ describe('pick', () => {
       { a: 4, b: 1, c: 2 },
     ];
 
-    const p1 = Pick({ fields: ['a', 'b'] });
+    const p1 = Pick({ fields: ['a', 'b', 'd'] });
     expect(p1(data)).toEqual([
       { a: 1, b: 2 },
       { a: 2, b: 3 },
