35f9a9a8d3d786d6faa92ceeae9b211d4c535123
jihaina <1748420480@qq.com>
2022-03-28 13:47:30 +0800

    chore: comment

--- src/transform/pick.ts
+++ src/transform/pick.ts
@@ -6,7 +6,7 @@ import { useMemoTransform } from './utils';
 export type PickOptions = Omit<PickTransform, 'type'>;
 
 /**
- * Immutable data sort by specified fields.
+ * Immutable data pick by specified fields.
  */
 export const Pick: TC<PickOptions> = (options) => {
   const { fields: F = [] } = options;
@@ -28,25 +28,3 @@ export const Pick: TC<PickOptions> = (options) => {
 };
 
 Pick.props = {};
-
-/**
-  数据不规整的情况？
-const data = [
-  { x: 1, y: 11 },
-  { x: 2, y: 12 },
-  { y: 13 },
-  { x: 4, y: 14 },
-  { x: 5, y: 15 },
-];
-
-Pick({ fields: ['x'] })(data);
-/**
-  result: [
-    { x: 1 },
-    { x: 2 },
-    { x: 3 },
-    { x: 4 },
-    { x: 5 }
-  ]
-
- */
