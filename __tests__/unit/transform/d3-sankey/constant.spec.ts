b9b81a81ee7de22d4658df1caeec407639354907
visiky <736929286@qq.com>
2022-04-01 10:44:42 +0800

    Update constant.spec.ts

--- __tests__/unit/transform/d3-sankey/constant.spec.ts
+++ __tests__/unit/transform/d3-sankey/constant.spec.ts
@@ -1,7 +1,7 @@
 import { constant } from '../../../../src/transform/utils/d3-sankey/constant';
 
-describe('constant(...) returns a function returning a constant value', () => {
-  it('', () => {
+describe('constant', () => {
+  it('constant(...) returns a function returning a constant value', () => {
     expect(typeof constant('x')).toBe('function');
     expect(constant('x')()).toBe('x');
     expect(constant('1')()).toBe('1');
