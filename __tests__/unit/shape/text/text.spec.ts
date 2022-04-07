9031fa39b3d9851fa907725ba92d50cc0c0718e6
visiky <736929286@qq.com>
2022-04-07 09:56:58 +0800

    feat(shape): add text shape and test cases

---
+++ __tests__/unit/shape/text/text.spec.ts
@@ -0,0 +1,122 @@
+import { Text } from '../../../../src/shape';
+import { mount, createDiv } from '../../../utils/dom';
+import { draw, style } from '../helper';
+
+describe('Text', () => {
+  it('Text has expected defaults', () => {
+    expect(Text.props).toEqual({
+      defaultEnterAnimation: 'fadeIn',
+    });
+  });
+
+  it('Text() returns a function draw text', () => {
+    const shape = Text({})([[0, 0]], {}, null, { defaultColor: 'red' });
+    expect(shape.style.fill).toBe('red');
+  });
+
+  it('Text() returns a function draw text using color as fill and stroke', () => {
+    const container = document.createElement('div');
+    const shape = draw({
+      width: 150,
+      height: 50,
+      shape: Text({}),
+      container,
+      value: {
+        text: 'hello',
+        color: 'steelblue',
+      },
+      vectors: [[0.5, 0.5]],
+    });
+    mount(createDiv(), container);
+
+    expect(shape.nodeName).toBe('text');
+    expect(style(shape, ['x', 'y'])).toEqual({ x: 75, y: 25 });
+    expect(shape.style.fontSize).toEqual(14);
+    expect(style(shape, ['fill', 'stroke', 'lineWidth'])).toEqual({
+      fill: 'steelblue',
+      stroke: 'steelblue',
+      lineWidth: 0,
+    });
+  });
+
+  it('Text({...}) returns a function draw text with custom style', () => {
+    const container = document.createElement('div');
+    const shape = draw({
+      width: 150,
+      height: 50,
+      shape: Text({
+        fill: 'red',
+        fontSize: 18,
+        textAlign: 'right',
+        textTransform: 'uppercase',
+        lineWidth: 1,
+      }),
+      container,
+      value: {
+        text: 'hello',
+        color: 'steelblue',
+      },
+      vectors: [[0.5, 0.5]],
+    });
+    mount(createDiv(), container);
+
+    expect(shape.nodeName).toBe('text');
+    expect(
+      style(shape, [
+        'fill',
+        'stroke',
+        'steelblue',
+        'textAlign',
+        'textTransform',
+        'lineWidth',
+      ]),
+    ).toMatchObject({
+      fill: 'red',
+      stroke: 'steelblue',
+      textAlign: 'right',
+      textTransform: 'uppercase',
+      lineWidth: 1,
+    });
+
+    expect(shape.style.text).toEqual('HELLO');
+  });
+
+  it('Text({...}) returns a function draw text which enable custom wordWrap', () => {
+    const container = document.createElement('div');
+    let shape = draw({
+      width: 150,
+      height: 50,
+      shape: Text({
+        wordWrap: false,
+        wordWrapWidth: 8,
+        textAlign: 'left',
+      }),
+      container,
+      value: {
+        text: 'Good afternoon, everybody',
+        color: 'steelblue',
+      },
+      vectors: [[0.5, 0.5]],
+    });
+    expect(shape.getBoundingClientRect().width).toBeGreaterThan(80);
+    shape = draw({
+      width: 150,
+      height: 250,
+      shape: Text({
+        wordWrap: true,
+        wordWrapWidth: 80,
+        textAlign: 'left',
+      }),
+      container,
+      value: {
+        text: 'Good afternoon, everybody',
+        color: 'steelblue',
+      },
+      vectors: [[0.5, 0.5]],
+    });
+    expect(shape.getBoundingClientRect().width).not.toBeGreaterThan(80);
+    // @ts-ignore
+    expect(shape.getLineBoundingRects().length).toBeGreaterThan(1);
+    mount(createDiv(), container);
+  });
+});
---
+++ src/shape/text/text.ts
@@ -0,0 +1,38 @@
+import { Text as GText } from '@antv/g';
+import { ShapeComponent as SC } from '../../runtime';
+import { applyStyle } from '../../shape/utils';
+import { select } from '../../utils/selection';
+
+export type TextOptions = Record<string, any>;
+
+/**
+ * todo autoRotate when in polar coordinate
+ */
+export const Text: SC<TextOptions> = (options) => {
+  const { ...style } = options;
+  return (points, value, coordinate, theme) => {
+    const { defaultColor } = theme;
+    const {
+      color = defaultColor,
+      text = '',
+      fontSize = 14,
+      rotate = 0,
+    } = value;
+    const [[x0, y0]] = points;
+
+    return select(new GText())
+      .style('x', x0)
+      .style('y', y0)
+      .style('text', String(text))
+      .style('stroke', color)
+      .style('fill', color)
+      .style('fontSize', fontSize as any)
+      .style('transform', `rotate(${+rotate})`)
+      .call(applyStyle, style)
+      .node();
+  };
+};
+
+Text.props = {
+  defaultEnterAnimation: 'fadeIn',
+};
