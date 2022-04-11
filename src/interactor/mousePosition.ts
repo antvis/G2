3c82a380c0085efd3926b3e0919a0a2b155170a2
pearmini <subairui@icloud.com>
2022-04-11 16:37:14 +0800

    feat(interactor): add mousePosition and touchPosition

---
+++ src/interactor/mousePosition.ts
@@ -0,0 +1,13 @@
+import { InteractorComponent as IC } from '../runtime';
+
+export type MousePositionPotions = Record<string, unknown>;
+
+export const MousePosition: IC<MousePositionPotions> = () => {
+  return {
+    actions: [
+      { action: 'enter', events: ['plot:mouseenter'] },
+      { action: 'hover', events: ['plot:mousemove'] },
+      { action: 'leave', events: ['plot:mouseleave'] },
+    ],
+  };
+};
