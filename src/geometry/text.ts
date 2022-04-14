db845c6dbc8c216507649a1cbd3c5ebeb20cf8ce
pearmini <subairui@icloud.com>
2022-04-14 19:04:56 +0800

    feat(geometry): add offset for text

--- src/geometry/text.ts
+++ src/geometry/text.ts
@@ -7,8 +7,12 @@ export type TextOptions = Omit<TextGeometry, 'type'>;
 export const Text: MC<TextOptions> = () => {
   return (index, scale, value, coordinate) => {
     const { x: X, y: Y } = value;
+    const { x } = scale;
     const P = Array.from(index, (i) => {
-      return [coordinate.map([+X[i][0], +Y[i][0]])] as Vector2[];
+      const px = +X[i][0];
+      const py = +Y[i][0];
+      const xoffset = x?.getBandWidth?.() || 0;
+      return [coordinate.map([px + xoffset / 2, py])] as Vector2[];
     });
     return [index, P];
   };
