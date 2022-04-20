facea95c518473c6012e624c13ff3b4ee2e38e74
MiniPear <subairui@icloud.com>
2022-04-20 16:19:52 +0800

    fix(fetch): add useMemoConnector to memorize fetch (#3885)

--- __tests__/unit/transform/fetch.spec.ts
+++ __tests__/unit/transform/fetch.spec.ts
@@ -15,7 +15,7 @@ describe('Fetch', () => {
     ]);
   });
 
-  it('Fetch({...} returns a function calling  callback on each datum', async () => {
+  it('Fetch({...} returns a function calling callback on each datum', async () => {
     const transform = Fetch({
       url: 'https://gw.alipayobjects.com/os/bmw-prod/ce45e3d7-ba78-4a08-b411-28df40ef9b7f.json',
       callback: ({ sold, ...rest }) => ({ sold: `${sold}`, ...rest }),
