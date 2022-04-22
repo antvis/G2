0df086be90e617a49137903a02cc36f839cc1f28
pearmini <subairui@icloud.com>
2022-04-22 20:52:36 +0800

    feat(adjust): add pack

---
+++ src/adjust/pack.ts
@@ -0,0 +1,57 @@
+import { AdjustComponent as AC } from '../runtime';
+import { calcBBox } from '../utils/vector';
+
+export type PackOptions = Record<string, unknown>;
+
+/**
+ * Uniform pack to avid overlap.
+ * @todo Improve or change algorithm to increase space usage.
+ * @todo Take some special case into account.
+ */
+export const Pack: AC<PackOptions> = () => {
+  return (P, layout) => {
+    // col * row >= count
+    // row is close to col * aspect, so
+    // col * (col * aspect) >= count
+    const { innerWidth, innerHeight } = layout;
+    const count = P.length;
+    const aspect = innerHeight / innerWidth;
+    let col = Math.ceil(Math.sqrt(count / aspect));
+
+    // Increase col to avoid total height of packed shape
+    // being large than height of bbox.
+    let size = innerWidth / col;
+    let row = Math.ceil(count / col);
+    let h0 = row * size;
+    while (h0 > innerHeight) {
+      col = col + 1;
+      size = innerWidth / col;
+      row = Math.ceil(count / col);
+      h0 = row * size;
+    }
+
+    const space = innerHeight - row * size;
+    return P.map((points, m) => {
+      const [x, y, width, height] = calcBBox(points);
+      const cx = x + width / 2;
+      const cy = y + height / 2;
+
+      const i = m % col;
+      const j = Math.floor(m / col);
+
+      const newCX = i * size + size / 2;
+      const newCY = (row - j - 1) * size + space + size / 2;
+
+      const sx = size / width;
+      const sy = size / height;
+
+      // Translate the shape and mark to make sure the center of
+      // shape is overlap before and after scale transformation.
+      const tx = newCX - cx;
+      const ty = newCY - cy;
+      return `translate(${tx}, ${ty}) scale(${sx}, ${sy})`;
+    });
+  };
+};
+
+Pack.props = {};
