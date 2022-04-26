50b319cfe715d0c94207d74261a1a41e3855cbb8
pearmini <subairui@icloud.com>
2022-04-26 12:54:58 +0800

    feat(composition): update flex, view and layer

--- src/composition/view.ts
+++ src/composition/view.ts
@@ -1,13 +1,21 @@
+import { deepMix } from '@antv/util';
 import { CompositionComponent as CC } from '../runtime';
 import { ViewComposition } from '../spec';
 
 export type ViewOptions = Omit<ViewComposition, 'type'>;
 
+/**
+ * @todo Propagate more options to children.(e.g. filter)
+ */
 export const View: CC<ViewOptions> = () => {
   return (options) => {
-    const { children = [], data: viewData, ...rest } = options;
-    const marks = children.map(({ data = viewData, ...rest }) => ({
+    const { children, ...restOptions } = options;
+    if (!Array.isArray(children)) return [];
+
+    const { data: viewData, scale: viewScale, ...rest } = restOptions;
+    const marks = children.map(({ data = viewData, scale, ...rest }) => ({
       data,
+      scale: deepMix(viewScale, scale),
       ...rest,
     }));
     return [{ ...rest, marks, type: 'standardView' }];
