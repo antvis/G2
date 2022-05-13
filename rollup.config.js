81939abd4e934a9bb4eeff8de4af075e0be0b811
hustcc <i@hust.cc>
2022-05-13 11:43:34 +0800

    chore: add bundle analysis visualizer (#3916)

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const isBundleVis = !!process.env.BUNDLE_VIS;
 
 module.exports = [
   {
@@ -13,6 +16,13 @@ module.exports = [
       format: 'umd',
       sourcemap: false,
     },
-    plugins: [nodePolyfills(), resolve(), commonjs(), typescript(), uglify()],
+    plugins: [
+      nodePolyfills(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      uglify(),
+      ...(isBundleVis ? [visualizer()] : []),
+    ],
   },
 ];
