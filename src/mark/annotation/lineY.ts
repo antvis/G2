d7cf76e794cd896feacb0829fcbf585b324b2ece
visiky <736929286@qq.com>
2022-05-16 15:43:21 +0800

    fix: fix merge conflict

--- src/mark/annotation/lineY.ts
+++ src/mark/annotation/lineY.ts
@@ -27,11 +27,8 @@ LineY.props = {
     { name: 'key', scale: 'identity' },
     { name: 'x', required: true },
     { name: 'shape' },
-<<<<<<< HEAD
     { name: 'size' },
     { name: 'color' },
-=======
->>>>>>> ea25c31fa (feat(annotation): support lineX, lineY annotation)
   ],
   infer: [{ type: 'maybeTuple' }],
   shapes: ['annotation.line'],
