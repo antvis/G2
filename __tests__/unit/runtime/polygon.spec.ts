ea4a865861d82a9390e7d8b6f686a7f832aeb681
hustcc <i@hust.cc>
2022-05-19 11:41:02 +0800

    chore: use ordial scale type

--- __tests__/unit/runtime/polygon.spec.ts
+++ __tests__/unit/runtime/polygon.spec.ts
@@ -27,19 +27,7 @@ describe('polygon', () => {
       },
       scale: {
         color: {
-          type: 'quantile',
-          range: [
-            '#5B8FF9',
-            '#5AD8A6',
-            '#5D7092',
-            '#F6BD16',
-            '#6F5EF9',
-            '#6DC8EC',
-            '#945FB9',
-            '#FF9845',
-            '#1E9493',
-            '#FF99C3',
-          ],
+          type: 'ordinal',
         },
       },
     });
@@ -72,19 +60,7 @@ describe('polygon', () => {
       },
       scale: {
         color: {
-          type: 'quantile',
-          range: [
-            '#5B8FF9',
-            '#5AD8A6',
-            '#5D7092',
-            '#F6BD16',
-            '#6F5EF9',
-            '#6DC8EC',
-            '#945FB9',
-            '#FF9845',
-            '#1E9493',
-            '#FF99C3',
-          ],
+          type: 'ordinal',
         },
       },
       coordinate: [{ type: 'polar' }],
