0494ba7857236a3e87731630578abab1af11d74e
hustcc <i@hust.cc>
2022-05-19 11:44:33 +0800

    feat: add vh, hv, hvh shape for line geometry (#3928)

--- __tests__/unit/runtime/line.spec.ts
+++ __tests__/unit/runtime/line.spec.ts
@@ -323,4 +323,76 @@ describe('line', () => {
 
     mount(createDiv(), chart);
   });
+
+  it('render({...}) should render basic line chart with hv shape', () => {
+    const chart = render<G2Spec>({
+      type: 'line',
+      data: [
+        { year: '1991', value: 15468 },
+        { year: '1992', value: 16100 },
+        { year: '1993', value: 15900 },
+        { year: '1994', value: 17409 },
+        { year: '1995', value: 17000 },
+        { year: '1996', value: 31056 },
+        { year: '1997', value: 31982 },
+        { year: '1998', value: 32040 },
+        { year: '1999', value: 33233 },
+      ],
+      encode: {
+        x: 'year',
+        y: 'value',
+        shape: 'hv',
+      },
+    });
+
+    mount(createDiv(), chart);
+  });
+
+  it('render({...}) should render basic line chart with vh shape', () => {
+    const chart = render<G2Spec>({
+      type: 'line',
+      data: [
+        { year: '1991', value: 15468 },
+        { year: '1992', value: 16100 },
+        { year: '1993', value: 15900 },
+        { year: '1994', value: 17409 },
+        { year: '1995', value: 17000 },
+        { year: '1996', value: 31056 },
+        { year: '1997', value: 31982 },
+        { year: '1998', value: 32040 },
+        { year: '1999', value: 33233 },
+      ],
+      encode: {
+        x: 'year',
+        y: 'value',
+        shape: 'vh',
+      },
+    });
+
+    mount(createDiv(), chart);
+  });
+
+  it('render({...}) should render basic line chart with hvh shape', () => {
+    const chart = render<G2Spec>({
+      type: 'line',
+      data: [
+        { year: '1991', value: 15468 },
+        { year: '1992', value: 16100 },
+        { year: '1993', value: 15900 },
+        { year: '1994', value: 17409 },
+        { year: '1995', value: 17000 },
+        { year: '1996', value: 31056 },
+        { year: '1997', value: 31982 },
+        { year: '1998', value: 32040 },
+        { year: '1999', value: 33233 },
+      ],
+      encode: {
+        x: 'year',
+        y: 'value',
+        shape: 'hvh',
+      },
+    });
+
+    mount(createDiv(), chart);
+  });
 });
