63b3c8e6166a59510c8dfb532f44d4ac91e8b409
hustcc <i@hust.cc>
2022-06-09 19:51:20 +0800

    chore: refactor for code review

--- src/shape/interval/funnel.ts
+++ src/shape/interval/funnel.ts
@@ -4,7 +4,11 @@ import { ShapeComponent as SC, Vector2 } from '../../runtime';
 import { ColorRect } from './colorRect';
 
 export type FunnelOptions = {
-  isPyramid?: boolean;
+  adjustPoints?: (
+    points: Vector2[],
+    nextPoints: Vector2[],
+    coordinate: Coordinate,
+  ) => Vector2[];
   [key: string]: any;
 };
 
@@ -15,65 +19,28 @@ function getFunnelPoints(
   points: Vector2[],
   nextPoints: Vector2[],
   coordinate: Coordinate,
-  isPyramid: boolean,
 ) {
-  // eslint-disable-next-line
-  let [p0, p1, p2, p3] = points;
+  const [p0, p1, p2, p3] = points;
 
   if (isTranspose(coordinate)) {
-    p1 = [
-      nextPoints
-        ? nextPoints[0][0]
-        : isPyramid
-        ? (points[1][0] + points[2][0]) / 2
-        : points[1][0],
-      points[1][1],
-    ];
-    p2 = [
-      nextPoints
-        ? nextPoints[3][0]
-        : isPyramid
-        ? (points[1][0] + points[2][0]) / 2
-        : points[2][0],
-      points[2][1],
-    ];
-  } else {
-    // Other case.
-    p1 = [
-      points[1][0],
-      nextPoints
-        ? nextPoints[0][1]
-        : isPyramid
-        ? (points[1][1] + points[2][1]) / 2
-        : points[1][1],
-    ];
-    p2 = [
-      points[2][0],
-      nextPoints
-        ? nextPoints[3][1]
-        : isPyramid
-        ? (points[1][1] + points[2][1]) / 2
-        : points[2][1],
-    ];
+    const newP1 = [nextPoints ? nextPoints[0][0] : p1[0], p1[1]] as Vector2;
+    const newP2 = [nextPoints ? nextPoints[3][0] : p2[0], p2[1]] as Vector2;
+    return [p0, newP1, newP2, p3];
   }
-
-  return [p0, p1, p2, p3];
+  const newP1 = [p1[0], nextPoints ? nextPoints[0][1] : p1[1]] as Vector2;
+  const newP2 = [p2[0], nextPoints ? nextPoints[3][1] : p2[1]] as Vector2;
+  return [p0, newP1, newP2, p3];
 }
 
 /**
  * Render funnel in different coordinate and using color channel for stroke and fill attribute.
  */
 export const Funnel: SC<FunnelOptions> = (options) => {
-  const { isPyramid, ...style } = options;
+  const { adjustPoints = getFunnelPoints, ...style } = options;
   return (points, value, coordinate, theme, point2d, ...args) => {
     const { index } = value;
     const nextPoints = point2d[index + 1];
-    const funnelPoints = getFunnelPoints(
-      points,
-      nextPoints,
-      coordinate,
-      isPyramid,
-    );
+    const funnelPoints = adjustPoints(points, nextPoints, coordinate);
 
     return ColorRect({ colorAttribute: 'fill', ...style })(
       funnelPoints,
--- src/shape/interval/pyramid.ts
+++ src/shape/interval/pyramid.ts
@@ -7,10 +7,42 @@ import { Funnel } from './funnel';
 export type PyramidOptions = Record<string, any>;
 
 /**
+ * Adjust and return the new `points`.
+ */
+function getPyramidPoints(
+  points: Vector2[],
+  nextPoints: Vector2[],
+  coordinate: Coordinate,
+) {
+  const [p0, p1, p2, p3] = points;
+
+  if (isTranspose(coordinate)) {
+    const newP1 = [
+      nextPoints ? nextPoints[0][0] : (p1[0] + p2[0]) / 2,
+      p1[1],
+    ] as Vector2;
+    const newP2 = [
+      nextPoints ? nextPoints[3][0] : (p1[0] + p2[0]) / 2,
+      p2[1],
+    ] as Vector2;
+    return [p0, newP1, newP2, p3];
+  }
+  const newP1 = [
+    p1[0],
+    nextPoints ? nextPoints[0][1] : (p1[1] + p2[1]) / 2,
+  ] as Vector2;
+  const newP2 = [
+    p2[0],
+    nextPoints ? nextPoints[3][1] : (p1[1] + p2[1]) / 2,
+  ] as Vector2;
+  return [p0, newP1, newP2, p3];
+}
+
+/**
  * Render pyramid in different coordinate and using color channel for stroke and fill attribute.
  */
 export const Pyramid: SC<PyramidOptions> = (options) => {
-  return Funnel({ isPyramid: true, ...options });
+  return Funnel({ adjustPoints: getPyramidPoints, ...options });
 };
 
 Pyramid.props = {
