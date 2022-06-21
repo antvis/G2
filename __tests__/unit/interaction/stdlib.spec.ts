7153fb4583e5e24dd68a5eec06f4305b92b21e42
visiky <736929286@qq.com>
2022-06-21 13:28:54 +0800

    feat(interaction): support element-highlight interactions (#3992)
    * feat(interaction): support element-highlight interacions
    
    - service: elementSelection
    - transfromer: highlight
    - interactions: elementHighlight, elementHighlightByX, elementHighlightByColor
    
    * fix: fix cr suggestion

--- __tests__/unit/interaction/stdlib.spec.ts
+++ __tests__/unit/interaction/stdlib.spec.ts
@@ -5,6 +5,8 @@ import {
   Tooltip,
   FisheyeFocus,
   Plot,
+  ElementSelection,
+  Highlight,
 } from '../../../src/interaction/action';
 import {
   MousePosition,
@@ -15,7 +17,9 @@ describe('createInteractionLibrary', () => {
   it('createInteractionLibrary() returns expected action and interactor', () => {
     expect(createInteractionLibrary()).toEqual({
       'action.surfacePointSelection': SurfacePointSelection,
+      'action.elementSelection': ElementSelection,
       'action.highlightSelection': HighlightSelection,
+      'action.highlight': Highlight,
       'action.tooltip': Tooltip,
       'action.fisheyeFocus': FisheyeFocus,
       'action.plot': Plot,
--- src/interaction/action/index.ts
+++ src/interaction/action/index.ts
@@ -3,9 +3,14 @@ export {
   SurfacePointSelectionOptions,
 } from './service/surfacePointSelection';
 export {
+  ElementSelection,
+  ElementSelectionOptions,
+} from './service/elementSelection';
+export {
   HighlightSelection,
   HighlightSelectionOptions,
 } from './transformer/highlightSelection';
+export { Highlight, HighlightOption } from './transformer/highlight';
 export { Tooltip, TooltipOptions } from './transformer/tooltip';
 export { Plot, PlotOptions } from './transformer/plot';
 export { FisheyeFocus, FisheyeFocusOptions } from './service/fisheyeFocus';
