c2d483e6787de1d0f37b5607c67aa8128d0dd653
云剪者 <584518260@qq.com>
2022-08-10 19:48:50 +0800

    fix(legend-highlight): reset highlighting after legend filter (#4078)
    * fix(legend-highlight): highlight reset when filter
    
    * fix(legend-highlight): highlight reset when filter

--- src/index.ts
+++ src/index.ts
@@ -576,7 +576,7 @@ registerInteraction('legend-filter', {
       isEnable: (context) => {
         return !context.isInShape('legend-item-radio');
       },
-      action: ['list-unchecked:toggle', 'data-filter:filter', 'list-radio:show'],
+      action: ['legend-item-highlight:reset', 'element-highlight:reset', 'list-unchecked:toggle', 'data-filter:filter', 'list-radio:show'],
     },
     //  正反选数据: 只有当 radio === truthy 的时候才会有 legend-item-radio 这个元素
     {
@@ -609,7 +609,7 @@ registerInteraction('legend-visible-filter', {
     { trigger: 'legend-item:mouseenter', action: 'cursor:pointer' },
     { trigger: 'legend-item:mouseleave', action: 'cursor:default' },
   ],
-  start: [{ trigger: 'legend-item:click', action: ['list-unchecked:toggle', 'element-filter:filter'] }],
+  start: [{ trigger: 'legend-item:click', action: ['legend-item-highlight:reset', 'element-highlight:reset', 'list-unchecked:toggle', 'element-filter:filter'] }],
 });
 
 // 出现背景框
