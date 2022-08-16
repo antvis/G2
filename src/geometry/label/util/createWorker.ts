a307c72d79a4e42c4e85bb5ef25bd5d84d6b2321
贝哲 <loki.zjj@antgroup.com>
2022-08-16 10:36:36 +0800

    fix(label): 修复label-webworker执行多条任务时渲染混乱问题

--- src/geometry/label/util/createWorker.ts
+++ src/geometry/label/util/createWorker.ts
@@ -1,5 +1,39 @@
+import { isFunction } from '@antv/util';
+
+class MyWorker {
+  queue: any[] = [];
+  worker: Worker;
+
+  constructor(url) {
+    this.worker = new Worker(url);
+    this.worker.onmessage = (e: MessageEvent) => {
+      this.queue.shift()?.resolve(e);
+    };
+    this.worker.onmessageerror = (e: MessageEvent) => {
+      console.warn('[AntV G2] Web worker is not available');
+      this.queue.shift()?.reject(e);
+    };
+  }
+
+  post(params, onError?: () => any): Promise<MessageEvent> {
+    return new Promise((resolve, reject) => {
+      this.queue.push({ resolve, reject });
+      try {
+        this.worker.postMessage(params);
+      } catch (e) {
+        console.warn('[AntV G2] Web worker is not available');
+        isFunction(onError) && onError();
+      }
+    });
+  }
+
+  destroy() {
+    this.worker.terminate();
+  }
+}
+
 export function createWorker(f: any) {
-  if (typeof window === "undefined") return;
+  if (typeof window === 'undefined') return;
 
   let blob;
   try {
@@ -11,5 +45,5 @@ export function createWorker(f: any) {
     blob = blob.getBlob();
   }
 
-  return new Worker(URL.createObjectURL(blob));
+  return new MyWorker(URL.createObjectURL(blob));
 }
