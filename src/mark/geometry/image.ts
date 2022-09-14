5fffbb0ba14c8643f71edeb6e7299e9ec84908e2
hustcc <i@hust.cc>
2022-09-14 20:22:08 +0800

    docs: add Image docs (#4142)
    * docs: add Image docs
    
    * chore: update docs

--- src/mark/geometry/image.ts
+++ src/mark/geometry/image.ts
@@ -10,12 +10,13 @@ export type ImageOptions = Omit<ImageGeometry, 'type'>;
 export const Image: MC<ImageOptions> = () => {
   return (index, scale, value, coordinate) => {
     const { x: X, y: Y } = value;
-    const { x } = scale;
+    const { x, y } = scale;
+    const xoffset = (x?.getBandWidth?.() || 0) / 2;
+    const yoffset = (y?.getBandWidth?.() || 0) / 2;
     const P = Array.from(index, (i) => {
       const px = +X[i];
       const py = +Y[i];
-      const xoffset = x?.getBandWidth?.() || 0;
-      return [coordinate.map([px + xoffset / 2, py])] as Vector2[];
+      return [coordinate.map([px + xoffset, py + yoffset])] as Vector2[];
     });
     return [index, P];
   };
