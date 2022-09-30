2593de01e8726d4cc33dc6f1c3ea28248062c17b
hustcc <i@hust.cc>
2022-09-30 20:07:12 +0800

    docs: area mark and docuement (#4182)
    * test: add text vis demo into integration
    
    * feat: add area and stacked area
    
    * feat: add area.step shape & add diffY transform
    
    * docs: add area demo with population by state
    
    * chore: add integration image
    
    * chore: fix ci

---
+++ __tests__/integration/charts/unemployment-area-normalize-stacked.ts
@@ -0,0 +1,25 @@
+import { G2Spec } from '../../../src';
+
+export function unemploymentAreaNormalizedStacked(): G2Spec {
+  return {
+    width: 800,
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/unemployment-by-industry.csv',
+    },
+    children: [
+      {
+        type: 'area',
+        transform: [{ type: 'stackY' }, { type: 'normalizeY' }],
+        encode: {
+          x: 'date',
+          y: 'unemployed',
+          color: 'industry',
+        },
+      },
+    ],
+  };
+}
+
+unemploymentAreaNormalizedStacked.maxError = 100;
