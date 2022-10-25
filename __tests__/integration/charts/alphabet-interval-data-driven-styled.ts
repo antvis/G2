e6a297332720923a8036903d2eac03de76502e91
MiniPear <subairui@icloud.com>
2022-10-25 15:34:42 +0800

    feat(runtime): style supports callback (#4218)
    * fix(animation): opacity for zoom in and out
    
    * feat(runtime): style supports callback

---
+++ __tests__/integration/charts/alphabet-interval-data-driven-styled.ts
@@ -0,0 +1,23 @@
+import { G2Spec } from '../../../src';
+
+export function alphabetIntervalDataDrivenStyled(): G2Spec {
+  return {
+    type: 'interval',
+    transform: [{ type: 'sortX', by: 'y', reverse: true }],
+    data: {
+      type: 'fetch',
+      value: 'data/alphabet.csv',
+    },
+    axis: {
+      y: { tickFormatter: '.0%' },
+    },
+    encode: {
+      x: 'letter',
+      y: 'frequency',
+      color: 'steelblue',
+    },
+    style: {
+      stroke: (d) => (d.frequency > 0.01 ? 'red' : 'yellow'),
+    },
+  };
+}
