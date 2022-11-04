5f6aa369d4e0173d2179a7e5f64e97b16bbd9a53
hustcc <i@hust.cc>
2022-11-04 13:40:58 +0800

    test: add integration of boxplot

---
+++ __tests__/integration/charts/sepal-box-polar-box.ts
@@ -0,0 +1,23 @@
+import { G2Spec } from '../../../src';
+import { sepalByRegion } from '../data/sepal';
+
+export function sepalBoxPolarBox(): G2Spec {
+  return {
+    type: 'box',
+    data: sepalByRegion,
+    coordinate: [{ type: 'polar', innerRadius: 0.2 }],
+    encode: {
+      x: 'x',
+      y: 'y',
+      color: 'x',
+    },
+    legend: false,
+    scale: {
+      x: { paddingInner: 0.6, paddingOuter: 0.3 },
+      y: { zero: true },
+    },
+    style: {
+      stroke: 'black',
+    },
+  };
+}
---
+++ __tests__/integration/charts/sepal-box-simple-box.ts
@@ -0,0 +1,22 @@
+import { G2Spec } from '../../../src';
+import { sepalByRegion } from '../data/sepal';
+
+export function sepalBoxSimpleBox(): G2Spec {
+  return {
+    type: 'box',
+    data: sepalByRegion,
+    encode: {
+      x: 'x',
+      y: 'y',
+      color: 'x',
+    },
+    legend: false,
+    scale: {
+      x: { paddingInner: 0.6, paddingOuter: 0.3 },
+      y: { zero: true },
+    },
+    style: {
+      stroke: 'black',
+    },
+  };
+}
