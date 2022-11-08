ff09ffddc5f8e6e80d15484a2f382de90f3351f0
hustcc <i@hust.cc>
2022-11-08 20:34:10 +0800

    docs: add Link demos (#4255)
    * docs: add Link demos
    
    * chore: add maxError = 32

---
+++ __tests__/integration/charts/metros-link-trending.ts
@@ -0,0 +1,26 @@
+import { G2Spec } from '../../../src';
+
+export function metrosLinkTrending(): G2Spec {
+  return {
+    type: 'link',
+    data: {
+      type: 'fetch',
+      value: 'data/metros.json',
+    },
+    encode: {
+      x: ['POP_1980', 'POP_2015'],
+      y: ['R90_10_1980', 'R90_10_2015'],
+      color: (d) => d.R90_10_2015 - d.R90_10_1980,
+    },
+    scale: {
+      x: { type: 'log' },
+    },
+    style: {
+      arrowSize: 6,
+    },
+    axis: {
+      x: { tickFormatter: '~s', label: { autoHide: true } },
+    },
+    legend: false,
+  };
+}
