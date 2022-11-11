8bbea20e21fd138949f58d6eeffb4f5f52cb4443
hustcc <i@hust.cc>
2022-11-11 19:16:16 +0800

    docs: add text search demo

--- site/.dumi/global.ts
+++ site/.dumi/global.ts
@@ -5,4 +5,5 @@ if (window) {
   (window as any).d3Array = require('d3-array');
   (window as any).d3GeoProjection = require('d3-geo-projection');
   (window as any).topojson = require('topojson');
+  (window as any).gPluginA11y = require('@antv/g-plugin-a11y');
 }
