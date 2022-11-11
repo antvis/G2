c3d07faeddb942d2bf252aa690f799f25739881c
hustcc <i@hust.cc>
2022-11-12 00:32:16 +0800

    docs: add text search demo (#4267)

---
+++ site/examples/others/plugins/demo/text-search.ts
@@ -0,0 +1,39 @@
+import { Chart } from '@antv/g2';
+import { Plugin } from '@antv/g-plugin-a11y';
+
+const plugin = new Plugin({ enableExtractingText: true });
+
+const left = (d) => d.end > -1500 && d.start > -3000;
+
+const chart = new Chart({
+  container: 'container',
+  width: 900,
+  height: 1000,
+  paddingRight: 80,
+  plugins: [plugin],
+});
+
+chart.coordinate({ type: 'transpose' });
+
+chart
+  .interval()
+  .data({
+    type: 'fetch',
+    value: 'https://assets.antv.antgroup.com/g2/world-history.json',
+  })
+  .transform({ type: 'sortX', by: 'y' })
+  .transform({ type: 'sortColor', by: 'y', reducer: 'min' })
+  .axis('x', false)
+  .encode('x', 'civilization')
+  .encode('y', ['start', 'end'])
+  .encode('color', 'region')
+  .scale('color', { palette: 'set2' })
+  .label({
+    text: 'civilization',
+    position: (d) => (left(d) ? 'left' : 'right'),
+    textAlign: (d) => (left(d) ? 'end' : 'start'),
+    dx: (d) => (left(d) ? -5 : 5),
+    fontSize: 10,
+  });
+
+chart.render();
