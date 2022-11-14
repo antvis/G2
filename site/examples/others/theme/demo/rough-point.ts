988f869b42b7689abdafefa0f918d9fb3a4d53a4
xiaoiver <yuqi.pyq@antgroup.com>
2022-11-14 15:10:37 +0800

    docs(rough): add 2 sketchy-styled examples (#4265)
    * docs(rough): add 2 sketchy-styled examples
    
    * docs(rough): change axis syntax & remove unused comments

---
+++ site/examples/others/theme/demo/rough-point.ts
@@ -0,0 +1,37 @@
+import { Chart } from '@antv/g2';
+import { Plugin } from '@antv/g-plugin-rough-canvas-renderer';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Gaegu'],
+  },
+  active: () => {
+    const chart = new Chart({
+      container: 'container',
+      autoFit: true,
+      plugins: [new Plugin()],
+    });
+
+    chart
+      .point()
+      .data({
+        type: 'fetch',
+        value:
+          'https://gw.alipayobjects.com/os/basement_prod/6b4aa721-b039-49b9-99d8-540b3f87d339.json',
+      })
+      .encode('x', 'height')
+      .encode('y', 'weight')
+      .encode('color', 'gender')
+      .axis('x', {
+        titleFontSize: 15,
+        titleFontFamily: 'Gaegu',
+      })
+      .axis('y', {
+        titleFontSize: 15,
+        titleFontFamily: 'Gaegu',
+      });
+
+    chart.render();
+  },
+});
