97e6e6dbcaea37106fceac7eb3c4dcbfb84cecec
xiaoiver <yuqi.pyq@antgroup.com>
2022-11-14 16:25:37 +0800

    docs(chore): add sketchy radial & line chart (#4276)
    * docs(chore): add sketchy radial & line chart
    
    * docs(rough): use fillStyle as pattern
    
    * chore: restore dumi theme version

---
+++ site/examples/others/theme/demo/rough-line.ts
@@ -0,0 +1,37 @@
+import { Chart } from '@antv/g2';
+import { Plugin } from '@antv/g-plugin-rough-canvas-renderer';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Gaegu'],
+  },
+  active: () => {
+    const chart = new Chart({
+      container: 'container',
+      autoFit: true,
+      plugins: [new Plugin()],
+    });
+
+    chart
+      .line()
+      .data({
+        type: 'fetch',
+        value:
+          'https://gw.alipayobjects.com/os/bmw-prod/cb99c4ab-e0a3-4c76-9586-fe7fa2ff1a8c.csv',
+      })
+      .transform({ type: 'groupX', y: 'mean' })
+      .encode('x', (d) => new Date(d.date).getFullYear())
+      .encode('y', 'price')
+      .encode('color', 'symbol')
+      .label({
+        text: 'price',
+        fontSize: 10,
+        fontFamily: 'Gaegu',
+        transform: [{ type: 'dodgeY' }],
+      })
+      .style('roughness', 2);
+
+    chart.render();
+  },
+});
---
+++ site/examples/others/theme/demo/rough-radial.ts
@@ -0,0 +1,65 @@
+import { Chart } from '@antv/g2';
+import { Plugin } from '@antv/g-plugin-rough-canvas-renderer';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Gaegu'],
+  },
+  active: () => {
+    const chart = new Chart({
+      container: 'container',
+      height: 480,
+      plugins: [new Plugin()],
+    });
+
+    chart.coordinate({ type: 'theta' });
+
+    chart
+      .interval()
+      .transform({ type: 'stackY' })
+      .data({
+        type: 'fetch',
+        value:
+          'https://gw.alipayobjects.com/os/bmw-prod/79fd9317-d2af-4bc4-90fa-9d07357398fd.csv',
+      })
+      .encode('y', 'value')
+      .encode('color', 'name')
+      .scale('color', {
+        range: [
+          'hachure',
+          'solid',
+          'zigzag',
+          'cross-hatch',
+          'dots',
+          'dashed',
+          'zigzag-line',
+        ],
+      })
+      .style('fill', 'black')
+      .style('stroke', 'black')
+      .style('strokeWidth', '4')
+      .style('colorAttribute', 'fillStyle')
+      .legend(false)
+      .label({
+        text: 'name',
+        radius: 0.8,
+        fontSize: 10,
+        fontWeight: 'bold',
+        fontFamily: 'Gaegu',
+        fill: 'black',
+        stroke: 'white',
+      })
+      .label({
+        text: (d, i, data) => (i < data.length - 3 ? d.value : ''),
+        radius: 0.8,
+        fontSize: 12,
+        fontFamily: 'Gaegu',
+        fill: 'black',
+        stroke: 'white',
+        dy: '0.75em',
+      });
+
+    chart.render();
+  },
+});
