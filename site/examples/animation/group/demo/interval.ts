0be553ae9ab79db7a6f9009a1aa67d5745f57290
hustcc <i@hust.cc>
2022-11-19 20:38:16 +0800

    docs: add group animation demo (#4323)

---
+++ site/examples/animation/group/demo/interval.ts
@@ -0,0 +1,28 @@
+import { Chart } from '@antv/g2';
+
+const fruits = [
+  { type: 'Apple', year: '2001', value: 260 },
+  { type: 'Orange', year: '2001', value: 100 },
+  { type: 'Banana', year: '2001', value: 90 },
+  { type: 'Apple', year: '2002', value: 210 },
+  { type: 'Orange', year: '2002', value: 150 },
+  { type: 'Banana', year: '2002', value: 30 },
+];
+
+const chart = new Chart({
+  container: 'container',
+});
+
+chart
+  .interval()
+  .data(fruits)
+  .transform({ type: 'stackEnter', groupBy: 'color' })
+  .transform({ type: 'dodgeX' })
+  .encode('x', 'year')
+  .encode('y', 'value')
+  .encode('color', 'type')
+  .encode('color', 'type')
+  .animate('enterDuration', 500)
+  .axis('y', false);
+
+chart.render();
