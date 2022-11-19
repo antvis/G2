64b3e66fe0cd471c6c608f2fee55221bbb7939a4
hustcc <i@hust.cc>
2022-11-19 20:38:16 +0800

    docs: add group animation demo (#4323)

---
+++ site/examples/animation/group/demo/area.ts
@@ -0,0 +1,25 @@
+/**
+ * A recreation of this demo: https://canisjs.github.io/canis-editor/index.html?exmp=os_1
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart
+  .area()
+  .data({
+    type: 'fetch',
+    value: 'https://assets.antv.antgroup.com/g2/os.json',
+  })
+  .encode('x', 'Year')
+  .encode('y', 'Share')
+  .encode('color', 'OperatingSystem')
+  .encode('shape', 'smooth')
+  .transform({ type: 'stackEnter', groupBy: 'color', duration: 5000 })
+  .transform({ type: 'stackY', orderBy: 'value' })
+  .animate('enterType', 'growInX');
+
+chart.render();
---
+++ site/examples/animation/group/demo/line.ts
@@ -0,0 +1,50 @@
+/**
+ * A recreation of this demo: https://canisjs.github.io/canis-editor/index.html?exmp=purchases_1
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart.data({
+  type: 'fetch',
+  value: 'https://assets.antv.antgroup.com/g2/doughnut-purchases.json',
+});
+
+chart
+  .line()
+  .encode('x', 'year')
+  .encode('y', 'count')
+  .encode('color', 'year')
+  .encode('shape', 'smooth')
+  .scale('y', { zero: true, nice: true })
+  .style('gradient', 'x')
+  .style('gradientColor', 'start')
+  .animate('enterType', 'pathIn')
+  .animate('enterDuration', 3000)
+  .axis('y', { tickFormatter: '~s' });
+
+chart
+  .point()
+  .transform({ type: 'stackEnter' })
+  .encode('x', 'year')
+  .encode('y', 'count')
+  .encode('color', 'year')
+  .encode('shape', 'point')
+  .animate('enterDuration', 300);
+
+chart
+  .text()
+  .transform({ type: 'stackEnter' })
+  .encode('x', 'year')
+  .encode('y', 'count')
+  .encode('text', 'year')
+  .animate('enterDuration', 300)
+  .style('strokeWidth', 5)
+  .style('stroke', '#fff')
+  .style('textAlign', 'center')
+  .style('dy', -8);
+
+chart.render();
