4ce4d5fa02b809be47b314f120136cc294575960
pearmini <subairui@icloud.com>
2022-11-21 23:20:18 +0800

    docs: add keyframe

---
+++ site/examples/animation/storytelling/demo/facet-keyframe.ts
@@ -0,0 +1,62 @@
+import { Chart } from '@antv/g2';
+
+fetch(
+  'https://gw.alipayobjects.com/os/bmw-prod/7fbb7084-cf34-4e7c-91b3-09e4748dc5e9.json',
+)
+  .then((res) => res.json())
+  .then((data) => {
+    const chart = new Chart({
+      container: 'container',
+      width: 800,
+    });
+    const padding = (node) =>
+      node.attr('paddingRight', 86).attr('paddingLeft', 54);
+    const encode = (node) =>
+      node
+        .encode('shape', 'smooth')
+        .encode('x', (d) => new Date(d.date))
+        .encode('y', 'unemployed')
+        .encode('color', 'industry')
+        .encode('key', 'industry');
+    const utcX = (node) => node.scale('x', { utc: true });
+
+    const keyframe = chart
+      .timingKeyframe()
+      .attr('direction', 'alternate')
+      .attr('iterationCount', 2);
+
+    keyframe
+      .facetRect()
+      .call(padding)
+      .data(data)
+      .encode('y', 'industry')
+      .area()
+      .attr('class', 'area')
+      .attr('frame', false)
+      .call(encode)
+      .call(utcX)
+      .scale('y', { facet: false })
+      .style('fillOpacity', 1)
+      .animate('enterType', 'scaleInY');
+
+    keyframe
+      .area()
+      .call(padding)
+      .data(data)
+      .attr('class', 'area')
+      .transform({ type: 'stackY', reverse: true })
+      .call(encode)
+      .call(utcX)
+      .style('fillOpacity', 1);
+
+    keyframe
+      .area()
+      .call(padding)
+      .data(data)
+      .attr('class', 'area')
+      .call(encode)
+      .call(utcX)
+      .style('fillOpacity', 0.8);
+
+    chart.render();
+  });
