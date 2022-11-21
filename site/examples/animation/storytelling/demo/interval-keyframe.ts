510ea36201bbad41661f8fc34f090e99ffa1b44b
MiniPear <subairui@icloud.com>
2022-11-21 23:22:19 +0800

    docs: add keyframe (#4345)

---
+++ site/examples/animation/storytelling/demo/interval-keyframe.ts
@@ -0,0 +1,35 @@
+import { Chart } from '@antv/g2';
+
+const sex = [
+  { city: 'A', sex: '男', value: 52 },
+  { city: 'A', sex: '女', value: 48 },
+  { city: 'B', sex: '男', value: 130 },
+  { city: 'B', sex: '女', value: 70 },
+];
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+  paddingTop: 60,
+  paddingLeft: 100,
+});
+
+const keyframe = chart.timingKeyframe();
+
+keyframe
+  .interval()
+  .data(sex)
+  .transform({ type: 'groupX', y: 'sum' })
+  .encode('x', 'city')
+  .encode('y', 'value')
+  .encode('key', 'city');
+
+keyframe
+  .interval()
+  .data(sex)
+  .transform({ type: 'dodgeX' })
+  .encode('x', 'city')
+  .encode('y', 'value')
+  .encode('color', 'sex')
+  .encode('groupKey', 'city');
+
+chart.render();
