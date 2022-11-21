a4064d9c9cd92ce8fe00f380560f1c488db10878
MiniPear <subairui@icloud.com>
2022-11-21 23:22:19 +0800

    docs: add keyframe (#4345)

---
+++ site/examples/animation/storytelling/demo/point-keyframe.ts
@@ -0,0 +1,38 @@
+import { Chart } from '@antv/g2';
+
+fetch(
+  'https://gw.alipayobjects.com/os/bmw-prod/fbe4a8c1-ce04-4ba3-912a-0b26d6965333.json',
+)
+  .then((res) => res.json())
+  .then((data) => {
+    const chart = new Chart({
+      container: 'container',
+      paddingTop: 60,
+      paddingLeft: 100,
+    });
+
+    const keyframe = chart
+      .timingKeyframe()
+      .attr('direction', 'alternate')
+      .attr('iterationCount', 4);
+
+    keyframe
+      .interval()
+      .data(data)
+      .transform({ type: 'groupX', y: 'mean' })
+      .encode('x', 'gender')
+      .encode('y', 'weight')
+      .encode('color', 'gender')
+      .encode('key', 'gender');
+
+    keyframe
+      .point()
+      .data(data)
+      .encode('x', 'height')
+      .encode('y', 'weight')
+      .encode('color', 'gender')
+      .encode('groupKey', 'gender')
+      .encode('shape', 'point');
+
+    chart.render();
+  });
