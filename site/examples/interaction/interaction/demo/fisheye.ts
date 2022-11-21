d9ddd0e3c7bd23a5d7289b57b2719dca44b9089d
MiniPear <subairui@icloud.com>
2022-11-21 22:52:51 +0800

    docs: add interaction (#4344)

---
+++ site/examples/interaction/interaction/demo/fisheye.ts
@@ -0,0 +1,24 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+});
+
+chart
+  .point()
+  .data({
+    type: 'fetch',
+    value: 'https://gw.alipayobjects.com/os/antvdemo/assets/data/bubble.json',
+  })
+  .encode('x', 'GDP')
+  .encode('y', 'LifeExpectancy')
+  .encode('size', 'Population')
+  .encode('color', 'continent')
+  .encode('shape', 'point')
+  .scale('size', { type: 'log', range: [4, 20] })
+  .style('fillOpacity', 0.3)
+  .style('lineWidth', 1);
+
+chart.interaction({ type: 'fisheye' });
+
+chart.render();
