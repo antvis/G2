f8e0c52c35e0430b45269d9ff607e7441ed91964
hustcc <i@hust.cc>
2022-11-24 10:24:50 +0800

    docs: add poisson distribution

--- site/.dumi/global.ts
+++ site/.dumi/global.ts
@@ -10,6 +10,7 @@ if (window) {
   (window as any).d3Voronoi = require('d3-voronoi');
   (window as any).d3Array = require('d3-array');
   (window as any).d3GeoProjection = require('d3-geo-projection');
+  (window as any).d3Random = require('d3-random');
   (window as any).topojson = require('topojson');
   (window as any).gLottiePlayer = require('@antv/g-lottie-player');
   (window as any).webfontloader = require('webfontloader');
