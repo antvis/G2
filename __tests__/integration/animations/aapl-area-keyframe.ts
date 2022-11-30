054bcc7bb3f008946af9933ad92c48622e255c57
pearmini <subairui@icloud.com>
2022-11-30 14:33:13 +0800

    fix(animation): transition with morphing animation

---
+++ __tests__/integration/animations/aapl-area-keyframe.ts
@@ -0,0 +1,40 @@
+import { csv } from 'd3-fetch';
+import { autoType } from 'd3-dsv';
+import { G2Spec } from '../../../src';
+
+export async function aaplAreaKeyframe(): Promise<G2Spec> {
+  const data = await csv('data/aapl.csv', autoType);
+  const missing = data.map((d) => ({
+    ...d,
+    close: d.date.getUTCMonth() <= 3 ? NaN : d.close,
+  }));
+  const area = {
+    type: 'area',
+    data: missing,
+    encode: {
+      x: 'date',
+      y: 'close',
+    },
+    scale: {
+      x: { type: 'time' },
+    },
+    style: {
+      connectNulls: true,
+      connectFill: 'grey',
+      connectFillOpacity: 0.15,
+    },
+  };
+  return {
+    type: 'timingKeyframe',
+    width: 800,
+    children: [
+      // @ts-ignore
+      { ...area, width: 800 },
+      // @ts-ignore
+      { ...area, width: 400 },
+    ],
+  };
+}
+
+aaplAreaKeyframe.intervals = [false, false, [333, 666]];
+aaplAreaKeyframe.only = true;
