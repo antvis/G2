2e37a10a44e7ce5ad5b248c8462b0f10a357d3d8
MiniPear <subairui@icloud.com>
2022-12-12 22:02:10 +0800

    feat(facet): support top most axis (#4452)

---
+++ __tests__/integration/charts/indices-line-chart-fact-axis.ts
@@ -0,0 +1,43 @@
+import { csv } from 'd3-fetch';
+import { autoType } from 'd3-dsv';
+import { G2Spec } from '../../../src';
+
+export async function indicesLineChartFacetAxis(): Promise<G2Spec> {
+  const data = await csv('data/indices.csv', autoType);
+  return {
+    type: 'facetRect',
+    height: 600,
+    width: 700,
+    paddingTop: 50,
+    paddingLeft: 60,
+    encode: { y: 'Symbol' },
+    scale: { y: { paddingInner: 0.2 } },
+    legend: false,
+    data,
+    children: [
+      {
+        type: 'line',
+        frame: false,
+        scale: { y: { nice: true, facet: false } },
+        axis: {
+          y: { labelAutoRotate: false },
+          x: ({ rowIndex }) =>
+            rowIndex === 0
+              ? { position: 'top' }
+              : {
+                  tickLine: false,
+                  tick: false,
+                  title: false,
+                  label: false,
+                },
+        },
+        encode: {
+          x: 'Date',
+          y: 'Close',
+          color: 'Symbol',
+          key: 'Symbol',
+        },
+      },
+    ],
+  };
+}
