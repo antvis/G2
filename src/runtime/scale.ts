15ac0ae8d5adef7b1d2041483ac2077e5dfb0e3e
visiky <736929286@qq.com>
2022-12-15 13:48:06 +0800

    feat(theme): add academy theme (#4469)
    * feat(theme): add academy theme and schemaTableau10 palette
    
    * feat(theme): add academy theme
    
    * fix: cr suggestions

--- src/runtime/scale.ts
+++ src/runtime/scale.ts
@@ -275,11 +275,15 @@ function categoricalColors(
   );
   const { defaultCategory10: c10, defaultCategory20: c20 } = theme;
   const defaultPalette = unique(values.flat()).length <= c10.length ? c10 : c20;
-  const { palette, offset } = options;
-  const colors =
-    interpolatedColors(palette, domain, offset) ||
-    usePalette({ type: palette || defaultPalette });
-  return colors;
+  const { palette = defaultPalette, offset } = options;
+  // Built-in palettes have higher priority.
+  try {
+    return usePalette({ type: palette });
+  } catch (e) {
+    const colors = interpolatedColors(palette, domain, offset);
+    if (colors) return colors;
+    throw new Error(`Unknown Component: ${palette} `);
+  }
 }
 
 function gradientColors(range: string): string[] {
