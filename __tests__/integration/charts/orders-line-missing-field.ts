cf84f2284f80d163c65636422c32ce2f5f044d82
MiniPear <subairui@icloud.com>
2022-12-16 12:02:26 +0800

    fix(runtime): missing field (close: #4437) (#4487)

---
+++ __tests__/integration/charts/orders-line-missing-field.ts
@@ -0,0 +1,18 @@
+import { G2Spec } from '../../../src';
+import { orders } from '../data/orders';
+
+export function ordersLineMissingField(): G2Spec {
+  return {
+    type: 'line',
+    paddingLeft: 50,
+    data: orders,
+    encode: {
+      x: 'hour',
+      y: 'new_recharge_orders',
+      color: 'date',
+    },
+    axis: {
+      x: { labelFormatter: (d) => (d === undefined ? '' : d) },
+    },
+  };
+}
---
+++ __tests__/integration/data/orders.js
@@ -0,0 +1,326 @@
+export const orders = [
+  {
+      "time": " ",
+      "new_recharge_orders": 1656,
+      "recharge_orders": 12672,
+      "total_row": 1
+  },
+  {
+      "time": "2022-12-03 00",
+      "new_recharge_orders": 29,
+      "recharge_orders": 864,
+      "total_row": false,
+      "hour": "00",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 00",
+      "new_recharge_orders": 19,
+      "recharge_orders": 715,
+      "total_row": false,
+      "hour": "00",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 01",
+      "new_recharge_orders": 36,
+      "recharge_orders": 618,
+      "total_row": false,
+      "hour": "01",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 01",
+      "new_recharge_orders": 31,
+      "recharge_orders": 556,
+      "total_row": false,
+      "hour": "01",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 02",
+      "new_recharge_orders": 38,
+      "recharge_orders": 591,
+      "total_row": false,
+      "hour": "02",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 02",
+      "new_recharge_orders": 19,
+      "recharge_orders": 579,
+      "total_row": false,
+      "hour": "02",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 03",
+      "new_recharge_orders": 67,
+      "recharge_orders": 452,
+      "total_row": false,
+      "hour": "03",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 03",
+      "new_recharge_orders": 29,
+      "recharge_orders": 430,
+      "total_row": false,
+      "hour": "03",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 04",
+      "new_recharge_orders": 54,
+      "recharge_orders": 443,
+      "total_row": false,
+      "hour": "04",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 04",
+      "new_recharge_orders": 51,
+      "recharge_orders": 495,
+      "total_row": false,
+      "hour": "04",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 05",
+      "new_recharge_orders": 67,
+      "recharge_orders": 486,
+      "total_row": false,
+      "hour": "05",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 05",
+      "new_recharge_orders": 42,
+      "recharge_orders": 477,
+      "total_row": false,
+      "hour": "05",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 06",
+      "new_recharge_orders": 56,
+      "recharge_orders": 459,
+      "total_row": false,
+      "hour": "06",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 06",
+      "new_recharge_orders": 41,
+      "recharge_orders": 380,
+      "total_row": false,
+      "hour": "06",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 07",
+      "new_recharge_orders": 61,
+      "recharge_orders": 600,
+      "total_row": false,
+      "hour": "07",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 07",
+      "new_recharge_orders": 54,
+      "recharge_orders": 569,
+      "total_row": false,
+      "hour": "07",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 08",
+      "new_recharge_orders": 67,
+      "recharge_orders": 481,
+      "total_row": false,
+      "hour": "08",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 08",
+      "new_recharge_orders": 34,
+      "recharge_orders": 422,
+      "total_row": false,
+      "hour": "08",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 09",
+      "new_recharge_orders": 70,
+      "recharge_orders": 491,
+      "total_row": false,
+      "hour": "09",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 09",
+      "new_recharge_orders": 37,
+      "recharge_orders": 381,
+      "total_row": false,
+      "hour": "09",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 10",
+      "new_recharge_orders": 75,
+      "recharge_orders": 485,
+      "total_row": false,
+      "hour": "10",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 10",
+      "new_recharge_orders": 52,
+      "recharge_orders": 390,
+      "total_row": false,
+      "hour": "10",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 11",
+      "new_recharge_orders": 55,
+      "recharge_orders": 406,
+      "total_row": false,
+      "hour": "11",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 11",
+      "new_recharge_orders": 45,
+      "recharge_orders": 411,
+      "total_row": false,
+      "hour": "11",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 12",
+      "new_recharge_orders": 68,
+      "recharge_orders": 477,
+      "total_row": false,
+      "hour": "12",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 12",
+      "new_recharge_orders": 44,
+      "recharge_orders": 462,
+      "total_row": false,
+      "hour": "12",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 13",
+      "new_recharge_orders": 79,
+      "recharge_orders": 691,
+      "total_row": false,
+      "hour": "13",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 13",
+      "new_recharge_orders": 24,
+      "recharge_orders": 639,
+      "total_row": false,
+      "hour": "13",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 14",
+      "new_recharge_orders": 61,
+      "recharge_orders": 525,
+      "total_row": false,
+      "hour": "14",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-09 14",
+      "new_recharge_orders": 25,
+      "recharge_orders": 481,
+      "total_row": false,
+      "hour": "14",
+      "date": "2022-12-09"
+  },
+  {
+      "time": "2022-12-03 15",
+      "new_recharge_orders": 55,
+      "recharge_orders": 467,
+      "total_row": false,
+      "hour": "15",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 16",
+      "new_recharge_orders": 72,
+      "recharge_orders": 438,
+      "total_row": false,
+      "hour": "16",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 17",
+      "new_recharge_orders": 67,
+      "recharge_orders": 446,
+      "total_row": false,
+      "hour": "17",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 18",
+      "new_recharge_orders": 93,
+      "recharge_orders": 505,
+      "total_row": false,
+      "hour": "18",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 19",
+      "new_recharge_orders": 90,
+      "recharge_orders": 551,
+      "total_row": false,
+      "hour": "19",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 20",
+      "new_recharge_orders": 107,
+      "recharge_orders": 571,
+      "total_row": false,
+      "hour": "20",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 21",
+      "new_recharge_orders": 94,
+      "recharge_orders": 531,
+      "total_row": false,
+      "hour": "21",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 22",
+      "new_recharge_orders": 85,
+      "recharge_orders": 538,
+      "total_row": false,
+      "hour": "22",
+      "date": "2022-12-03"
+  },
+  {
+      "time": "2022-12-03 23",
+      "new_recharge_orders": 110,
+      "recharge_orders": 556,
+      "total_row": false,
+      "hour": "23",
+      "date": "2022-12-03"
+  },
+  {
+      "time": " ",
+      "new_recharge_orders": 547,
+      "recharge_orders": 7387,
+      "total_row": 1
+  }
+]
