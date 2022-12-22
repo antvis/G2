93240098cc3f9d66e3cb033840608d29e986dd3a
pearmini <subairui@icloud.com>
2022-12-22 11:33:16 +0800

    fix(boxplot): pass scale, axis and legend (close: #4355)

---
+++ __tests__/integration/charts/gdp2-boxplot-log.ts
@@ -0,0 +1,22 @@
+import { G2Spec } from '../../../src';
+
+export function gdp2BoxplotLog(): G2Spec {
+  return {
+    type: 'boxplot',
+    paddingLeft: 60,
+    data: {
+      type: 'fetch',
+      value: 'data/gdp2.csv',
+    },
+    encode: {
+      x: 'continent',
+      y: 'Population',
+    },
+    scale: {
+      y: { type: 'log', nice: true },
+    },
+    axis: {
+      y: { labelFormatter: '~s', tickCount: 3 },
+    },
+  };
+}
