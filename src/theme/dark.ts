0e42a1aafb67fe2cf0efd3eeca24b991830b6c02
visiky <736929286@qq.com>
2022-12-26 21:06:50 +0800

    feat(theme): add connectorLength

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -364,6 +364,7 @@ export const Dark: TC<DarkOptions> = (options) => {
       connectorStroke: COLORS.BLACK,
       connectorStrokeOpacity: 0.45,
       connectorLineWidth: 1,
+      connectorLength: 12,
       connectorLength2: 8,
       connectorDistance: 4,
     },
