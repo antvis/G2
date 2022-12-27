b1a02a52655575fa068acc27569cee64638dc131
visiky <736929286@qq.com>
2022-12-27 15:20:44 +0800

    eat(slider): support slider sync scale

--- __tests__/integration/charts/index.ts
+++ __tests__/integration/charts/index.ts
@@ -161,3 +161,4 @@ export { ordersLineMissingField } from './orders-line-missing-field';
 export { gdp2BoxplotLog } from './gdp2-boxplot-log';
 export { aaplLineScrollbar } from './aapl-line-scrollbar';
 export { alphabetIntervalRenderToMountedGroup } from './alphabet-interval-render-to-mounted-element';
+export { weatherLineMultiSlider } from './weather-line-multi-slider';
