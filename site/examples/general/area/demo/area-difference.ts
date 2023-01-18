8ba8a51ed44b02cd91a774f22aa56c35eef7d2de
hustcc <i@hust.cc>
2023-01-18 11:26:00 +0800

    feat: add area shapes include hvh, hv, vh (#4568)
    * feat: rename area.step -> area.hvh
    
    * feat: add area hvh, vh, hv shape and docs

--- site/examples/general/area/demo/area-difference.ts
+++ site/examples/general/area/demo/area-difference.ts
@@ -26,7 +26,7 @@ chart
   .encode('x', (d) => new Date(d.date))
   .encode('y', 'temperature')
   .encode('color', 'city')
-  .encode('shape', 'step')
+  .encode('shape', 'hvh')
   .scale('color', { range: ['#67a9cf', '#ef8a62'] });
 
 chart
--- src/shape/area/hvh.ts
+++ src/shape/area/hvh.ts
@@ -2,14 +2,14 @@ import { curveStep } from 'd3-shape';
 import { ShapeComponent as SC } from '../../runtime';
 import { Curve } from './curve';
 
-export type StepOptions = Record<string, any>;
+export type HVHOptions = Record<string, any>;
 
-export const Step: SC<StepOptions> = (options) => {
+export const HVH: SC<HVHOptions> = (options) => {
   return (P, value, coordinate, theme) => {
     return Curve({ curve: curveStep, ...options })(P, value, coordinate, theme);
   };
 };
 
-Step.props = {
+HVH.props = {
   ...Curve.props,
 };
