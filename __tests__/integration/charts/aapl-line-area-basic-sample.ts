d3eaa3616b9b3f8b63de34294cdfb3f5b1fe7698
hustcc <i@hust.cc>
2023-01-29 19:28:27 +0800

    feat: add transform.sample and document (#4547)
    * feat: add transform.sample and documents
    
    * fix: get X Y value typo
    
    * chore: update screenshot
    
    * docs: add line + point case

---
+++ __tests__/integration/charts/aapl-line-area-basic-sample.ts
@@ -0,0 +1,46 @@
+import { G2Spec } from '../../../src';
+
+export function aaplLineAreaBasicSample(): G2Spec {
+  return {
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    children: [
+      {
+        type: 'line',
+        encode: {
+          x: 'date',
+          y: 'close',
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+      },
+      {
+        type: 'area',
+        encode: {
+          x: 'date',
+          y: 'close',
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+        style: {
+          fillOpacity: 0.5,
+        },
+      },
+    ],
+  };
+}
+
+aaplLineAreaBasicSample.maxError = 100;
---
+++ __tests__/integration/charts/aapl-line-point-basic-sample.ts
@@ -0,0 +1,38 @@
+import { G2Spec } from '../../../src';
+
+export function aaplLinePointBasicSample(): G2Spec {
+  return {
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    children: [
+      {
+        type: 'line',
+        encode: {
+          x: 'date',
+          y: 'close',
+        },
+      },
+      {
+        type: 'point',
+        encode: {
+          x: 'date',
+          y: 'close',
+          shape: 'point',
+          size: 2,
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+      },
+    ],
+  };
+}
+
+aaplLinePointBasicSample.maxError = 100;
