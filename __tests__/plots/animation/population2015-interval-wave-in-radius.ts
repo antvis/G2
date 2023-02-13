2628f76217172f8857908b5b4fed3db2cb20402b
MiniPear <pearminipro@gmail.com>
2023-02-13 12:20:55 +0800

    fix(pie): add radius and inset to shape.style

---
+++ __tests__/plots/animation/population2015-interval-wave-in-radius.ts
@@ -0,0 +1,42 @@
+import { G2Spec } from '../../../src';
+
+export function population2015IntervalWaveInRadius(): G2Spec {
+  return {
+    type: 'view',
+    height: 640,
+    data: {
+      type: 'fetch',
+      value: 'data/population2015.csv',
+    },
+    children: [
+      {
+        type: 'interval',
+        transform: [{ type: 'stackY' }],
+        coordinate: { type: 'theta', innerRadius: 0.6 },
+        scale: {
+          color: {
+            palette: 'spectral',
+            offset: (t) => t * 0.8 + 0.1,
+          },
+        },
+        legend: false,
+        encode: {
+          y: 'value',
+          color: 'name',
+        },
+        style: {
+          stroke: 'white',
+          inset: 1,
+          radius: 10,
+        },
+        animate: {
+          enterType: 'waveIn',
+        },
+      },
+    ],
+  };
+}
+
+population2015IntervalWaveInRadius.intervals = [[]];
+
+population2015IntervalWaveInRadius.only = true;
