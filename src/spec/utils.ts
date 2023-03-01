05099f65d1df4dca1b9467fdaa8c8893d99a8e2e
MiniPear <pearminipro@gmail.com>
2023-03-01 14:15:04 +0800

    fix(types): object prop

--- src/spec/utils.ts
+++ src/spec/utils.ts
@@ -1,5 +1,3 @@
-import { Interaction } from './interaction';
-
 export type Literal2Object<Literal extends { type?: any }> = Partial<{
   [Type in Literal['type']]: Literal extends { type?: Type }
     ? Omit<Literal, 'type'> | boolean
