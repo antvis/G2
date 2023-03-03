834fa84de3f8cf2e3d9080f05d22965827bf7acd
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2023-03-03 17:37:16 +0800

    feat(options): add chart options and remove chart getOptions (#4721)
    * feat(options): add chart options and remove chart getOptions
    
    * refactor(options): improve chart options functions

---
+++ __tests__/integration/api-chart-options.spec.ts
@@ -0,0 +1,36 @@
+import { chartOptions as render } from '../plots/api/chart-options';
+import { createNodeGCanvas } from './utils/createNodeGCanvas';
+import { sleep } from './utils/sleep';
+import { kebabCase } from './utils/kebabCase';
+import './utils/useSnapshotMatchers';
+import './utils/useCustomFetch';
+
+describe('chart.options', () => {
+  const dir = `${__dirname}/snapshots/api/${kebabCase(render.name)}`;
+  const canvas = createNodeGCanvas(800, 500);
+  let chart;
+
+  it('chart.options should init', async () => {
+    const { finished, ...rest } = render({
+      canvas,
+      container: document.createElement('div'),
+    });
+    chart = rest.chart;
+    await finished;
+    await sleep(20);
+    await expect(canvas).toMatchCanvasSnapshot(dir, 'step0');
+  });
+
+  it('chart.options({ width: 480 }) should update chart._options', async () => {
+    chart.options({
+      width: 480,
+    });
+    await chart.render();
+    await sleep(20);
+    await expect(canvas).toMatchCanvasSnapshot(dir, 'step1');
+  });
+
+  afterAll(() => {
+    canvas?.destroy();
+  });
+});
---
+++ __tests__/plots/api/chart-options.ts
@@ -0,0 +1,32 @@
+import { Chart } from '../../../src';
+
+export function chartOptions(context) {
+  const { container, canvas } = context;
+
+  const div = document.createElement('div');
+  container.appendChild(div);
+
+  const chart = new Chart({
+    container: div,
+    canvas,
+  });
+
+  chart.options({
+    type: 'line',
+    clip: true,
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    encode: {
+      x: 'date',
+      y: 'close',
+    },
+  });
+
+  const finished = chart.render();
+
+  return { chart, finished };
+}
+
+chartOptions.only = true;
