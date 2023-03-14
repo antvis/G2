ca806ad611c6edcebcb1df6dc0c11ce8af0267de
wuding.why <wuding.why@alibaba-inc.com>
2023-03-14 13:20:34 +0800

    fix: 水平圆角柱状图展示有错误-https://github.com/antvis/G2/issues/4761

---
+++ __tests__/plots/static/penguins-interval-transpose-group-color.ts
@@ -0,0 +1,26 @@
+import { G2Spec } from '../../../src';
+
+export function penguinsIntervalTransposeGroupColor(): G2Spec {
+  return {
+    type: 'interval',
+    data: {
+      type: 'fetch',
+      value: 'data/penguins.csv',
+    },
+    height: 120,
+    coordinate: { transform: [{ type: 'transpose' }] },
+    transform: [
+      { type: 'groupColor', y: 'count' },
+      { type: 'stackY' },
+      { type: 'normalizeY' },
+    ],
+    axis: { y: { labelFormatter: '.0%' } },
+    encode: {
+      color: 'sex',
+    },
+    labels: [{ text: 'sex', position: 'inside' }],
+    style: {
+      radius: 10,
+    },
+  };
+}
