738addacc2c65a02ffaf8cfdb4230500aa99630c
Aaron <943720372@qq.com>
2023-03-16 14:41:04 +0800

    docs: add interaction and components demos (#4774)
    * docs: add interaction and components demos
    
    * docs: update cases and fix test issues

---
+++ site/examples/interaction/interaction/demo/brush.ts
@@ -0,0 +1,22 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  theme: 'classic',
+  autoFit: true,
+});
+
+chart
+  .point()
+  .data({
+    type: 'fetch',
+    value:
+      'https://gw.alipayobjects.com/os/basement_prod/6b4aa721-b039-49b9-99d8-540b3f87d339.json',
+  })
+  .encode('x', 'height')
+  .encode('y', 'weight')
+  .encode('color', 'gender')
+  .state({ inactive: { stroke: 'gray', opacity: 0.5 } })
+  .interaction('brushHighlight', true);
+
+chart.render();
