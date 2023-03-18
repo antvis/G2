5d5959ba976d5cd12926b60a8b5a8034e36b1829
MiniPear <subairui@icloud.com>
2023-03-18 21:27:58 +0800

    feat(interaction): add legendFilter to defaults (#4788)

---
+++ __tests__/plots/interaction/stateages-interval-normalized.ts
@@ -0,0 +1,34 @@
+import { G2Spec } from '../../../src';
+import { LEGEND_ITEMS_CLASS_NAME } from '../../../src/interaction/legendFilter';
+import { step } from './utils';
+
+export function stateAgesIntervalNormalized(): G2Spec {
+  return {
+    type: 'interval',
+    paddingLeft: 50,
+    width: 800,
+    height: 1000,
+    data: {
+      type: 'fetch',
+      value: 'data/stateages.csv',
+    },
+    coordinate: { transform: [{ type: 'transpose' }] },
+    transform: [
+      { type: 'stackY' },
+      { type: 'sortX', by: 'y', reverse: true, reducer: 'sum' },
+      { type: 'normalizeY' },
+    ],
+    encode: {
+      x: 'state',
+      y: 'population',
+      color: 'age',
+    },
+  };
+}
+
+stateAgesIntervalNormalized.steps = ({ canvas }) => {
+  const { document } = canvas;
+  const elements = document.getElementsByClassName(LEGEND_ITEMS_CLASS_NAME);
+  const [e0] = elements;
+  return [step(e0, 'click')];
+};
