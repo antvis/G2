99ad0e3bcb4796b9bcb97aacd3e13b2191ee7da9
MiniPear <subairui@icloud.com>
2023-03-24 22:54:57 +0800

    fix(tooltip): sample (#4832)

---
+++ __tests__/plots/tooltip/aapl-line-area-basic-sample.ts
@@ -0,0 +1,38 @@
+import { G2Spec } from '../../../src';
+import { seriesTooltipSteps } from './utils';
+
+export function aaplLineAreaBasicSample(): G2Spec {
+  return {
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    children: [
+      {
+        type: 'area',
+        encode: {
+          x: 'date',
+          y: 'close',
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+        style: {
+          fillOpacity: 0.5,
+        },
+        tooltip: {
+          title: (d) => new Date(d.date).toUTCString(),
+        },
+      },
+    ],
+  };
+}
+
+aaplLineAreaBasicSample.maxError = 100;
+
+aaplLineAreaBasicSample.steps = seriesTooltipSteps([500, 200]);
