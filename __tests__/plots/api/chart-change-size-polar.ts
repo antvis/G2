c44c22288b049d92eb78264b2e1c60fe7a3074e9
MiniPear <subairui@icloud.com>
2023-03-29 16:26:21 +0800

    fix(animation): shape to shape animation take style.transform into account (#4849)

---
+++ __tests__/plots/api/chart-change-size-polar.ts
@@ -0,0 +1,53 @@
+import { Chart } from '../../../src';
+import { scoreByItem } from '../../data/score-by-item';
+
+export function chartChangeSizePolar(context) {
+  const { container, canvas } = context;
+
+  const button = document.createElement('button');
+  button.innerText = 'Update Size';
+  button.style.display = 'block';
+  container.appendChild(button);
+
+  const div = document.createElement('div');
+  container.appendChild(div);
+
+  const chart = new Chart({ theme: 'classic', container: div, canvas });
+
+  chart.options({
+    type: 'view',
+    data: scoreByItem,
+    coordinate: { type: 'polar' },
+    encode: { x: 'item', y: 'score', color: 'type' },
+    scale: {
+      x: { padding: 0.5, align: 0 },
+      y: { tickCount: 5, domainMax: 80 },
+    },
+    axis: {
+      x: { grid: true },
+      y: { zIndex: 1, title: false, direction: 'center' },
+    },
+    legend: { color: { layout: { justifyContent: 'flex-start' } } },
+    children: [
+      {
+        type: 'area',
+        style: { fillOpacity: 0.5 },
+      },
+      {
+        type: 'line',
+        style: { lineWidth: 2 },
+      },
+    ],
+  });
+
+  const finished = chart.render();
+
+  let resolve;
+  const resized = new Promise((r) => (resolve = r));
+
+  button.onclick = () => {
+    chart.options({ width: 400, height: 300 }).render().then(resolve);
+  };
+
+  return { chart, button, finished, resized };
+}
