41cc3b0df28a83389a9c7c196886f5210f419a7e
shield2018 <>
2023-04-10 15:35:06 +0800

    fix(interval): fix interval render funnel (#4868)

---
+++ __tests__/plots/static/alphabet-interval-funnel.ts
@@ -0,0 +1,38 @@
+import { G2Spec } from '../../../src';
+
+export function alphabetIntervalFunnel(): G2Spec {
+  return {
+    type: 'interval',
+    coordinate: {
+      transform: [{ type: 'transpose' }],
+    },
+    data: [
+      { text: '页面', value: 1000 },
+      { text: '页面1', value: 900 },
+      { text: '页面2', value: 800 },
+      { text: '页面3', value: 700 },
+    ],
+    transform: [
+      {
+        type: 'symmetryY',
+      },
+    ],
+    axis: {
+      x: false,
+      y: false,
+    },
+    style: {
+      stroke: '#ff0000',
+    },
+    encode: {
+      x: 'text',
+      y: 'value',
+      color: 'text',
+      shape: 'funnel',
+    },
+    scale: {
+      x: { paddingOuter: 0, paddingInner: 0 },
+      color: { type: 'ordinal', range: ['red', 'green', 'blue', '#e45ca2'] },
+    },
+  };
+}
