18163398f91c90e9eaf55cf465c7ade5f6f5f88f
Aaron <943720372@qq.com>
2023-04-23 17:49:24 +0800

    fix: update legend component inferring strategies (#4906)
    * fix: add extra rules to avoid unexcept component inferring
    
    * refactor: update legend inferring config

--- src/component/legendContinuousSize.ts
+++ src/component/legendContinuousSize.ts
@@ -1,4 +1,5 @@
 import { GuideComponentComponent as GCC } from '../runtime';
+import { scaleOf } from './utils';
 import { LegendContinuous, LegendContinuousOptions } from './legendContinuous';
 
 export type LegendContinuousSizeOptions = LegendContinuousOptions;
@@ -6,18 +7,26 @@ export type LegendContinuousSizeOptions = LegendContinuousOptions;
 export const LegendContinuousSize: GCC<LegendContinuousSizeOptions> = (
   options,
 ) => {
-  return LegendContinuous(
-    Object.assign(
-      {},
-      {
-        type: 'size',
-        tick: false,
-        labelFilter: (datum, index, data) =>
-          index === 0 || index === data.length - 1,
-      },
-      options,
-    ),
-  );
+  return (context) => {
+    const { scales } = context;
+    const sizeScale = scaleOf(scales, 'size');
+    return LegendContinuous(
+      Object.assign(
+        {},
+        {
+          type: 'size',
+          tick: false,
+          data: sizeScale.getOptions().domain.map((value, index) => ({
+            value,
+            label: String(value),
+          })),
+          labelFilter: (datum, index, data) =>
+            index === 0 || index === data.length - 1,
+        },
+        options,
+      ),
+    )(context);
+  };
 };
 
 LegendContinuousSize.props = {
