d7b74f58d5445b3269f3b2434b7185f9403960e0
MiniPear <subairui@icloud.com>
2023-04-25 14:56:25 +0800

    fix(demo): disable series tooltip for regression (#4912)
    * fix(demo): disable series tooltip for regression
    
    * fix(tooltip): series tooltip need defiend series element

---
+++ __tests__/plots/tooltip/points-point-regression-quad.ts
@@ -0,0 +1,42 @@
+import { regressionQuad } from 'd3-regression';
+import { G2Spec } from '../../../src';
+import { points } from '../../data/points';
+import { seriesTooltipSteps } from './utils';
+
+const regression = regressionQuad()
+  .x((d) => d.x)
+  // @ts-ignore
+  .y((d) => d.y)
+  .domain([-4, 4]);
+
+export function pointsPointRegressionQuad(): G2Spec {
+  return {
+    type: 'view',
+    data: points,
+    scale: { x: { domain: [-4, 4] }, y: { domain: [-2, 14] } },
+    axis: { x: { title: false }, y: { title: false } },
+    children: [
+      {
+        type: 'point',
+        encode: { x: 'x', y: 'y', shape: 'point' },
+      },
+      {
+        type: 'line',
+        data: { transform: [{ type: 'custom', callback: regression }] },
+        encode: {
+          x: (d) => d[0],
+          y: (d) => d[1],
+        },
+        style: {
+          stroke: '#30BF78',
+          lineWidth: 2,
+        },
+        tooltip: false,
+      },
+      { type: 'lineX', data: [0] },
+      { type: 'lineY', data: [0] },
+    ],
+  };
+}
+
+pointsPointRegressionQuad.steps = seriesTooltipSteps([100, 300]);
