889f378b5d97753a755bdd4e5af87d2a19edbebb
MiniPear <pearminipro@gmail.com>
2023-04-27 12:29:03 +0800

    fix(chart): autoFit do not set width and height of chart options

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -32,37 +32,6 @@ export function getContainerSize(container: HTMLElement): Size {
 }
 
 /**
- *
- * @description Calculate the chart size.
- * @param container DOM container element.
- * @param autoFit Should auto fit.
- * @param width Chart width which is set by user.
- * @param height Chart height which is set by user.
- * @returns chart width and height.
- */
-export function getChartSize(
-  container: HTMLElement,
-  autoFit: boolean,
-  width: number,
-  height: number,
-): Size {
-  if (!autoFit) {
-    return {
-      width,
-      height,
-    };
-  }
-
-  // Get size from wrapper container.
-  const size = getContainerSize(container);
-
-  return {
-    width: size.width,
-    height: size.height,
-  };
-}
-
-/**
  * @description Calculate the real canvas size by view options.
  */
 export function getBBoxSize(options: G2View): Size {
