dd77db88519cc16bae9aebbd9f4982f7085d19e7
pepper-nice <109653633+pepper-nice@users.noreply.github.com>
2023-05-04 19:11:49 -0700

    fix: chart.options changeData problem (#4936)

---
+++ __tests__/integration/api-chart-options-change-data.spec.ts
@@ -0,0 +1,26 @@
+import { chartOptionsChangeData as render } from '../plots/api/chart-options-change-data';
+import { createNodeGCanvas } from './utils/createNodeGCanvas';
+import { sleep } from './utils/sleep';
+import './utils/useSnapshotMatchers';
+import './utils/useCustomFetch';
+
+describe('chart.options.changeData', () => {
+  const canvas = createNodeGCanvas(800, 500);
+
+  it('chart.options.changeData should rerender expected chart', async () => {
+    const { finished, button, chart } = render({
+      canvas,
+      container: document.createElement('div'),
+    });
+    await finished;
+    button.dispatchEvent(new CustomEvent('click'));
+    await new Promise<void>((resolve) => chart.on('afterrender', resolve));
+    const dir = `${__dirname}/snapshots/api`;
+    await sleep(20);
+    await expect(canvas).toMatchCanvasSnapshot(dir, render.name);
+  });
+
+  afterAll(() => {
+    canvas?.destroy();
+  });
+});
