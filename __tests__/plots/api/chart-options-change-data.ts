99bdec2062af347b8d46dfde30ab2508248b0466
pepper-nice <109653633+pepper-nice@users.noreply.github.com>
2023-05-04 19:11:49 -0700

    fix: chart.options changeData problem (#4936)

---
+++ __tests__/plots/api/chart-options-change-data.ts
@@ -0,0 +1,52 @@
+import { Chart } from '../../../src';
+
+export function chartOptionsChangeData(context) {
+  const { container, canvas } = context;
+
+  const button = document.createElement('button');
+  button.innerText = 'Update Data';
+  button.style.display = 'block';
+  container.appendChild(button);
+
+  const div = document.createElement('div');
+  container.appendChild(div);
+
+  const chart = new Chart({
+    theme: 'classic',
+    container,
+    canvas,
+  });
+
+  chart.options({
+    type: 'interval',
+    coordinate: { type: 'theta' },
+    transform: [{ type: 'stackY' }],
+    data: [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
+    ],
+    encode: {
+      y: 'sold',
+      color: 'genre',
+    },
+    animate: {
+      enter: {
+        type: 'waveIn',
+      },
+    },
+  });
+
+  const finished = chart.render();
+
+  button.onclick = () => {
+    chart.changeData([
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+    ]);
+  };
+
+  return { chart, finished, button };
+}
