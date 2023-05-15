dfacc6ff2c625c182715916548d849de06954efc
MiniPear <pearminipro@gmail.com>
2023-05-15 16:18:37 +0800

    chore(dev): make @antv/gui linkable

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite';
+import { deepMix } from '@antv/util';
 
-export default defineConfig({
-  root: './__tests__/',
-  server: {
-    port: 8080,
-    open: '/',
-  },
-});
+const linkOptions =
+  process.env.LINK === true
+    ? {
+        server: {
+          watch: {
+            ignored: ['!**/node_modules/@antv/gui/**'],
+          },
+        },
+        optimizeDeps: {
+          exclude: ['@antv/gui'],
+        },
+      }
+    : {};
+
+export default defineConfig(
+  deepMix(
+    {
+      root: './__tests__/',
+      server: {
+        port: 8080,
+        open: '/',
+      },
+    },
+    linkOptions,
+  ),
+);
