c502d7234a108659f16f6cae970b3faeb716a2c9
pepper-nice <109653633+pepper-nice@users.noreply.github.com>
2023-05-14 23:31:34 -0700

    feat: add thresholds params in gauge mark (#5021)

---
+++ site/examples/general/gauge/demo/gauge-custom-color.ts
@@ -0,0 +1,28 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  theme: 'classic',
+  autoFit: true,
+});
+
+chart
+  .gauge()
+  .data({
+    value: {
+      target: 159,
+      total: 400,
+      name: 'score',
+      thresholds: [100, 200, 400],
+    },
+  })
+  .scale('color', {
+    range: ['#F4664A', '#FAAD14', 'green'],
+  })
+  .style(
+    'textContent',
+    (target, total) => `得分：${target}\n占比：${(target / total) * 100}%`,
+  )
+  .legend(false);
+
+chart.render();
