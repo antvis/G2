86052fced2b165b13a5dc2e0c37617a3dcd9c1f3
MiniPear <subairui@icloud.com>
2023-05-16 16:22:34 +0800

    fix(axis): autoHide should not set crossSize (#5032)

---
+++ __tests__/plots/static/aapl-line-axis-y-hide.ts
@@ -0,0 +1,21 @@
+import { G2Spec } from '../../../src';
+
+export function aaplLineAxisYHide(): G2Spec {
+  return {
+    type: 'line',
+    height: 200,
+    paddingLeft: 80,
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    encode: {
+      x: 'date',
+      y: 'close',
+    },
+    axis: {
+      x: false,
+      y: { labelFormatter: (d) => `${d}000`, transform: [{ type: 'hide' }] },
+    },
+  };
+}
