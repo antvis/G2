321fb1bce8e177cfc2d9ea83cdb1291c3fd6c728
MiniPear <subairui@icloud.com>
2023-05-19 16:49:02 +0800

    fix(brush): handle brush:highlight once when emit (#5063)

---
+++ __tests__/unit/api/options.spec.ts
@@ -0,0 +1,22 @@
+import { Chart } from '../../../src';
+
+describe('API and Options', () => {
+  it('', () => {
+    const chart = new Chart({});
+
+    chart.options({
+      type: 'interval',
+      data: [
+        { genre: 'Sports', sold: 275 },
+        { genre: 'Strategy', sold: 115 },
+        { genre: 'Action', sold: 120 },
+        { genre: 'Shooter', sold: 350 },
+        { genre: 'Other', sold: 150 },
+      ],
+      encode: {
+        x: 'genre',
+        y: 'sold',
+      },
+    });
+  });
+});
