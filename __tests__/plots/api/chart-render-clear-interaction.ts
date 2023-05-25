f489fba7c6802cc75ec4c81017ec8924c2f9b0e7
MiniPear <subairui@icloud.com>
2023-05-25 11:45:17 +0800

    fix(interaction): interaction can be clear (#5087)

---
+++ __tests__/plots/api/chart-render-clear-interaction.ts
@@ -0,0 +1,31 @@
+import { Chart } from '../../../src';
+
+export function chartRenderClearInteraction(context) {
+  const { container, canvas } = context;
+
+  const chart = new Chart({
+    container,
+    canvas,
+  });
+
+  chart.options({
+    theme: 'classic',
+    type: 'line',
+    clip: true,
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    encode: {
+      x: 'date',
+      y: 'close',
+    },
+  });
+
+  const finished = chart.render().then(() => {
+    chart.options({ interaction: { tooltip: false } });
+    return chart.render();
+  });
+
+  return { chart, finished };
+}
