4bf0fda5c5e6f36fd89afd4520198e8a2ae806e1
MiniPear <pearminipro@gmail.com>
2023-06-09 12:56:00 +0800

    feat(api): chart.options render composite mark

---
+++ __tests__/plots/api/chart-options-composite-mark.ts
@@ -0,0 +1,38 @@
+import { deepMix } from '@antv/util';
+import { Chart } from '../../../src';
+
+function Pie(options, context) {
+  const { encode = {}, ...rest } = options;
+  const { value, ...restEncode } = encode;
+  return deepMix(rest, {
+    type: 'interval',
+    transform: [{ type: 'stackY' }],
+    coordinate: { type: 'theta' },
+    encode: {
+      ...restEncode,
+      y: value,
+    },
+  });
+}
+
+export function chartOptionsCompositeMark(context) {
+  const { container, canvas } = context;
+
+  const chart = new Chart({ theme: 'classic', container, canvas });
+
+  chart.options({
+    type: Pie,
+    data: [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
+    ],
+    encode: { value: 'sold', color: 'genre' },
+  });
+
+  const finished = chart.render();
+
+  return { finished };
+}
