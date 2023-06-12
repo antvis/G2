c1bd41a3d3b025c03b11f3b322455e5dc922385e
MiniPear <pearminipro@gmail.com>
2023-06-12 13:05:17 +0800

    docs(mark): add axisX and axisY

---
+++ site/examples/component/axis/demo/axis-multi.ts
@@ -0,0 +1,31 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  theme: 'classic',
+  autoFit: true,
+});
+
+chart
+  .interval()
+  .data({
+    type: 'fetch',
+    value:
+      'https://gw.alipayobjects.com/os/bmw-prod/fb9db6b7-23a5-4c23-bbef-c54a55fee580.csv',
+  })
+  .encode('x', 'letter')
+  .encode('y', 'frequency')
+  .axis('y', { labelFormatter: '.0%' });
+
+chart
+  .axisY()
+  .attr('position', 'right')
+  .scale('y', {
+    type: 'linear',
+    independent: true,
+    domain: [0, 10],
+    range: [1, 0],
+  })
+  .style('grid', false);
+
+chart.render();
---
+++ site/examples/component/axis/demo/axis-x.ts
@@ -0,0 +1,20 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  theme: 'classic',
+  padding: 'auto',
+  height: 80,
+});
+
+chart
+  .axisX()
+  .scale('x', {
+    type: 'linear',
+    domain: [5, 10],
+    range: [0, 1],
+  })
+  .attr('tickCount', 10)
+  .attr('title', 'AxisX');
+
+chart.render();
