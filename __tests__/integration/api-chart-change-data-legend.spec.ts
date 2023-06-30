48873ba1ff5838ad176d0c8fe323a353b1118b93
MiniPear <subairui@icloud.com>
2023-06-30 19:18:35 +0800

    fix(legend): update legend item (#5258)

---
+++ __tests__/integration/api-chart-change-data-legend.spec.ts
@@ -0,0 +1,23 @@
+import { chartChangeDataLegend as render } from '../plots/api/chart-change-data-legend';
+import { createNodeGCanvas } from './utils/createNodeGCanvas';
+import { sleep } from './utils/sleep';
+import './utils/useSnapshotMatchers';
+
+describe('chart.changeData', () => {
+  const canvas = createNodeGCanvas(640, 480);
+
+  it('mark.changeSize(width, height) should rerender expected chart', async () => {
+    const { finished } = render({
+      canvas,
+      container: document.createElement('div'),
+    });
+    await finished;
+    const dir = `${__dirname}/snapshots/api`;
+    await sleep(20);
+    await expect(canvas).toMatchCanvasSnapshot(dir, render.name);
+  });
+
+  afterAll(() => {
+    canvas?.destroy();
+  });
+});
