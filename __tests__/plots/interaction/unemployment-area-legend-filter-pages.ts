2a8b794809882a474555761bc6e929823c5e7a6a
MiniPear <pearminipro@gmail.com>
2023-07-07 12:35:17 +0800

    fix(legend): filter with pages

---
+++ __tests__/plots/interaction/unemployment-area-legend-filter-pages.ts
@@ -0,0 +1,31 @@
+import { G2Spec } from '../../../src';
+import { LEGEND_ITEMS_CLASS_NAME } from '../../../src/interaction/legendFilter';
+import { step } from './utils';
+
+export function unemploymentAreaLegendFilterPages(): G2Spec {
+  return {
+    width: 800,
+    type: 'area',
+    data: {
+      type: 'fetch',
+      value: 'data/unemployment-by-industry.csv',
+    },
+    transform: [{ type: 'stackY' }],
+    encode: {
+      x: 'date',
+      y: 'unemployed',
+      color: 'industry',
+    },
+    interaction: { tooltip: false },
+  };
+}
+
+unemploymentAreaLegendFilterPages.steps = ({ canvas }) => {
+  const { document } = canvas;
+  const [, page] = document.getElementsByClassName('items-item-page');
+  const elements = page.getElementsByClassName(LEGEND_ITEMS_CLASS_NAME);
+  const [e0] = elements;
+  return [step(e0, 'click')];
+};
+
+unemploymentAreaLegendFilterPages.maxError = 200;
