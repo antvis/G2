778cdf33432c0284f1a6ca1a18536bb8766edb76
MiniPear <subairui@icloud.com>
2023-07-20 14:57:31 +0800

    feat(title): auto layout (#5332)

---
+++ __tests__/plots/static/alphabet-interval-title-auto.ts
@@ -0,0 +1,32 @@
+import { G2Spec } from '../../../src';
+
+export function alphabetIntervalTitleAuto(): G2Spec {
+  return {
+    type: 'interval',
+    padding: 'auto',
+    title: {
+      title: 'Use frequency of keyboard keys',
+      subtitle: 'The mostest frequency letter are E, T, A.',
+      titleFontSize: 30,
+      subtitleFontSize: 20,
+    },
+    data: {
+      type: 'fetch',
+      value: 'data/alphabet.csv',
+    },
+    axis: {
+      y: { labelFormatter: '.0%' },
+    },
+    encode: {
+      x: 'letter',
+      y: 'frequency',
+      color: 'steelblue',
+    },
+    viewStyle: {
+      viewFill: '#4e79a7',
+      plotFill: '#f28e2c',
+      mainFill: '#e15759',
+      contentFill: '#76b7b2',
+    },
+  };
+}
