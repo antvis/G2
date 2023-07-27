085147125d83296cad75b73a2d1cfc3933860af5
MiniPear <subairui@icloud.com>
2023-07-27 19:51:39 +0800

    fix(line-xy): support bandOffset (#5351)

---
+++ __tests__/plots/static/seattle-weather-cell-lineXY.ts
@@ -0,0 +1,34 @@
+import { G2Spec } from '../../../src';
+
+export function settleWeatherCellLineXY(): G2Spec {
+  return {
+    type: 'view',
+    height: 325,
+    children: [
+      {
+        type: 'cell',
+        data: {
+          type: 'fetch',
+          value: 'data/seattle-weather.csv',
+        },
+        encode: {
+          x: (d) => new Date(d.date).getUTCDate(),
+          y: (d) => new Date(d.date).getUTCMonth(),
+          color: 'temp_max',
+        },
+        transform: [{ type: 'group', color: 'max' }],
+        style: { inset: 0.5 },
+      },
+      {
+        type: 'lineY',
+        data: [4],
+        style: { stroke: 'red', strokeWidth: 4, bandOffset: 0.5 },
+      },
+      {
+        type: 'lineX',
+        data: [4],
+        style: { stroke: 'red', strokeWidth: 4, bandOffset: 0.5 },
+      },
+    ],
+  };
+}
--- src/mark/lineY.ts
+++ src/mark/lineY.ts
@@ -1,21 +1,27 @@
-import { Vector } from '@antv/coord';
+import { deepMix } from '@antv/util';
 import { MarkComponent as MC, Vector2 } from '../runtime';
 import { LineYMark } from '../spec';
 import {
   baseAnnotationChannels,
   basePreInference,
   basePostInference,
+  createBandOffset,
 } from './utils';
 
 export type LineYOptions = Omit<LineYMark, 'type'>;
 
-export const LineY: MC<LineYOptions> = () => {
+export const LineY: MC<LineYOptions> = (options) => {
   return (index, scale, value, coordinate) => {
     const { y: Y } = value;
+    const offset = createBandOffset(
+      scale,
+      value,
+      deepMix({ style: { bandOffset: 0 } }, options),
+    );
     const P = Array.from(index, (i) => {
-      const p1 = [0, Y[i]] as Vector;
-      const p2 = [1, Y[i]] as Vector;
-      return [p1, p2].map((d) => coordinate.map(d)) as Vector2[];
+      const p1 = [0, Y[i]] as Vector2;
+      const p2 = [1, Y[i]] as Vector2;
+      return [p1, p2].map((d) => coordinate.map(offset(d, i))) as Vector2[];
     });
     return [index, P];
   };
