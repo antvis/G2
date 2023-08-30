ad175a0b232552c94c3c6d6305da28a9c42ee497
MiniPear <subairui@icloud.com>
2023-08-29 20:55:46 -0500

    feat(lib): change graphlib and plotlib (#5484)

--- __tests__/unit/lib/graph.spec.ts
+++ __tests__/unit/lib/graph.spec.ts
@@ -1,11 +1,17 @@
 import { graphlib } from '../../../src/lib';
-import { ForceGraph, Tree } from '../../../src/mark';
+import { ForceGraph, Tree, Sankey, Treemap, Pack } from '../../../src/mark';
+import { Cluster, Arc } from '../../../src/data';
 
 describe('graphlib', () => {
   it('graphlib() should returns expected graph components.', () => {
     expect(graphlib()).toEqual({
+      'data.cluster': Cluster,
+      'data.arc': Arc,
       'mark.forceGraph': ForceGraph,
       'mark.tree': Tree,
+      'mark.sankey': Sankey,
+      'mark.treemap': Treemap,
+      'mark.pack': Pack,
     });
   });
 });
