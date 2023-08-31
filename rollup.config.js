62608118b78cb50abd65cb6548a9624e712cd063
xcy960815 <18763006837@163.com>
2023-08-31 10:27:31 +0800

    fix(rollup): update rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,25 @@
-import { uglify } from 'rollup-plugin-uglify';
-import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+// 公共插件
 import nodePolyfills from 'rollup-plugin-polyfill-node';
-import { visualizer } from 'rollup-plugin-visualizer';
+import visualizer from 'rollup-plugin-visualizer';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
+// 新配置插件---------------
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
 const isBundleVis = !!process.env.BUNDLE_VIS;
-
-module.exports = [
-  {
-    input: 'src/index.ts',
-    output: {
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
       file: 'dist/g2.min.js',
       name: 'G2',
       format: 'umd',
       sourcemap: false,
+      // 私有插件
+      plugins: [isBundleVis && visualizer()],
     },
-    plugins: [
-      nodePolyfills(),
-      resolve(),
-      commonjs(),
-      typescript(),
-      uglify(),
-      ...(isBundleVis ? [visualizer()] : []),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
+    {
       file: 'dist/g2-lite.min.js',
       name: 'G2',
       format: 'umd',
@@ -37,7 +29,58 @@ module.exports = [
         '@antv/g-canvas': 'window.G.Canvas2D',
       },
     },
-    external: ['@antv/g', '@antv/g-canvas'],
-    plugins: [nodePolyfills(), resolve(), commonjs(), typescript(), uglify()],
-  },
-];
+  ],
+  // 公共插件
+  plugins: [
+    nodePolyfills(),
+    resolve(),
+    commonjs(),
+    typescript({
+      exclude: 'node_modules/**',
+      useTsconfigDeclarationDir: true,
+      extensions: ['.js', '.ts'],
+    }),
+    terser(),
+  ],
+  context: 'window', // 屏蔽 'THIS_IS_UNDEFINED' 警告
+};
+
+// 老配置插件---------------
+// import resolve from 'rollup-plugin-node-resolve';
+// import typescript from 'rollup-plugin-typescript';
+// const isBundleVis = !!process.env.BUNDLE_VIS;
+
+// module.exports = [
+//   {
+//     input: 'src/index.ts',
+//     output: {
+//       file: 'dist/g2.min.js',
+//       name: 'G2',
+//       format: 'umd',
+//       sourcemap: false,
+//     },
+//     plugins: [
+//       nodePolyfills(),
+//       resolve(),
+//       commonjs(),
+//       typescript(),
+//       terser(),
+//       ...(isBundleVis ? [visualizer()] : []),
+//     ],
+//   },
+//   {
+//     input: 'src/index.ts',
+//     output: {
+//       file: 'dist/g2-lite.min.js',
+//       name: 'G2',
+//       format: 'umd',
+//       sourcemap: false,
+//       globals: {
+//         '@antv/g': 'window.G',
+//         '@antv/g-canvas': 'window.G.Canvas2D',
+//       },
+//     },
+//     external: ['@antv/g', '@antv/g-canvas'],
+//     plugins: [nodePolyfills(), resolve(), commonjs(), typescript(), terser()],
+//   },
+// ];
