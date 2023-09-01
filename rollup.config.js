f3875b5f47bb8d7b531d6581878a93f67eb870d5
xcy960815 <18763006837@163.com>
2023-09-01 17:25:59 +0800

    fix(rollup): remove comments and remove old config code

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-// 公共插件
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import visualizer from 'rollup-plugin-visualizer';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-
-// 新配置插件---------------
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+
 const isBundleVis = !!process.env.BUNDLE_VIS;
+
 export default {
   input: 'src/index.ts',
   output: [
@@ -30,7 +29,6 @@ export default {
       },
     },
   ],
-  // 公共插件
   plugins: [
     nodePolyfills(),
     resolve(),
@@ -42,45 +40,5 @@ export default {
     }),
     terser(),
   ],
-  context: 'window', // 屏蔽 'THIS_IS_UNDEFINED' 警告
+  context: 'window', // Disable 'THIS_IS_UNDEFINED' warnings
 };
-
-// 老配置插件---------------
-// import resolve from 'rollup-plugin-node-resolve';
-// import typescript from 'rollup-plugin-typescript';
-// const isBundleVis = !!process.env.BUNDLE_VIS;
-
-// module.exports = [
-//   {
-//     input: 'src/index.ts',
-//     output: {
-//       file: 'dist/g2.min.js',
-//       name: 'G2',
-//       format: 'umd',
-//       sourcemap: false,
-//     },
-//     plugins: [
-//       nodePolyfills(),
-//       resolve(),
-//       commonjs(),
-//       typescript(),
-//       terser(),
-//       ...(isBundleVis ? [visualizer()] : []),
-//     ],
-//   },
-//   {
-//     input: 'src/index.ts',
-//     output: {
-//       file: 'dist/g2-lite.min.js',
-//       name: 'G2',
-//       format: 'umd',
-//       sourcemap: false,
-//       globals: {
-//         '@antv/g': 'window.G',
-//         '@antv/g-canvas': 'window.G.Canvas2D',
-//       },
-//     },
-//     external: ['@antv/g', '@antv/g-canvas'],
-//     plugins: [nodePolyfills(), resolve(), commonjs(), typescript(), terser()],
-//   },
-// ];
