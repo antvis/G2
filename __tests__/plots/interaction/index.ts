30450c33168ba4097f1dac5f33c8fd63adfeedc8
MiniPear <subairui@icloud.com>
2023-09-22 16:35:45 +0800

    fix(tooltip): marker (#5586)

--- __tests__/plots/interaction/index.ts
+++ __tests__/plots/interaction/index.ts
@@ -70,3 +70,4 @@ export { settleWeatherLegendFilter } from './seattle-weather-legend-filter';
 export { countriesBubbleMultiLegends } from './countries-bubble-multi-legends';
 export { pointsPointTooltipMarker } from './points-point-tooltip-marker';
 export { mockPieInteraction } from './mock-pie-interaction';
+export { missingAreaTooltipMarker } from './missing-area-tooltip-marker';
---
+++ __tests__/plots/interaction/missing-area-tooltip-marker.ts
@@ -0,0 +1,55 @@
+import { G2Spec, PLOT_CLASS_NAME } from '../../../src';
+import { step } from './utils';
+
+export function missingAreaTooltipMarker(): G2Spec {
+  return {
+    type: 'area',
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+      transform: [
+        {
+          type: 'map',
+          callback: (d) => ({
+            ...d,
+            close: d.date.getUTCMonth() <= 3 ? NaN : d.close,
+          }),
+        },
+      ],
+    },
+    encode: {
+      x: 'date',
+      y: 'close',
+    },
+    style: {
+      connect: true,
+      connectFill: 'grey',
+      connectFillOpacity: 0.15,
+    },
+    interaction: {
+      tooltip: {
+        body: false,
+        markerR: 10,
+        markerStrokeWidth: 5,
+        markerStroke: '#aaa',
+      },
+    },
+  };
+}
+
+missingAreaTooltipMarker.tooltip = true;
+
+missingAreaTooltipMarker.steps = ({ canvas }) => {
+  const { document } = canvas;
+  const [plot] = document.getElementsByClassName(PLOT_CLASS_NAME);
+  return [
+    step(plot, 'pointermove', {
+      offsetX: 100,
+      offsetY: 350,
+    }),
+    step(plot, 'pointermove', {
+      offsetX: 176,
+      offsetY: 350,
+    }),
+  ];
+};
