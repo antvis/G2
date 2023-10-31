ed9c12307eb540a4e36b19e3dfa443c36051b23b
Liu QingYi <qingyidev@126.com>
2023-10-31 21:52:21 +0800

    docs: 添加line-sample DEMO案例

---
+++ site/examples/general/line/demo/line-sample.ts
@@ -0,0 +1,26 @@
+/**
+ * A recreation of this demo: https://observablehq.com/@d3/line-chart
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart
+  .line()
+  .data({
+    type: 'fetch',
+    value:
+      'https://gw.alipayobjects.com/os/bmw-prod/551d80c6-a6be-4f3c-a82a-abd739e12977.csv',
+  })
+  .encode('x', 'date')
+  .encode('y', 'close')
+  .transform({
+    type: 'sample',
+    thresholds: 500,
+    strategy: 'max',
+  });
+
+chart.render();
