cc7c9fe2e5a6bab135ff49728ae09a3991289a08
MiniPear <subairui@icloud.com>
2023-11-06 17:59:35 +0800

    docs: add example for custom style of view frame (#5749)

---
+++ site/examples/composition/facet/demo/rect-frame.ts
@@ -0,0 +1,33 @@
+/**
+ * A recreation of one of these demos: https://observablehq.com/@observablehq/plot-facets?collection=@observablehq/plot
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  width: 928,
+  height: 320,
+  paddingLeft: 60,
+  paddingBottom: 60,
+});
+
+const facetRect = chart
+  .facetRect()
+  .data({
+    type: 'fetch',
+    value: 'https://assets.antv.antgroup.com/g2/anscombe.json',
+  })
+  .encode('x', 'series');
+
+facetRect
+  .point()
+  .attr('inset', 10)
+  .encode('x', 'x')
+  .encode('y', 'y')
+  .style('stroke', '#000')
+  .attr('frame', false) // Hide the default frame.
+  .viewStyle('plotStroke', 'red') // Customize the plot area to mock a frame.
+  .viewStyle('plotStrokeWidth', 2)
+  .viewStyle('plotOpacity', 0.5);
+
+chart.render();
