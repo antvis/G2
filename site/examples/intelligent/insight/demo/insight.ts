76db1c4763d80f16a6fe9cc1d0110192d7642eb7
yihe <laixingui1608@163.com>
2023-11-20 17:46:45 +0800

    docs: supplement optional parameters of insight mark

--- site/examples/intelligent/insight/demo/insight.ts
+++ site/examples/intelligent/insight/demo/insight.ts
@@ -11,19 +11,52 @@ const chart = new Chart({
   autoFit: true,
 });
 
-chart
-  .data({
-    type: 'fetch',
-    value:
-      'https://gw.alipayobjects.com/os/bmw-prod/551d80c6-a6be-4f3c-a82a-abd739e12977.csv',
-  })
-  .encode('x', 'date')
-  .encode('y', 'close');
-
-chart.line();
-// 指定洞察类型为趋势
-chart.insight({
-  insightType: 'trend',
+chart.options({
+  children: [
+    {
+      type: 'line',
+      data: {
+        type: 'fetch',
+        value:
+          'https://gw.alipayobjects.com/os/bmw-prod/551d80c6-a6be-4f3c-a82a-abd739e12977.csv',
+      },
+      encode: {
+        x: 'date',
+        y: 'close',
+      },
+    },
+    // insight mark
+    {
+      type: 'insight',
+      data: {
+        type: 'fetch',
+        value:
+          'https://gw.alipayobjects.com/os/bmw-prod/551d80c6-a6be-4f3c-a82a-abd739e12977.csv',
+      },
+      // Specify to add marks of type 'trend'
+      insightType: 'trend',
+      // If the value of dimensions or measures is not specified, it will be obtained from the encode information by default.
+      dimensions: [{ fieldName: 'date' }],
+      measures: [{ fieldName: 'close', method: 'SUM' }],
+      options: {
+        // Filter out not significant insights
+        filterInsight: true,
+        // Verify whether the input meets the algorithm requirements
+        dataValidation: true,
+        // Adjust the significance test threshold
+        algorithmParameter: {
+          // Parameter for trend mark
+          trend: {
+            threshold: 0.05,
+          },
+        },
+        // Generate Chinese spec
+        visualizationOptions: {
+          lang: 'zh-CN',
+        },
+      },
+    },
+  ],
 });
 
 chart.render();
