ecc910d0f836e13e01845f1d3a17e6346c3e96f8
MiniPear <pearminipro@gmail.com>
2023-11-28 14:55:07 +0800

    fix(tooltip): handle undefined

---
+++ __tests__/plots/interaction/stocks-line-slider.ts
@@ -0,0 +1,51 @@
+import { G2Spec, PLOT_CLASS_NAME } from '../../../src';
+import { SLIDER_CLASS_NAME } from '../../../src/interaction/sliderFilter';
+import { dispatchValueChange } from './appl-line-slider-filter';
+import { step } from './utils';
+
+export function stocksLineSlider(): G2Spec {
+  return {
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/stocks.csv',
+    },
+    encode: {
+      x: 'date',
+      y: 'price',
+      color: 'symbol',
+    },
+    children: [
+      {
+        type: 'line',
+        tooltip: { items: [{ channel: 'y', valueFormatter: '.1f' }] },
+        slider: { x: { formatter: () => '' } },
+      },
+      {
+        type: 'point',
+        tooltip: false,
+      },
+    ],
+    interaction: { tooltip: { crosshairsStrokeWidth: 10, body: false } },
+  };
+}
+
+stocksLineSlider.steps = ({ canvas }) => {
+  const { document } = canvas;
+  const [s1] = document.getElementsByClassName(SLIDER_CLASS_NAME);
+  const [plot] = document.getElementsByClassName(PLOT_CLASS_NAME);
+  return [
+    {
+      changeState: () => {
+        dispatchValueChange(s1, [0.1, 1]);
+      },
+      skip: true,
+    },
+    step(plot, 'pointermove', {
+      offsetX: 50,
+      offsetY: 200,
+    }),
+  ];
+};
+
+stocksLineSlider.tooltip = true;
