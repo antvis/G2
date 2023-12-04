d224b08c653d725c05a150c2f8f3c755a9379328
Runtus <893119806@qq.com>
2023-12-04 15:11:10 +0800

    fix: abstract code logic into function

--- src/animation/scaleInY.ts
+++ src/animation/scaleInY.ts
@@ -1,5 +1,11 @@
 import { arc } from 'd3-shape';
-import { Path, convertToPath, CSS, PropertySyntax } from '@antv/g';
+import {
+  Path,
+  convertToPath,
+  CSS,
+  PropertySyntax,
+  DisplayObject,
+} from '@antv/g';
 import { G2Element } from 'utils/selection';
 import { AnimationComponent as AC } from '../runtime';
 import { isTranspose, isPolar } from '../utils/coordinate';
@@ -29,17 +35,15 @@ export const ScaleInY: AC<ScaleInYOptions> = (options, context) => {
 
   return (from, _, defaults) => {
     const [shape] = from;
-    const { __data__, style } = shape as G2Element;
-    const {
-      transform: prefix = '',
-      radius = 0,
-      inset = 0,
-      fillOpacity = 1,
-      strokeOpacity = 1,
-      opacity = 1,
-    } = style;
-
-    if (isPolar(coordinate)) {
+    const PolarScaleInY = (shape: DisplayObject) => {
+      const { __data__, style } = shape as G2Element;
+      const {
+        radius = 0,
+        inset = 0,
+        fillOpacity = 1,
+        strokeOpacity = 1,
+        opacity = 1,
+      } = style;
       const { points, y, y1 } = __data__;
       const arcObject = getArcObject(coordinate, points, [y, y1]);
       const { innerRadius, outerRadius } = arcObject;
@@ -101,42 +105,57 @@ export const ScaleInY: AC<ScaleInYOptions> = (options, context) => {
       };
 
       return animation;
+    };
+
+    const RectangularScaleInY = (shape: DisplayObject) => {
+      const { style } = shape as G2Element;
+      const {
+        transform: prefix = '',
+        fillOpacity = 1,
+        strokeOpacity = 1,
+        opacity = 1,
+      } = style;
+      const [transformOrigin, transform]: [string, string] = isTranspose(
+        coordinate,
+      )
+        ? [`left top`, `scale(${ZERO}, 1)`] // left-top corner
+        : [`left bottom`, `scale(1, ${ZERO})`]; // left-bottom corner
+
+      // Using a short fadeIn transition to hide element with scale(0.001)
+      // which is still visible.
+      const keyframes = [
+        {
+          transform: `${prefix} ${transform}`.trimStart(),
+          transformOrigin,
+          fillOpacity: 0,
+          strokeOpacity: 0,
+          opacity: 0,
+        },
+        {
+          transform: `${prefix} ${transform}`.trimStart(),
+          transformOrigin,
+          fillOpacity,
+          strokeOpacity,
+          opacity,
+          offset: 0.01,
+        },
+        {
+          transform: `${prefix} scale(1, 1)`.trimStart(),
+          transformOrigin,
+          fillOpacity,
+          strokeOpacity,
+          opacity,
+        },
+      ];
+
+      const animation = shape.animate(keyframes, { ...defaults, ...options });
+      return animation;
+    };
+
+    if (isPolar(coordinate)) {
+      return PolarScaleInY(shape);
+    } else {
+      return RectangularScaleInY(shape);
     }
-
-    const [transformOrigin, transform]: [string, string] = isTranspose(
-      coordinate,
-    )
-      ? [`left top`, `scale(${ZERO}, 1)`] // left-top corner
-      : [`left bottom`, `scale(1, ${ZERO})`]; // left-bottom corner
-
-    // Using a short fadeIn transition to hide element with scale(0.001)
-    // which is still visible.
-    const keyframes = [
-      {
-        transform: `${prefix} ${transform} `.trimStart(),
-        transformOrigin,
-        fillOpacity: 0,
-        strokeOpacity: 0,
-        opacity: 0,
-      },
-      {
-        transform: `${prefix} ${transform}`.trimStart(),
-        transformOrigin,
-        fillOpacity,
-        strokeOpacity,
-        opacity,
-        offset: 0.01,
-      },
-      {
-        transform: `${prefix} scale(1, 1)`.trimStart(),
-        transformOrigin,
-        fillOpacity,
-        strokeOpacity,
-        opacity,
-      },
-    ];
-
-    const animation = shape.animate(keyframes, { ...defaults, ...options });
-    return animation;
   };
 };
