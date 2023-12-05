b21224269a8e35f7bf09377fa1e3d5d801073af1
taoliujun <290762903@qq.com>
2023-12-05 14:20:37 +0800

    fix: area and line shares the same curve generator(#5877)
    * docs: update description of mark shape options
    
    * fix: let area and line use the same curve smoothing strategy
    
    * test: update tests for modification of the area curve strategy
    
    * test: add tests for modification of the area curve strategy
    
    ---------
    
    Co-authored-by: taoliujun <taoliujun@qq.com>

---
+++ __tests__/plots/static/aapl-area-line-smooth-sample.ts
@@ -0,0 +1,49 @@
+import { G2Spec } from '../../../src';
+
+export function aaplAreaLineSmoothSample(): G2Spec {
+  return {
+    type: 'view',
+    data: {
+      type: 'fetch',
+      value: 'data/aapl.csv',
+    },
+    children: [
+      {
+        type: 'line',
+        encode: {
+          x: 'date',
+          y: 'close',
+          shape: 'smooth',
+        },
+        style: {
+          strokeWidth: 4,
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+      },
+      {
+        type: 'area',
+        encode: {
+          x: 'date',
+          y: 'close',
+          shape: 'smooth',
+        },
+        style: {
+          fillOpacity: 0.5,
+        },
+        transform: [
+          {
+            type: 'sample',
+            thresholds: 100,
+            strategy: 'lttb',
+          },
+        ],
+      },
+    ],
+  };
+}
