96fddb954c86fc248c08e18eb5eff45329d37c18
kun6696 <982492477@qq.com>
2024-01-04 18:20:06 +0800

    fix(axis):  gridClose accuracy error

--- __tests__/unit/coordinate/index.spec.ts
+++ __tests__/unit/coordinate/index.spec.ts
@@ -27,6 +27,7 @@ describe('coordinate', () => {
       ['translate', 0, -0.5],
       ['polar', (3 * Math.PI) / 2, (Math.PI * 7) / 2, 0, 1],
     ]);
+    // startAngle > endAgle && endAgle < 0
     expect(
       Polar({
         startAngle: Math.PI,
@@ -40,19 +41,15 @@ describe('coordinate', () => {
       ['translate', 0, -0.5],
       ['polar', Math.PI, (3 * Math.PI) / 2, 0.2, 0.8],
     ]);
-    expect(
-      Polar({
-        startAngle: Math.PI / 2,
-        endAngle: 0,
-        innerRadius: 0.2,
-        outerRadius: 0.8,
-      }),
-    ).toEqual([
-      ['translate', 0, 0.5],
-      ['reflect.y'],
-      ['translate', 0, -0.5],
-      ['polar', Math.PI / 2, 2 * Math.PI, 0.2, 0.8],
-    ]);
+    // endAgle - startAngle >= 2*Math.PI
+    const [, , , [, startAngle, endAngle]] = Polar({
+      startAngle: 3 * Math.PI,
+      endAngle: (15 * Math.PI) / 2,
+      innerRadius: 0.2,
+      outerRadius: 0.8,
+    });
+    expect(startAngle).toBeCloseTo(Math.PI);
+    expect(endAngle).toBeCloseTo((3 * Math.PI) / 2);
   });
 
   it('Parallel({...}) returns expected coordinate transformations', () => {
