e0cefbb5f1d4904092fc27e2e6e7b46cbd443ec0
Frank William <65594180+ai-qing-hai@users.noreply.github.com>
2024-01-29 15:00:33 +0800

    feat(shape): add gauge shape round (#6069)
    Co-authored-by: wb-xcf804241 <wb-xcf804241@alibaba-inc.com>

--- __tests__/plots/static/index.ts
+++ __tests__/plots/static/index.ts
@@ -216,6 +216,7 @@ export { soldIntervalCustomShape } from './sold-interval-custom-shape';
 export { gaugeDefault } from './gauge-default';
 export { gaugeCustomColor } from './gauge-custom-color';
 export { gaugeCustomShape } from './gauge-custom-shape';
+export { gaugeRoundShape } from './gauge-round-shape';
 export { scoreByItemAreaRadarSize } from './score-by-item-area-radar-size';
 export { mockPointLogTicks } from './mock-point-log-ticks';
 export { alphabetIntervalLabelRotate } from './alphabet-interval-label-rotate';
---
+++ site/examples/general/gauge/demo/gauge-round.ts
@@ -0,0 +1,32 @@
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart
+  .gauge()
+  .data({
+    value: {
+      target: 159,
+      total: 400,
+      name: 'score',
+      thresholds: [100, 200, 400],
+    },
+  })
+  .scale('color', {
+    range: ['#F4664A', '#FAAD14', 'green'],
+  })
+  .style({
+    arcShape: 'round',
+    arcLineWidth: 2,
+    arcStroke: '#fff',
+  })
+  .style(
+    'textContent',
+    (target, total) => `得分：${target}\n占比：${(target / total) * 100}%`,
+  )
+  .legend(false);
+
+chart.render();
