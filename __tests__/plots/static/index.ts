91b5c2f52acb0f075e33b4da725f92b86fe5f5e9
kun6696 <982492477@qq.com>
2024-02-21 19:40:36 +0800

    feat: add  mark of table

--- __tests__/plots/static/index.ts
+++ __tests__/plots/static/index.ts
@@ -317,3 +317,4 @@ export { scoreByItemAreaBasicRadar } from './score-by-item-area-basic-radar';
 export { scoreByItemAreaLineRadar } from './score-by-item-area-line-radar';
 export { mockComplexRadar } from './mock-complex-radar';
 export { mockSquareRadar } from './mock-square-radar';
+export { cars3Table } from './car3-table';
