633b291a563e3a61e534b225378af2a5d1a6f464
Bairui Su <subairui@icloud.com>
2024-07-22 19:09:55 +0800

    fix(slider): wrong position for labels (#6378)

--- src/interaction/sliderFilter.ts
+++ src/interaction/sliderFilter.ts
@@ -227,9 +227,6 @@ export function SliderFilter({
       }
     }
 
-    // Update to async state such as scale.
-    update();
-
     return () => {
       for (const [slider, handler] of sliderHandler) {
         slider.removeEventListener('valuechange', handler);
