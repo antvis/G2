aa089681bb20a6f20c87b453646e593e922d1cf1
Aaron <yangtao.yangtao@antgroup.com>
2024-10-24 11:48:52 +0800

    test: update test case and snapshots

--- __tests__/unit/g/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import '../../../src';
-import { runtime } from '@antv/g';
-
-describe('g.runtime.enableCSSParsing', () => {
-  it('should disable enableCSSParsing for g when import.', () => {
-    expect(runtime.enableCSSParsing).toBe(false);
-  });
-});
