aa82d9d2bbe0a0cf4ce1832c1f65a7125197b889
ljs <lujunsheng.ljs@antgroup.com>
2024-11-19 14:46:42 +0800

    feat: update test

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -8,13 +8,5 @@ export type Color = {
 
 export function parseToRGB(c: string | Color): Color {
   if (typeof c === 'object') return c;
-  const out = parseColor(c) as Color;
-  if (
-    typeof out?.r !== 'number' ||
-    typeof out?.g !== 'number' ||
-    typeof out?.b !== 'number'
-  ) {
-    console.warn(`parseToRGB: Invalid color: ${c}`);
-  }
-  return out;
+  return parseColor(c) as Color;
 }
