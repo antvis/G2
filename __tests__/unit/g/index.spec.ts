cac93854dbed3d4c527b849266e90255b7772b21
ljs <lujunsheng.ljs@antgroup.com>
2024-11-19 12:55:56 +0800

    fix: update test

--- __tests__/unit/g/index.spec.ts
+++ __tests__/unit/g/index.spec.ts
@@ -3,8 +3,8 @@ import { runtime } from '@antv/g';
 
 describe('g.runtime.enableCSSParsing', () => {
   it('should disable enableCSSParsing for g when import.', () => {
-    expect((runtime as { enableCSSParsing?: boolean })?.enableCSSParsing).toBe(
-      false,
-    );
+    expect(
+      (runtime as { enableCSSParsing?: boolean })?.enableCSSParsing,
+    ).toBeFalsy();
   });
 });
