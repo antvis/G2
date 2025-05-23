0270e427604c1c8c64696124524b35d33343f82d
ian <yihengz@outlook.com>
2025-05-23 13:19:53 +0800

    test: remove jest

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,18 +7,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     include: ['__tests__/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    exclude: [
-      '__tests__/integration/api-chart-emit-legend-filter.spec.ts',
-      '__tests__/integration/api-chart-emit-slider-filter.spec.ts',
-      '__tests__/integration/api-chart-on-component-click.spec.ts',
-      '__tests__/integration/api-chart-on-brush-filter.spec.ts',
-      '__tests__/integration/api-chart-auto-fit.spec.ts',
-      '__tests__/integration/chart-on-brush-filter.spec.ts',
-      '__tests__/integration/spec-interaction.spec.ts',
-      '__tests__/integration/spec-static.spec.ts',
-      '__tests__/integration/spec-animation.spec.ts',
-      '__tests__/integration/animation-control.spec.ts',
-    ],
     coverage: {
       provider: 'istanbul',
       include: ['src/**/*.ts'],
