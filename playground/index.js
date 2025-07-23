3eb82866ba582ceb34530a80aef61a1cf13a21b6
huangtao <ht01604267@antgroup.com>
2025-07-23 10:47:38 +0800

    fix: exceedAdjust don't work ok

--- playground/index.js
+++ playground/index.js
@@ -1,45 +1,30 @@
 import { Chart } from '../src';
-// 有必要的话，请在需要注意的地方加上注释
 
-const chart = new Chart({ container: 'container', autoFit: true });
+const chart = new Chart({ container: 'container' });
 
-// ========== spec 风格模板 ==========
-// chart.options({
-//   type: 'interval',
-//   autoFit: true,
-//   data: [
-//     { letter: 'A', frequency: 0.08167 },
-//     { letter: 'B', frequency: 0.01492 },
-//     { letter: 'C', frequency: 0.02782 },
-//     { letter: 'D', frequency: 0.04253 },
-//     { letter: 'E', frequency: 0.12702 },
-//     { letter: 'F', frequency: 0.02288 },
-//     { letter: 'G', frequency: 0.02015 },
-//   ],
-//   encode: { x: 'letter', y: 'frequency' },
-// });
-
-// // ========== api 风格模板 ==========
-const data = [
-  { letter: 'A', frequency: 0.08167 },
-  { letter: 'B', frequency: 0.01492 },
-  { letter: 'C', frequency: 0.02782 },
-  { letter: 'D', frequency: 0.04253 },
-  { letter: 'E', frequency: 0.12702 },
-  { letter: 'F', frequency: 0.02288 },
-  { letter: 'G', frequency: 0.02015 },
-];
-chart
-  .interval()
-  .data(data)
-  .encode('x', 'letter')
-  .encode('y', 'frequency')
-  .scale('y', {
-    nice: 1,
-    tickMethod: (a, b, c, d) => {
-      console.log({ a, b, c, d });
-      return [0, 0.04, 0.08, 0.12, 0.14, 0.16];
+chart.options({
+  type: 'interval',
+  animate: false,
+  data: [{ letter: 'A', frequency: 0.08167 }],
+  encode: { x: 'letter', y: 'frequency' },
+  labels: [
+    {
+      text: 'frequency',
+      position: 'bottom',
+      dy: 20,
+      transform: [
+        {
+          type: 'exceedAdjust', // 加了这个，标签应该在图表内
+          bounds: 'main',
+        },
+      ],
     },
-  });
+  ],
+  slider: {
+    x: {
+      values: [0, 0.5], // 注释后正常生效，label 展示在图表区域内
+    },
+  },
+});
 
 chart.render();
