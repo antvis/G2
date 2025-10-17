f27557b5a1a616ea27c698859ba9ab939531da2c
Joel Alan <31396322+lxfu1@users.noreply.github.com>
2025-10-17 14:28:22 +0800

    fix: tooltip display is abnormal (#7149)

--- src/interaction/tooltip.ts
+++ src/interaction/tooltip.ts
@@ -146,7 +146,10 @@ function showTooltip({
   tooltipElement.update({
     x,
     y,
-    data: items,
+    data: items.map((item) => ({
+      ...item,
+      value: !item.value && item.value !== 0 ? '' : item.value,
+    })),
     title,
     position,
     enterable,
