a8a0eb7f73dd35569dd061ae6030cbc855dd1f05
blue.wang <wanglxi1@163.com>
2025-10-21 19:17:50 +0800

    修复 legend:focus 的 off 误写成 on 的BUG (#7157)

--- src/interaction/legendFilter.ts
+++ src/interaction/legendFilter.ts
@@ -255,7 +255,7 @@ function legendFilterOrdinal(
         focusIcon.removeEventListener('click', focusIconClick.get(item));
       }
 
-      emitter.on('legend:focus', onFocus);
+      emitter.off('legend:focus', onFocus);
       emitter.off('legend:filter', onFilter);
       emitter.off('legend:reset', onEnd);
     }
